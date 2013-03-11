e85fa024ad528c6c5ec63aef10fe5822b2b3cc7f
mekka blue <mekka@mekkablue.com>
2013-03-11 20:22:25 +0100

    Added Horizontal Center script
    Script centers the selected path inside the layer width.

---
+++ Align/Selection to Center.py
@@ -0,0 +1,27 @@
+#MenuTitle: Align Selection to Horizontal Center
+"""Align selected paths (and parts of paths) in the frontmost layer to the Horizontal Center."""
+
+import GlyphsApp
+
+Doc = Glyphs.currentDocument
+selectedLayer = Doc.selectedLayers()[0]
+layerCenter = selectedLayer.width // 2
+
+try:
+	selection = selectedLayer.selection()
+	selectionXList = [ n.x for n in selection ]
+	leftMostX = min( selectionXList )
+	rightMostX = max( selectionXList )
+	selectionCenter = ( leftMostX + rightMostX ) // 2
+	centerOffset = float( layerCenter - selectionCenter )
+
+	Font.disableUpdateInterface()
+
+	for thisNode in selection:
+		thisNode.x += centerOffset
+
+	Font.enableUpdateInterface()
+	
+except Exception, e:
+	print "Error: Nothing selected in frontmost layer?"
+	print e
