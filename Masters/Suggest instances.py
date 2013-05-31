5ef9a00dfcca67ea7b83e45e90835287fb78dfee
mekka blue <mekka@mekkablue.com>
2013-05-31 17:19:24 +0200

    Improved algorithm
    - significantly cleaned up the code
    - now future-imports division lib
    - thx hop!

--- Masters/Suggest instances.py
+++ Masters/Suggest instances.py
@@ -1,18 +1,11 @@
 #MenuTitle: Suggest instances
 # -*- coding: utf-8 -*-
 """Outputs suggestions for instance weight values, based on the weight values of the masters in the current font."""
-
+from __future__ import division
 import GlyphsApp
 
-def distribute( minWt, maxWt, numOfInst ):
-	returnlist = [  ]
-	for i in range( numOfInst ):
-		gain = ( float(maxWt) / float(minWt) ) ** ( 1 / float(numOfInst) )
-		instanceValue = float(minWt) * gain ** i
-		returnlist.append( str(int(instanceValue)) )
-	
-	returnlist.append( str(int(maxWt)) )
-	return (returnlist, gain)
+def distribute(min, max, n):
+	return [ min * (max / min) ** ( i/(n-1) ) for i in range(n) ]
 			
 weightvalues = [ m.weightValue for m in Glyphs.font.masters ]
 minimumWeight = min( weightvalues )
@@ -20,17 +13,21 @@ maximumWeight = max( weightvalues )
 
 Glyphs.clearLog()
 Glyphs.showMacroWindow()
+
 if minimumWeight != maximumWeight:
 	print "Suggested Luc(as) distributions between %i and %i:" % ( minimumWeight, maximumWeight )
 	print
 	
-	for i in range(2,10):
-		(distributedValues, distributedGain) = distribute( minimumWeight, maximumWeight, i )
-		print "%i instances: %s --> growth: %.1f%%" % ( i+1, "-".join( distributedValues ), distributedGain*100-100 )
+	for i in range( 3, 11 ):
+		distributedValues = distribute( minimumWeight, maximumWeight, i )
+		distributedGain   = distributedValues[1] / distributedValues[0]
+		print "%i instances: %s --> growth: %.1f%%" % ( i, "-".join("{0:.0f}".format(weight) for weight in distributedValues ), distributedGain*100-100 )
 	
 	print
 	print "Hint 1: growth of 41.4% = squareroot of 2."
 	print "Hint 2: growth of 61.8% = Golden Mean."
 	print "Hint 3: steady growth curves may not work well for light weights."
+	
 else:
 	print "You need at least two masters with differing weight values."
+	
