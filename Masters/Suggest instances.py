fb85b83b011239feec1c0b65ced63c16d0a5b901
mekka blue <mekka@mekkablue.com>
2013-05-31 14:04:54 +0200

    Improved Suggest instances
    - now also outputs the percentage gain
    - gives you hints (thx @clauseggers)

--- Masters/Suggest instances.py
+++ Masters/Suggest instances.py
@@ -7,11 +7,12 @@ import GlyphsApp
 def distribute( minWt, maxWt, numOfInst ):
 	returnlist = [  ]
 	for i in range( numOfInst ):
-		instanceValue = float(minWt) * ( float(maxWt)/float(minWt) ) ** ( (i)/float(numOfInst) )
+		gain = ( float(maxWt) / float(minWt) ) ** ( 1 / float(numOfInst) )
+		instanceValue = float(minWt) * gain ** i
 		returnlist.append( str(int(instanceValue)) )
 	
 	returnlist.append( str(int(maxWt)) )
-	return returnlist
+	return (returnlist, gain)
 			
 weightvalues = [ m.weightValue for m in Glyphs.font.masters ]
 minimumWeight = min( weightvalues )
@@ -20,8 +21,16 @@ maximumWeight = max( weightvalues )
 Glyphs.clearLog()
 Glyphs.showMacroWindow()
 if minimumWeight != maximumWeight:
-	print "Suggested distributions between %i and %i:" % ( minimumWeight, maximumWeight )
-	for i in range(3,10):
-		print "%i instances: %s" % ( i, " - ".join( distribute( minimumWeight, maximumWeight, i ) ))
+	print "Suggested Luc(as) distributions between %i and %i:" % ( minimumWeight, maximumWeight )
+	print
+	
+	for i in range(2,10):
+		(distributedValues, distributedGain) = distribute( minimumWeight, maximumWeight, i )
+		print "%i instances: %s --> growth: %.1f%%" % ( i+1, "-".join( distributedValues ), distributedGain*100-100 )
+	
+	print
+	print "Hint 1: growth of 41.4% = squareroot of 2."
+	print "Hint 2: growth of 61.8% = Golden Mean."
+	print "Hint 3: steady growth curves may not work well for light weights."
 else:
 	print "You need at least two masters with differing weight values."
