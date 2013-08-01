4a6e95a904a6219a9467cc12e881df4dce6fb9b8
mekka blue <mekka@mekkablue.com>
2013-08-01 12:46:56 +0200

    Bugfix for Anchor Mover 2
    fixed a bug where the rest of the glyphs was skipped if there was a
    newline in the selection

--- Anchors/Anchor Mover 2.py
+++ Anchors/Anchor Mover 2.py
@@ -130,55 +130,56 @@ class AnchorMover2( object ):
 		Font.disableUpdateInterface()
 		
 		for originalLayer in selectedLayers:
-			thisGlyph = originalLayer.parent
-			thisLayer = originalLayer.copyDecomposedLayer()
-			originalLayer.setDisableUpdates()
-			thisGlyph.undoManager().beginUndoGrouping() # not working?
+			if originalLayer.name != None:
+				thisGlyph = originalLayer.parent		
+				thisLayer = originalLayer.copyDecomposedLayer()
+				originalLayer.setDisableUpdates()
+				thisGlyph.undoManager().beginUndoGrouping() # not working?
 			
-			try:
-				if respectItalic:
-					for mPath in thisLayer.paths:
-						for mNode in mPath.nodes:
-							mNode.x = italicSkew( mNode.x, mNode.y, -italicAngle )
+				try:
+					if respectItalic:
+						for mPath in thisLayer.paths:
+							for mNode in mPath.nodes:
+								mNode.x = italicSkew( mNode.x, mNode.y, -italicAngle )
 							
-					for mAnchor in thisLayer.anchors:
-						mAnchor.x = italicSkew( mAnchor.x, mAnchor.y, -italicAngle )
+						for mAnchor in thisLayer.anchors:
+							mAnchor.x = italicSkew( mAnchor.x, mAnchor.y, -italicAngle )
 							
-				for thisAnchor in thisLayer.anchors:
-					if thisAnchor.name == anchor_name:
-						old_anchor_x = thisAnchor.x
-						old_anchor_y = thisAnchor.y
-						xMove = eval( evalCodeH ) + horizontal_change
-						yMove = eval( evalCodeV ) + vertical_change
+					for thisAnchor in thisLayer.anchors:
+						if thisAnchor.name == anchor_name:
+							old_anchor_x = thisAnchor.x
+							old_anchor_y = thisAnchor.y
+							xMove = eval( evalCodeH ) + horizontal_change
+							yMove = eval( evalCodeV ) + vertical_change
 						
-						# Ignore moves relative to bbox if there are no paths:
-						if not thisLayer.paths:
-							if "bounds" in evalCodeH:
-								xMove = old_anchor_x
+							# Ignore moves relative to bbox if there are no paths:
+							if not thisLayer.paths:
+								if "bounds" in evalCodeH:
+									xMove = old_anchor_x
 							
-							if "bounds" in evalCodeV:
-								yMove = old_anchor_y
+								if "bounds" in evalCodeV:
+									yMove = old_anchor_y
 						
-						# Only move if the calculated position differs from the original one:
-						if [ int(old_anchor_x + italicCorrection), int(old_anchor_y) ] != [ int(xMove), int(yMove) ]:
-							if respectItalic:
-								# skew back
-								xMove = italicSkew( xMove, yMove, italicAngle ) - italicCorrection
-								old_anchor_x = italicSkew( old_anchor_x, old_anchor_y, italicAngle ) - italicCorrection
+							# Only move if the calculated position differs from the original one:
+							if [ int(old_anchor_x + italicCorrection), int(old_anchor_y) ] != [ int(xMove), int(yMove) ]:
+								if respectItalic:
+									# skew back
+									xMove = italicSkew( xMove, yMove, italicAngle ) - italicCorrection
+									old_anchor_x = italicSkew( old_anchor_x, old_anchor_y, italicAngle ) - italicCorrection
 							
-							originalAnchorPosition = NSPoint()
-							originalAnchorPosition.x = xMove
-							originalAnchorPosition.y = yMove
-							originalAnchor = GSAnchor( anchor_name, originalAnchorPosition )
-							originalLayer.addAnchor_( originalAnchor )
+								originalAnchorPosition = NSPoint()
+								originalAnchorPosition.x = xMove
+								originalAnchorPosition.y = yMove
+								originalAnchor = GSAnchor( anchor_name, originalAnchorPosition )
+								originalLayer.addAnchor_( originalAnchor )
 							
-							print "Moved %s anchor from %i, %i to %i, %i in %s." % ( anchor_name, old_anchor_x, old_anchor_y, xMove, yMove, thisGlyph.name )
-			except Exception, e:
-				print "ERROR: Failed to move anchor in %s." % thisGlyph.name
-				print e
+								print "Moved %s anchor from %i, %i to %i, %i in %s." % ( anchor_name, old_anchor_x, old_anchor_y, xMove, yMove, thisGlyph.name )
+				except Exception, e:
+					print "ERROR: Failed to move anchor in %s." % thisGlyph.name
+					print e
 			
-			thisGlyph.undoManager().endUndoGrouping()
-			originalLayer.setEnableUpdates()
+				thisGlyph.undoManager().endUndoGrouping()
+				originalLayer.setEnableUpdates()
 		
 		Font.enableUpdateInterface()
 		Font.updateInterface()
