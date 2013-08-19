1dee3a2f19b52b4c8fce09d70c5f2a6c1dfc9a03
mekka blue <mekka@mekkablue.com>
2013-08-19 08:05:36 -0700

    Added Reset alternate glyph width script
    Reset alternate glyph width: resets the width of selected suffixed
    glyphs to the width of their unsuffixed counterparts. E.g.,
    Adieresis.ss01 will be reset to the width of Adieresis.

---
+++ Metrics/Reset alternate glyph width.py
@@ -0,0 +1,29 @@
+#MenuTitle: Reset alternate glyph widths
+# -*- coding: utf-8 -*-
+"""Sets the width of selected .ss01 (or any other extension) widths in the font to the width of their base glyphs. E.g. A.ss01 will have the same width as A."""
+
+import GlyphsApp
+Doc  = Glyphs.currentDocument
+Font = Glyphs.font
+FontMaster = Doc.selectedFontMaster()
+selectedLayers = Doc.selectedLayers()
+
+def resetWidth( thisLayer, thisName ):
+	baseGlyphName = thisName[:thisName.find(".")]
+	baseGlyph = Font.glyphs[ baseGlyphName ]
+	baseLayer = baseGlyph.layers[ FontMaster.id ]
+	baseWidth = baseLayer.width
+	thisLayer.width = baseWidth
+	return baseWidth
+
+Font.disableUpdateInterface()
+
+for thisLayer in selectedLayers:
+	thisGlyph = thisLayer.parent
+	thisGlyphName = thisGlyph.name
+	if "." in thisGlyphName:
+		thisLayer.undoManager().beginUndoGrouping()
+		print "Resetting width of %s to %.0f." % ( thisGlyphName, resetWidth( thisLayer, thisGlyphName ) )
+		thisLayer.undoManager().endUndoGrouping()
+
+Font.enableUpdateInterface()
--- README.md
+++ README.md
@@ -85,6 +85,7 @@ And if you are running Glyphs on Lion (10.7) or later, you may want to add:
 * Extract kern strings 2nd char: asks you for a group of characters, then prompts you for one or more text files; it will then output all kerning pairs (containing these chars, found in the text files) to a new Edit tab, alphabetically sorted. Finds all pairs where the entered chars are the 2nd letter. *Needs Vanilla.*
 * Find and Replace Metrics Keys: GUI for searching and replacing text in the L and R metrics keys, e.g. replace "=X+15" by "=X", etc.
 * Make tab with letter combos: takes your selected glyphs and opens a new tab with all possible combinations of the letters.
+* Reset alternate glyph width: resets the width of suffixed glyphs to the width of their unsuffixed counterparts. E.g., Adieresis.ss01 will be reset to the width of Adieresis.
 
 ## OpenType
 * Floating Features: Floating palettes for activating and deactivating OT features. Same functionality as the pop-up menu. *Needs Vanilla.*
