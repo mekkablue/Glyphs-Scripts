03dbaa097f74014a2a268bdacbb00e741303145f
mekka blue <mekka@mekkablue.com>
2013-12-10 16:51:06 +0100

    Added script 'Change Metrics by Percentage'
    By popular request, I added a script to increase the LSB/RSB values of
    selected glyphs by a percentage value. Enter negative values to
    decrease.

---
+++ Metrics/Change Metrics by Percentage.py
@@ -0,0 +1,81 @@
+#MenuTitle: Change Metrics by Percentage
+# -*- coding: utf-8 -*-
+"""Increase sidebearings of selected glyphs by a percentage value."""
+
+import vanilla
+import GlyphsApp
+
+class ChangeMetricsbyPercentage( object ):
+	def __init__( self ):
+		self.w = vanilla.FloatingWindow( (350, 60), "Change Metrics by Percentage", minSize=(350, 60), maxSize=(500, 60), autosaveName="com.mekkablue.ChangeMetricsbyPercentage.mainwindow" )
+
+		self.w.text_1 = vanilla.TextBox( (15, 12+2, 50, 14), "Increase", sizeStyle='small' )
+		self.w.text_2 = vanilla.TextBox( (155, 12+2, 20, 14), "by", sizeStyle='small' )
+		self.w.text_3 = vanilla.TextBox( (-110, 12+2, -95, 14), "%", sizeStyle='small' )
+		
+		self.w.LSB = vanilla.CheckBox( (15+55,    12, 40, 18), "LSB", value=True, sizeStyle='small', callback=self.SavePreferences )
+		self.w.RSB = vanilla.CheckBox( (15+55+45, 12, 40, 18), "RSB", value=True, sizeStyle='small', callback=self.SavePreferences )
+
+		self.w.changeValue = vanilla.EditText( (180, 12, -180, 15+3), "+10.0", sizeStyle = 'small')
+		
+		
+		self.w.runButton    = vanilla.Button((-90,    12-1, -15, 19), "Change", sizeStyle='small', callback=self.ChangeMetricsbyPercentageMain )
+		self.w.revertButton = vanilla.Button((-90-80, 12-1, -95, 19), "Revert", sizeStyle='small', callback=self.ChangeMetricsbyPercentageMain )
+
+		self.w.setDefaultButton( self.w.runButton )
+		
+		try:
+			self.LoadPreferences( )
+		except:
+			pass
+
+		self.w.open()
+		
+	def SavePreferences( self, sender ):
+		try:
+			Glyphs.defaults["com.mekkablue.ChangeMetricsbyPercentage.LSB"] = self.w.LSB.get()
+			Glyphs.defaults["com.mekkablue.ChangeMetricsbyPercentage.LSB"] = self.w.RSB.get()
+			Glyphs.defaults["com.mekkablue.ChangeMetricsbyPercentage.changeValue"] = self.w.changeValue.get()
+		except:
+			return False
+			
+		return True
+
+	def LoadPreferences( self ):
+		try:
+			self.w.LSB.set( Glyphs.defaults["com.mekkablue.ChangeMetricsbyPercentage.LSB"] )
+			self.w.RSB.set( Glyphs.defaults["com.mekkablue.ChangeMetricsbyPercentage.RSB"] )
+			self.w.changeValue.set( Glyphs.defaults["com.mekkablue.ChangeMetricsbyPercentage.changeValue"] )
+		except:
+			return False
+			
+		return True
+
+	def ChangeMetricsbyPercentageMain( self, sender ):
+		try:
+			Font = Glyphs.font
+			selectedLayers = Font.selectedLayers
+			
+			changeLSB = self.w.LSB.get()
+			changeRSB = self.w.RSB.get()
+			change = ( 100.0 + float( self.w.changeValue.get() ) ) / 100.0
+
+			if sender == self.w.revertButton:
+				change = 1.0 / change
+			
+			if changeLSB:
+				for thisLayer in selectedLayers:
+					thisLayer.LSB *= change
+			
+			if changeRSB:
+				for thisLayer in selectedLayers:
+					thisLayer.RSB *= change
+			
+			if not self.SavePreferences( self ):
+				print "Note: could not write preferences."
+			
+			# self.w.close()
+		except Exception, e:
+			raise e
+
+ChangeMetricsbyPercentage()
--- README.md
+++ README.md
@@ -86,6 +86,7 @@ And if you are running Glyphs on Lion (10.7) or later, you should add:
 ## Metrics
 * **Adjust Kerning:** GUI to add a value to all kerning pairs, multiply all pairs by a value or round them by a value. *Needs Vanilla.*
 * **Center glyphs:** centers all selected glyphs inside their width, so that LSB=RSB.
+* **Change Metrics by Percentage:** Change LSB/RSB of selected glyphs by a percentage value. Undo with the Revert button. *Needs Vanilla.*
 * **Copy kerning groups:** steals left/right kerning groups for all selected glyphs from a 2nd font. *Needs Vanilla.*
 * **Copy sidebearings:** steals the sidebearing values for all selected glyphs from a 2nd font. Ignores metrics keys like '=x+20'. *Needs Vanilla.*
 * **Delete guidelines:** deletes all local guidelines in selected glyphs.
