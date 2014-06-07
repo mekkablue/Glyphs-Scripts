edfc38a450b5cc8b5b440712de02bfadf58c0e9e
mekka blue <mekka@mekkablue.com>
2014-06-08 01:46:24 +0200

    Major algorithm improvement
    Improved algorithm for zero handles. Now same as in
    FixZeroHandles.glyphsFilter

--- Paths/Tunnify.py
+++ Paths/Tunnify.py
@@ -10,6 +10,9 @@ selectedLayer = Font.selectedLayers[0]
 selectedGlyph = selectedLayer.parent
 selection = selectedLayer.selection()
 
+tunnifiedZeroLo = 0.43
+tunnifiedZeroHi = 0.73
+
 def intersect( x1, y1,  x2, y2,  x3, y3,  x4, y4 ):
 	"""Calculates the intersection of line P1-P2 with P3-P4."""
 
@@ -19,12 +22,12 @@ def intersect( x1, y1,  x2, y2,  x3, y3,  x4, y4 ):
 	
 	try:
 		slope12 = ( float(y2) - float(y1) ) / ( float(x2) - float(x1) )
-	except:
+	except: # division by zero
 		slope12vertical = True
 
 	try:
 		slope34 = ( float(y4) - float(y3) ) / ( float(x4) - float(x3) )
-	except:
+	except: # division by zero
 		slope34vertical = True
 	
 	if not slope12vertical and not slope34vertical:
@@ -42,51 +45,63 @@ def intersect( x1, y1,  x2, y2,  x3, y3,  x4, y4 ):
 def pointdistance( x1, y1, x2, y2 ):
 	"""Calculates the distance between P1 and P2."""
 	dist = ( ( float(x2) - float(x1) ) ** 2 + ( float(y2) - float(y1) ) **2 ) ** 0.5
-	
 	return dist
 
 def bezier( x1, y1,  x2,y2,  x3,y3,  x4,y4,  t ):
 	x = x1*(1-t)**3 + x2*3*t*(1-t)**2 + x3*3*t**2*(1-t) + x4*t**3
 	y = y1*(1-t)**3 + y2*3*t*(1-t)**2 + y3*3*t**2*(1-t) + y4*t**3
-
 	return x, y
 
-def tunnify( segment ):
-	"""Calculates the average curvature for Bezier curve segment P1, P2, P3, P4 and returns new values for P2, P3"""
-	[P1, P2, P3, P4] = segment
-	[x1, y1] = P1
-	[x2, y2] = P2
-	[x3, y3] = P3
-	[x4, y4] = P4
+def xyAtPercentageBetweenTwoPoints( firstPoint, secondPoint, percentage ):
+	"""
+	Returns the x, y for the point at percentage
+	(where 100 percent is represented as 1.0)
+	between NSPoints firstPoint and secondPoint.
+	"""
+	x = firstPoint.x + percentage * ( secondPoint.x - firstPoint.x )
+	y = firstPoint.y + percentage * ( secondPoint.y - firstPoint.y )
+	return x, y
+
+def handlePercentages( segment ):
+	"""Calculates the handle distributions and intersection for segment P1, P2, P3, P4."""
+	x1, y1 = segment[0]
+	x2, y2 = segment[1]
+	x3, y3 = segment[2]
+	x4, y4 = segment[3]
 	
-	
-	
-	# Fix Illustrator's zero-handles: 
 	if [x1, y1] == [x2, y2]:
-		x2, y2 = bezier( x1, y1,  x2, y2,  x3, y3,  x4, y4,  0.15)
-		tunnifiedPercentage = 0.627
-		xInt, yInt = intersect( x1, y1,  x2, y2,  x3, y3,  x4, y4 )
+		# zero handle at beginning of segment
+		xInt, yInt = x3, y3
+		return tunnifiedZeroLo, tunnifiedZeroHi, xInt, yInt
 	elif [x3, y3] == [x4, y4]:
-		x3, y3 = bezier( x1, y1,  x2, y2,  x3, y3,  x4, y4,  0.85)
-		tunnifiedPercentage = 0.627
-		xInt, yInt = intersect( x1, y1,  x2, y2,  x3, y3,  x4, y4 )
+		# zero handle at end of segment
+		xInt, yInt = x2, y2
+		return tunnifiedZeroHi, tunnifiedZeroLo, xInt, yInt
 	else:
-	# Calculate average percentage:
+		# no zero handle, just bad distribution
 		xInt, yInt = intersect( x1, y1,  x2, y2,  x3, y3,  x4, y4 )
 		percentageP1P2 = pointdistance( x1, y1, x2, y2 ) / pointdistance( x1, y1, xInt, yInt )
 		percentageP3P4 = pointdistance( x4, y4, x3, y3 ) / pointdistance( x4, y4, xInt, yInt )
-		tunnifiedPercentage = ( percentageP1P2 + percentageP3P4 ) / 2.0
+		tunnifiedPercentage = ( percentageP1P2 + percentageP3P4 ) / 1.95
+		return tunnifiedPercentage, tunnifiedPercentage, xInt, yInt
+
+def tunnify( segment ):
+	"""
+	Calculates the average curvature for Bezier curve segment P1, P2, P3, P4,
+	and returns new values for P2, P3.
+	"""
+	x1, y1 = segment[0]
+	x4, y4 = segment[3]
+	firstHandlePercentage, secondHandlePercentage, xInt, yInt = handlePercentages( segment )
 	
-	print tunnifiedPercentage * 100
+	intersectionPoint = NSPoint( xInt, yInt )
+	segmentStartPoint = NSPoint( x1, y1 )
+	segmentFinalPoint = NSPoint( x4, y4 )
 	
-	# Calculate new handle positions:
-	x_handle1 = x1 + tunnifiedPercentage * ( xInt - x1 )
-	y_handle1 = y1 + tunnifiedPercentage * ( yInt - y1 )
-	x_handle2 = x4 + tunnifiedPercentage * ( xInt - x4 )
-	y_handle2 = y4 + tunnifiedPercentage * ( yInt - y4 )
-	
-	return x_handle1, y_handle1, x_handle2, y_handle2
-	
+	firstHandleX,  firstHandleY  = xyAtPercentageBetweenTwoPoints( segmentStartPoint, intersectionPoint, firstHandlePercentage )
+	secondHandleX, secondHandleY = xyAtPercentageBetweenTwoPoints( segmentFinalPoint, intersectionPoint, secondHandlePercentage )
+	return firstHandleX, firstHandleY, secondHandleX, secondHandleY
+
 selectedGlyph.beginUndo()
 
 try:
@@ -117,5 +132,4 @@ except Exception, e:
 	print "Error:", e
 	pass
 
-#selectedGlyph.beginUndo()
 selectedGlyph.endUndo()
