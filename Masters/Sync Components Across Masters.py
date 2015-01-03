d0e9830a585bf7c444a204a466ee159df1ca9f13
mekka blue <mekka@mekkablue.com>
2015-01-03 20:19:40 +0100

    Now tries to recreate anchor associations
    If component x was attached to anchor top_x in the current layer, the
    script will try to attach the component in the other masters to the
    anchor of the same name, if available. Important for Arabic ligatures,
    where there is often top_1, top_2, etc. (Thx @iSkyFalConS)

--- Masters/Sync Components Across Masters.py
+++ Masters/Sync Components Across Masters.py
@@ -1,7 +1,7 @@
-#MenuTitle: Sync Components Across All Other Masters
+#MenuTitle: Sync Components Across Masters
 # -*- coding: utf-8 -*-
 __doc__="""
-Takes the current layer’s components, and resets all other masters to the same component structure.
+Takes the current layer’s components, and resets all other masters to the same component structure. Ignores paths and anchors.
 """
 
 import GlyphsApp
@@ -10,13 +10,46 @@ thisFont = Glyphs.font # frontmost font
 thisFontMaster = thisFont.selectedFontMaster # active master
 listOfSelectedLayers = thisFont.selectedLayers # active layers of selected glyphs
 
+def baseHasAnchor( thisComponent, masterID, anchorToLookFor ):
+	baseGlyph = thisComponent.component
+	baseLayer = baseGlyph.layers[masterID]
+	baseAnchors = [a for a in baseLayer.anchors]
+	anchorIsInLayer = False
+	for i in range(len(baseAnchors)):
+		if baseAnchors[i].name == anchorToLookFor:
+			anchorIsInLayer = True
+	return anchorIsInLayer
+
 def process( thisLayer ):
 	thisGlyph = thisLayer.parent
 	compSet = thisLayer.componentNames()
-	for thatLayer in thisGlyph.layers:
-		if thatLayer != thisLayer: # don't sync the layer with itself
-			if thatLayer.layerId == thatLayer.associatedMasterId: # only sync master layers
-				thatLayer.setComponentNames_( compSet )
+	
+	# only act if components are present:
+	if compSet: 
+		
+		# see if there are special anchors (e.g., top_1, top_2, etc.) the components are attached to:
+		componentAnchors = [ None ]
+		numberOfComponents = len(compSet)
+		for i in range( 1, numberOfComponents ):
+			thisAnchor =  thisLayer.components[i].anchor()
+			componentAnchors.append( thisAnchor )
+	
+		# go through all other layers:
+		for thatLayer in thisGlyph.layers:
+			if thatLayer != thisLayer: # don't sync the layer with itself
+				thatLayerID = thatLayer.layerId
+				if thatLayerID == thatLayer.associatedMasterId: # only sync master layers
+					thatLayer.setComponentNames_( compSet ) # sync components
+					
+					# try to attach the newly synced components to the right anchors:
+					if len(thatLayer.components) == numberOfComponents:
+						baseComponent = thatLayer.components[0]
+						for i in range( 1, numberOfComponents ):
+							thisAnchor = componentAnchors[i]
+							if thisAnchor:
+								if baseHasAnchor( baseComponent, thatLayerID, thisAnchor ):
+									thatComponent = thatLayer.components[i]
+									thatComponent.setAnchor_( thisAnchor )
 
 thisFont.disableUpdateInterface() # suppresses UI updates in Font View
 
