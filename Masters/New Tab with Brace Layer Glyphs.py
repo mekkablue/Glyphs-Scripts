e747bb955319e4a4ee140f47a0d5e90715ae3507
Rainer Erich Scheichelbauer <res@glyphsapp.com>
2015-06-05 12:26:01 +0200

    New script: New Tab with Brace Layer Glyphs

---
+++ Masters/New Tab with Brace Layer Glyphs.py
@@ -0,0 +1,16 @@
+#MenuTitle: New Tab with Brace Layer Glyphs
+# -*- coding: utf-8 -*-
+__doc__="""
+Opens a new Edit tab with all glyphs which contain the Brace Layer trick.
+"""
+
+import GlyphsApp
+from PyObjCTools.AppHelper import callAfter
+
+Font = Glyphs.font
+editString = ""
+for thisGlyph in Font.glyphs:
+	if "{" in "".join([ l.name for l in thisGlyph.layers ]):
+		editString += ( "/" + thisGlyph.name )
+
+callAfter( Glyphs.currentDocument.windowController().addTabWithString_, editString )
