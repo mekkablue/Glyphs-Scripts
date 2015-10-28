7f4cf8e34b15ccbd2542f5ea3df996919641c6bf
Rainer Erich Scheichelbauer <res@glyphsapp.com>
2015-10-28 20:27:40 +0100

    Deletes images on all layers of the glyph; improved log output.

--- Images/Delete Images.py
+++ Images/Delete Images.py
@@ -1,23 +1,34 @@
 #MenuTitle: Remove Images
 # -*- coding: utf-8 -*-
 __doc__="""
-Deletes placed images from selected glyphs.
+Deletes placed images from selected glyphs on all layers.
 """
 
 import GlyphsApp
 Font = Glyphs.font
 selectedLayers = Font.selectedLayers
 
-def process( thisLayer ):
-	thisLayer.setBackgroundImage_(None)
+def process( thisGlyph ):
+	deleteCount = 0
+	for thisLayer in thisGlyph.layers:
+		if thisLayer.backgroundImage:
+			thisLayer.setBackgroundImage_(None)
+			deleteCount += 1
+	return deleteCount
 
 Font.disableUpdateInterface()
 
+print "Removing images in %s selected glyphs ..." % len( selectedLayers )
+
 for thisLayer in selectedLayers:
 	thisGlyph = thisLayer.parent
-	print "Deleting image in", thisGlyph.name
 	thisGlyph.beginUndo()
-	process( thisLayer )
+	numberOfDeletedImages = process( thisGlyph )
+	if numberOfDeletedImages:
+		plural = 0
+		if numberOfDeletedImages > 1:
+			plural = 1
+		print "  Deleted %i image%s in %s." % ( numberOfDeletedImages, "s"*plural, thisGlyph.name )
 	thisGlyph.endUndo()
 
 Font.enableUpdateInterface()
