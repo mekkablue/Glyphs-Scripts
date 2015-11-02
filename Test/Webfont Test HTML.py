857d325bcf580b829ecf6bb701b4f368da890a60
Rainer Erich Scheichelbauer <res@glyphsapp.com>
2015-11-02 12:32:23 +0100

    Supports kern, liga, calt; waterfall for small sizes

--- Test/Webfont Test HTML.py
+++ Test/Webfont Test HTML.py
@@ -85,27 +85,79 @@ htmlContent = """<head>
 	<style type="text/css" media="screen">
 		<!-- fontFaces -->
 		
-		body { font-family: "nameOfTheFont"; }
+		body { 
+			font-family: "nameOfTheFont"; 
+			font-feature-settings: "kern" on, "liga" on, "calt" on;
+			-moz-font-feature-settings: "kern" on, "liga" on, "calt" on;
+			-webkit-font-feature-settings: "kern" on, "liga" on, "calt" on;
+			-ms-font-feature-settings: "kern" on, "liga" on, "calt" on;
+			-o-font-feature-settings: "kern" on, "liga" on, "calt" on;
+		}
 		p { padding: 5px; margin: 10px; }
-		p#smallParagraph { font-size: small; }
-		p#largeParagraph { font-size: x-large; }
+		p#p08 { font-size: 08pt; }
+		p#p09 { font-size: 09pt; }
+		p#p10 { font-size: 10pt; }
+		p#p11 { font-size: 11pt; }
+		p#p12 { font-size: 12pt; }
+		p#p13 { font-size: 13pt; }
+		p#p14 { font-size: 14pt; }
+		p#p15 { font-size: 15pt; }
+		p#p16 { font-size: 16pt; }
+		p#largeParagraph { font-size: 32pt; }
 		p#veryLargeParagraph { font-size: 100pt; }		
 	</style>
 	<script type="text/javascript">
 		function updateParagraph() {
 			var txt = document.getElementById('textInput');
-			var p1 = document.getElementById('smallParagraph');
-			var p2 = document.getElementById('largeParagraph');
-			var p3 = document.getElementById('veryLargeParagraph');
-			p1.textContent = txt.value;
-			p2.textContent = txt.value;
-			p3.textContent = txt.value;			
+			
+			var p08 = document.getElementById('p08');
+			var p09 = document.getElementById('p09');
+			var p10 = document.getElementById('p10');
+			var p11 = document.getElementById('p11');
+			var p12 = document.getElementById('p12');
+			var p13 = document.getElementById('p13');
+			var p14 = document.getElementById('p14');
+			var p15 = document.getElementById('p15');
+			var p16 = document.getElementById('p16');
+			var pLarge = document.getElementById('largeParagraph');
+			var pVeryLarge = document.getElementById('veryLargeParagraph');
+			
+			p08.textContent = txt.value;
+			p09.textContent = txt.value;
+			p10.textContent = txt.value;
+			p11.textContent = txt.value;
+			p12.textContent = txt.value;
+			p13.textContent = txt.value;
+			p14.textContent = txt.value;
+			p15.textContent = txt.value;
+			p16.textContent = txt.value;
+			pLarge.textContent = txt.value;
+			pVeryLarge.textContent = txt.value;
 		}
 		function changeFont() {
 			var selector = document.getElementById('fontFamilySelector');
 			var selected_index = selector.selectedIndex;
 			var selected_option_text = selector.options[selected_index].text;
 			document.getElementById('fontTestBody').style.fontFamily = selected_option_text;
+			return false;
+		}
+		function setDefaultText(defaultText) {
+			document.getElementById('textInput').value = decodeEntities(defaultText);
+			updateParagraph();
+			return false;
+		}
+		function setLat1() {
+			var lat1 = "abcdefghijklm nopqrstuvwxyz ABCDEFGHIJKLM NOPQRSTUVWXYZ &Agrave;&Aacute;&Acirc;&Atilde;&Auml;&Aring;&AElig;&Ccedil;&Egrave;&Eacute;&Ecirc;&Euml;&Igrave;&Iacute;&Icirc;&Iuml;&ETH;&Ntilde;&Ograve;&Oacute;&Ocirc;&Otilde;&Ouml;&Oslash;&OElig;&THORN;&Scaron;&Ugrave;&Uacute;&Ucirc;&Uuml;&Yacute;&Yuml;&#x17D; &agrave;&aacute;&acirc;&atilde;&auml;&aring;&aelig;&ccedil;&egrave;&eacute;&ecirc;&euml;&igrave;&iacute;&icirc;&iuml;&eth;&ntilde;&ograve;&oacute;&ocirc;&otilde;&ouml;&oslash;&oelig;&thorn;&scaron;&szlig;&ugrave;&uacute;&ucirc;&uuml;&yacute;&yuml;&#x17E; .,:;&middot;&hellip;&iquest;?&iexcl;!&laquo;&raquo;&lsaquo;&rsaquo; /|&brvbar;\\()[]{}_-&ndash;&mdash;&sbquo;&bdquo;&lsquo;&rsquo;&ldquo;&rdquo;&quot;&#x27; #&amp;&sect;@&bull;&shy;*&dagger;&Dagger;&para; +&times;&divide;&plusmn;=&lt;&gt;&not;&mu; ^~&acute;`&circ;&macr;&tilde;&uml;&cedil; &yen;&euro;&pound;$&cent;&curren;&fnof; &trade;&reg;&copy; 1234567890 &ordf;&ordm;&deg;%&permil; &sup1;&sup2;&sup3;&frac14;&frac12;&frac34;";
+			return setDefaultText(lat1);
+		}
+		function setCharset() {
+			var completeCharSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+			return setDefaultText(completeCharSet);
+		}
+		function decodeEntities(string){
+			var elem = document.createElement('div');
+			elem.innerHTML = string;
+			return elem.textContent;
 		}
 	</script>
 </head>
@@ -114,7 +166,22 @@ htmlContent = """<head>
 		<!-- moreOptions -->
 	</select>
 	<input type="text" value="Type Text Here." id="textInput" onkeyup="updateParagraph()" size="80" />
-	<p id="smallParagraph">ABCDEFGHIJKLMNOPQRSTUVWXYZ</p>
+	<p>
+		<a href="javascript:setCharset();">Charset</a>
+		<a href="javascript:setLat1();">Lat1</a>
+		<a href="http://stateofwebtype.com/#eot">eot?</a>
+		<a href="http://stateofwebtype.com/#woff">woff?</a>
+		<a href="http://stateofwebtype.com/#woff2">woff2?</a>
+	</p>
+	<p id="p08">08pt ABCDEFGHIJKLMNOPQRSTUVWXYZ</p>
+	<p id="p09">09pt ABCDEFGHIJKLMNOPQRSTUVWXYZ</p>
+	<p id="p10">10pt ABCDEFGHIJKLMNOPQRSTUVWXYZ</p>
+	<p id="p11">11pt ABCDEFGHIJKLMNOPQRSTUVWXYZ</p>
+	<p id="p12">12pt ABCDEFGHIJKLMNOPQRSTUVWXYZ</p>
+	<p id="p13">13pt ABCDEFGHIJKLMNOPQRSTUVWXYZ</p>
+	<p id="p14">14pt ABCDEFGHIJKLMNOPQRSTUVWXYZ</p>
+	<p id="p15">15pt ABCDEFGHIJKLMNOPQRSTUVWXYZ</p>
+	<p id="p16">16pt ABCDEFGHIJKLMNOPQRSTUVWXYZ</p>
 	<p id="largeParagraph">ABCDEFGHIJKLMNOPQRSTUVWXYZ</p>
 	<p id="veryLargeParagraph">ABCDEFGHIJKLMNOPQRSTUVWXYZ</p>
 </body>
