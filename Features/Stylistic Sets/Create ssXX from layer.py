b845fae79d37043fc778d3a34a99eda62a643d7a
Rainer Erich Scheichelbauer <res@glyphsapp.com>
2015-11-25 19:30:21 +0100

    Also copies other master layers (thx @AndreiRobu)
    … also marks the new glyphs blue, marks unprocessed layers orange, and
    adds it to the Edit tab.

--- Features/Stylistic Sets/Create ssXX from layer.py
+++ Features/Stylistic Sets/Create ssXX from layer.py
@@ -1,7 +1,7 @@
 #MenuTitle: Create .ssXX glyph from current layer
 # -*- coding: utf-8 -*-
 __doc__="""
-Takes the currently opened layers and creates new glyphs with a .ssXX ending. Checks if the name is free.
+Takes the currently opened layers and creates new glyphs with the first available .ssXX ending. It marks the new glyph blue, and (in a multiple-master file) all other, unprocessed master layers orange.
 """
 
 import GlyphsApp
@@ -25,18 +25,29 @@ def findSuffix( glyphName ):
 	
 def process( sourceLayer ):
 	# find suffix
-	sourceGlyphName = sourceLayer.parent.name
+	sourceGlyph = sourceLayer.parent
+	sourceGlyphName = sourceGlyph.name
 	targetSuffix = findSuffix( sourceGlyphName )
 	
 	# append suffix, create glyph:
 	targetGlyphName = sourceGlyphName + targetSuffix
 	targetGlyph = GSGlyph( targetGlyphName )
 	thisFont.glyphs.append( targetGlyph )
+	targetGlyph.setColorIndex_(6)
 
 	# copy original layer into respective master of new glyph:
-	masterID = sourceLayer.associatedMasterId
+	sourceMasterID = sourceLayer.associatedMasterId
 	layerCopy = sourceLayer.copy()
-	targetGlyph.layers[ masterID ] = layerCopy
+	targetGlyph.layers[ sourceMasterID ] = layerCopy
+	
+	# copy other master layers into target layers:
+	for thisMasterID in [m.id for m in thisFont.masters if m.id != sourceMasterID]:
+		targetGlyph.layers[ thisMasterID ] = sourceGlyph.layers[ thisMasterID ].copy()
+		targetGlyph.layers[ thisMasterID ].setColorIndex_(1)
+	
+	# add new glyph to tab:
+	thisFont.currentText += "/%s" % targetGlyphName
+	
 	print "Created %s" % targetGlyphName
 
 for thisLayer in selectedLayers:
