4b1baaca12f143022b9470ee376016cc798d676d
Rainer Erich Scheichelbauer <res@glyphsapp.com>
2016-08-12 15:41:17 +0200

    Opens only relevant master layers and bracket/brace layers in tab.
    Still reports all layers in Macro Window.

--- Paths/New Tab with Path Problems.py
+++ Paths/New Tab with Path Problems.py
@@ -1,10 +1,8 @@
 #MenuTitle: New Tab with Path Problems
 # -*- coding: utf-8 -*-
 __doc__="""
-Opens a new tab with glyphs containing zero handles, bad outline orders or path directions.
+Opens a new tab with glyphs containing zero handles, bad outline orders or path directions. Only opens master layers in tabs. Full layer report (including non-master layers) in the Macro Window log.
 """
-
-import GlyphsApp
 
 thisFont = Glyphs.font # frontmost font
 
@@ -36,6 +34,11 @@ def reportString( myList, myMsg ):
 	else:
 		return ""
 
+def layerIsMasterLayer(thisLayer):
+	isMaster = thisLayer.associatedMasterId == thisLayer.layerId
+	isSpecial = "[" in thisLayer.name or "{" in thisLayer.name
+	return isMaster and isSpecial
+	
 zeroHandles = []
 outlineOrder = []
 
@@ -48,11 +51,13 @@ for thisGlyph in thisFont.glyphs:
 	glyphName = thisGlyph.name
 	for thisLayer in thisGlyph.layers:
 		if hasZeroHandles( thisLayer ):
-			print "%s: found zero handle on Layer '%s'." % ( glyphName, thisLayer.name )
-			zeroHandles.append( glyphName )
+			print "%s: zero handle on Layer '%s'." % ( glyphName, thisLayer.name )
+			if layerIsMasterLayer( thisLayer ):
+				zeroHandles.append( glyphName )
 		if hasBadOutlineOrder( thisLayer ):
-			print "%s: found bad path direction on Layer '%s'." % ( glyphName, thisLayer.name )
-			outlineOrder.append( glyphName )
+			print "%s: bad path direction on Layer '%s'." % ( glyphName, thisLayer.name )
+			if layerIsMasterLayer( thisLayer)
+				outlineOrder.append( glyphName )
 
 tabString = reportString(zeroHandles, "Zero Handles") + reportString(outlineOrder, "Outline Order and Orientation")
 
