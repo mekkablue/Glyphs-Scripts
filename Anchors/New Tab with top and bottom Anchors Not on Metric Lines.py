c495e4cda20bb45e55e23b070ec29767a635b877
Rainer Erich Scheichelbauer <res@glyphsapp.com>
2016-09-08 16:56:29 +0200

    Rewrite of the "Report top anchors" script.
    Now includes bottom anchors too, and it opens tabs.

---
+++ Anchors/New Tab with top and bottom Anchors Not on Metric Lines.py
@@ -0,0 +1,55 @@
+#MenuTitle: New Tab with top and bottom Anchors Not on Metric Lines
+# -*- coding: utf-8 -*-
+__doc__="""
+Reports the y positions of top and bottom anchors to the Macro Window if the anchors are not on a metric line (baseline, x-height, etc.).
+"""
+
+import GlyphsApp
+
+thisFont = Glyphs.font
+
+Glyphs.clearLog()
+# Glyphs.showMacroWindow()
+
+def isAnchorOnMetricLine( thisLayer, thisAnchorName ):
+	try:
+		myMaster = thisLayer.associatedFontMaster()
+		myY = thisLayer.anchors[ thisAnchorName ].y
+		if myY in [0.0, myMaster.xHeight, myMaster.descender, myMaster.ascender, myMaster.capHeight]:
+			return False
+		else:
+			print "%s ---> %s @ %s" % (thisLayer.parent.name, thisAnchorName, myY)
+			return True
+	except Exception, e:
+		return False
+
+anchorsToLookFor = ("top","bottom")
+collectedGlyphNames = {}
+for thisAnchor in anchorsToLookFor:
+	collectedGlyphNames[thisAnchor] = []
+
+for thisGlyph in thisFont.glyphs:
+	for thisAnchor in anchorsToLookFor:
+		for thisVeryLayer in [l for l in thisGlyph.layers 
+			if "{" in l.name # brace layer
+			or "[" in l.name # bracket layer
+			or l.layerId == l.associatedFontMaster().id # master layer
+		]:
+			if isAnchorOnMetricLine( thisVeryLayer, thisAnchor ):
+				collectedGlyphNames[thisAnchor].append(thisGlyph.name)
+
+offAnchorsFound = False
+for thisAnchor in anchorsToLookFor:
+	glyphNames = collectedGlyphNames[thisAnchor]
+	# opens new Edit tab:
+	if glyphNames:
+		offAnchorsFound = True
+		reporttext = "Anchor '%s' off metrics:\n/%s\n" % (thisAnchor, "/".join(glyphNames))
+		print "\n", reporttext # just in case opening a tab fails
+		thisFont.newTab( reporttext )
+
+if not offAnchorsFound:
+	reporttext = "Could not find %s anchors off metric lines on master layers." % " or ".join(anchorsToLookFor)
+	print reporttext # just in case opening a tab fails
+	Message("All is good", reporttext, OKButton="Cool")
+	
