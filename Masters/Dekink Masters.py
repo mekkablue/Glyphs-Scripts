6289e6ddbf32b85b003ce2ccb3bf19d62957550b
Rainer Erich Scheichelbauer <res@glyphsapp.com>
2016-09-15 11:11:17 +0200

    Dekink now also works for Brace/Bracket layers

--- Masters/Dekink Masters.py
+++ Masters/Dekink Masters.py
@@ -7,60 +7,66 @@ Synchronize node distance proportions for angled smooth connections through all 
 import GlyphsApp
 
 Font = Glyphs.font
-currentMaster = Font.selectedFontMaster
 currentLayer = Font.selectedLayers[0]
 currentGlyph = currentLayer.parent
 
-def dekink( myMaster, compareString, myGlyph, pathIndex, nodeIndex, proportion ):
+def dekink( originLayer, compatibleLayerIDs, pathIndex, nodeIndex, proportion ):
 	try:
-		for thisLayer in myGlyph.layers:
-			if compareString == thisLayer.compareString() and thisLayer != myGlyph.layers[myMaster.id]:
-				thisPath = thisLayer.paths[pathIndex]
-				pathLength = len(thisPath)
-				n0 = thisPath.nodes[ (nodeIndex-1) % pathLength ]
-				n1 = thisPath.nodes[  nodeIndex ]
-				n2 = thisPath.nodes[ (nodeIndex+1) % pathLength ]
-				n1.x = n0.x + proportion * (n2.x - n0.x)
-				n1.y = n0.y + proportion * (n2.y - n0.y)
+		myGlyph = originLayer.parent
+		for thisID in compatibleLayerIDs:
+			thisLayer = myGlyph.layers[thisID]
+			thisPath = thisLayer.paths[pathIndex]
+			pathLength = len(thisPath)
+			n0 = thisPath.nodes[ (nodeIndex-1) % pathLength ]
+			n1 = thisPath.nodes[  nodeIndex ]
+			n2 = thisPath.nodes[ (nodeIndex+1) % pathLength ]
+			n1.x = n0.x + proportion * (n2.x - n0.x)
+			n1.y = n0.y + proportion * (n2.y - n0.y)
 		return True
-		
 	except Exception, e:
 		print e
+		import traceback
+		print traceback.format_exc()
 		return False
 
-if currentGlyph.mastersCompatible:
-	try:
-		# until v2.1:
-		selection = currentLayer.selection()
-	except:
-		# since v2.2:
+layerIDs = []
+for subrunArray in currentGlyph.layerGroups_masters_error_( NSArray(Font.instances), Font.masters, None ):
+	subrun = tuple(subrunArray)
+	if currentLayer.layerId in subrun:
+		for ID in subrun:
+			if ID != currentLayer.layerId:
+				layerIDs.append(ID)
+
+if layerIDs:
+	if currentGlyph.mastersCompatible:
 		selection = currentLayer.selection
 	
-	s = list( selection )
-	currentCompareString = currentLayer.compareString()
+		s = list( selection )
+		currentCompareString = currentLayer.compareString()
 	
-	# find the indices for selected nodes:
-	for n in [n for n in s if n.connection == GSSMOOTH]:
-		for pi in range( len( currentLayer.paths )):
-			pathLength = len( currentLayer.paths[pi].nodes )
-			currentNodes = currentLayer.paths[pi].nodes
-			for ni in range( pathLength ):
-				if currentNodes[ni] == n:
-					n0 = currentNodes[(ni-1) % pathLength]
-					n1 = currentNodes[ ni ]
-					n2 = currentNodes[(ni+1) % pathLength]
+		# find the indices for selected nodes:
+		for n in [n for n in s if n.connection == GSSMOOTH]:
+			for pi in range( len( currentLayer.paths )):
+				pathLength = len( currentLayer.paths[pi].nodes )
+				currentNodes = currentLayer.paths[pi].nodes
+				for ni in range( pathLength ):
+					if currentNodes[ni] == n:
+						n0 = currentNodes[(ni-1) % pathLength]
+						n1 = currentNodes[ ni ]
+						n2 = currentNodes[(ni+1) % pathLength]
 				
-					longX  = n2.x - n0.x
-					longY  = n2.y - n0.y
-					shortX = n1.x - n0.x
-					shortY = n1.y - n0.y
+						longX  = n2.x - n0.x
+						longY  = n2.y - n0.y
+						shortX = n1.x - n0.x
+						shortY = n1.y - n0.y
 
-					if longY == 0.0 or longX == 0.0:
-						print "Ignoring node at %i, %i because the segment is straight." % ( n1.x, n1.y )
-					else:
-						proportion = ( shortX / longX + shortY / longY ) / 2.0
-					
-						if not dekink( currentMaster, currentCompareString, currentGlyph, pi, ni, proportion ):
-							print "Error: dekinking failed."
+						if longY == 0.0 or longX == 0.0:
+							print "Ignoring node at %i, %i because the segment is straight." % ( n1.x, n1.y )
+						else:
+							proportion = ( shortX / longX + shortY / longY ) / 2.0
+							if not dekink( currentLayer, layerIDs, pi, ni, proportion ):
+								Message("Dekink Error", "Error: dekinking failed.", OKButton=None)
+	else:
+		Message("Dekink Error", "Could not find compatible masters.", OKButton=None)
 else:
-	print "Error: masters not compatible."
+	Message("Dekink Error", "Could not find any other layers in this glyph for this interpolation.", OKButton=None)
