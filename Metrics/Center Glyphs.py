cd8379508f7924b97249401f664f4e0c893a2f24
Rainer Erich Scheichelbauer <res@glyphsapp.com>
2016-11-04 13:24:50 +0100

    Now handles background independently.

--- Metrics/Center Glyphs.py
+++ Metrics/Center Glyphs.py
@@ -4,19 +4,50 @@ __doc__="""
 Center all selected glyphs inside their respective widths.
 """
 
-import GlyphsApp
+import math
 Font = Glyphs.font
 selectedLayers = Font.selectedLayers
+
+def transform(shiftX=0.0, shiftY=0.0, rotate=0.0, skew=0.0, scale=1.0):
+	"""
+	Returns an NSAffineTransform object for transforming layers.
+	Apply an NSAffineTransform t object like this:
+		Layer.transform_checkForSelection_doComponents_(t,False,True)
+	Access its transformation matrix like this:
+		tMatrix = t.transformStruct() # returns the 6-float tuple
+	Apply the matrix tuple like this:
+		Layer.applyTransform(tMatrix)
+		Component.applyTransform(tMatrix)
+		Path.applyTransform(tMatrix)
+	Chain multiple NSAffineTransform objects t1, t2 like this:
+		t1.appendTransform_(t2)
+	"""
+	myTransform = NSAffineTransform.transform()
+	if rotate:
+		myTransform.rotateByDegrees_(rotate)
+	if scale != 1.0:
+		myTransform.scaleBy_(scale)
+	if not (shiftX == 0.0 and shiftY == 0.0):
+		myTransform.translateXBy_yBy_(shiftX,shiftY)
+	if skew:
+		skewStruct = NSAffineTransformStruct()
+		skewStruct.m11 = 1.0
+		skewStruct.m22 = 1.0
+		skewStruct.m21 = math.tan(math.radians(skew))
+		skewTransform = NSAffineTransform.transform()
+		skewTransform.setTransformStruct_(skewStruct)
+		myTransform.appendTransform_(skewTransform)
+	return myTransform
+
 
 Font.disableUpdateInterface()
 
 for thisLayer in selectedLayers:
-	thisGlyph = thisLayer.parent
-	thisGlyph.beginUndo()
-	oldWidth = thisLayer.width
-	thisLayer.LSB = ( thisLayer.LSB + thisLayer.RSB ) // 2
-	thisLayer.width = oldWidth
-	thisGlyph.endUndo()
+	center = thisLayer.width/2.0
+	centerOfObjects = thisLayer.bounds.origin.x + 0.5 * thisLayer.bounds.size.width
+	shift = center - centerOfObjects
+	shiftMatrix = transform(shiftX=shift).transformStruct()
+	thisLayer.applyTransform( shiftMatrix )
 
 Font.enableUpdateInterface()
 print "Centered: %s" % (", ".join( [ l.parent.name for l in selectedLayers ] ))
