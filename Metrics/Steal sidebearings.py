9e5737dd4ae763c26b30cf4441dbed3b1ddf3805
Rainer Erich Scheichelbauer <res@glyphsapp.com>
2017-02-13 02:31:25 +0100

    Frontmost font is target by default, small cleanups

--- Metrics/Steal sidebearings.py
+++ Metrics/Steal sidebearings.py
@@ -14,22 +14,22 @@ class MetricsCopy( object ):
 		self.listOfMasters = []
 		self.updateListOfMasters() 
 		
-		self.w = vanilla.FloatingWindow( (400, windowHeight), "Steal sidebearings", minSize=(350, windowHeight), maxSize=(650, windowHeight), autosaveName="com.mekkablue.MetricsCopy.mainwindow" )
+		self.w = vanilla.FloatingWindow( (400, windowHeight), "Steal Sidebearings", minSize=(350, windowHeight), maxSize=(650, windowHeight), autosaveName="com.mekkablue.MetricsCopy.mainwindow" )
 		
 		self.w.text_anchor = vanilla.TextBox( (15, 12+2, 130, 17), "Copy metrics from:", sizeStyle='small')
-		self.w.from_font = vanilla.PopUpButton( (150, 12, -15, 17), self.listOfMasterNames(), sizeStyle='small', callback=self.buttonCheck)
+		self.w.from_font = vanilla.PopUpButton( (150, 12, -15, 17), self.listOfMasterNames()[::-1], sizeStyle='small', callback=self.buttonCheck)
 		
 		self.w.text_value = vanilla.TextBox( (15, 12+2+25, 130, 17), "To selected glyphs in:", sizeStyle='small')
-		self.w.to_font = vanilla.PopUpButton( (150, 12+25, -15, 17), self.listOfMasterNames()[::-1], sizeStyle='small', callback=self.buttonCheck)
+		self.w.to_font = vanilla.PopUpButton( (150, 12+25, -15, 17), self.listOfMasterNames(), sizeStyle='small', callback=self.buttonCheck)
 		
 		self.w.lsb   = vanilla.CheckBox( ( 17, 12+50, 80, 20), "LSB", value=True, callback=self.buttonCheck, sizeStyle='small' )
 		self.w.rsb   = vanilla.CheckBox( ( 97, 12+50, 80, 20), "RSB", value=True, callback=self.buttonCheck, sizeStyle='small' )
 		self.w.width = vanilla.CheckBox( (177, 12+50, 80, 20), "Width", value=False, callback=self.buttonCheck, sizeStyle='small' )
 		
-		self.w.ignoreSuffixes  = vanilla.CheckBox( (15+2, 12+75, -15, 20), "Ignore dotsuffix:", value=False, sizeStyle='small', callback=self.buttonCheck )
+		self.w.ignoreSuffixes  = vanilla.CheckBox( (15+2, 12+75, 110, 20), "Ignore dotsuffix:", value=False, sizeStyle='small', callback=self.buttonCheck )
 		self.w.suffixToBeIgnored = vanilla.EditText( (150, 12+75, -15, 20), ".alt", sizeStyle = 'small')
 
-		self.w.keepWindowOpen  = vanilla.CheckBox( (15+2, 12+100, -15, 20), "Keep this window open", value=False, sizeStyle='small' )
+		self.w.keepWindowOpen  = vanilla.CheckBox( (15+2, 12+100, 150, 20), "Keep this window open", value=False, sizeStyle='small' )
 		
 		self.w.copybutton = vanilla.Button((-80, -32, -15, 17), "Copy", sizeStyle='small', callback=self.copyMetrics)
 		self.w.setDefaultButton( self.w.copybutton )
@@ -202,7 +202,6 @@ class MetricsCopy( object ):
 						thisLayer.transform_checkForSelection_doComponents_(shiftTransform,False,True)
 				elif rsbIsSet:
 					thisLayer.setRSB_( sourceLayer.RSB )
-						
 
 				print "     %i <- %s -> %i (w: %i)" % ( thisLayer.LSB, glyphName, thisLayer.RSB, thisLayer.width )
 			except Exception, e:
