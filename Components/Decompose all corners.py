b300790b83c8cdb2c6c023047013c6fd1365812b
Rainer Erich Scheichelbauer <mekka@mekkablue.com>
2017-03-20 11:02:17 +0100

    Better reporting

--- Components/Decompose all corners.py
+++ Components/Decompose all corners.py
@@ -1,18 +1,19 @@
-#MenuTitle: Decompose all corners
+#MenuTitle: Decompose Corners and Caps
 # -*- coding: utf-8 -*-
 __doc__="""
-Decomposes all corners.
+Decomposes all corner and cap components in selected glyphs. Reports to Macro Window.
 """
-
-import GlyphsApp
 
 thisFont = Glyphs.font
 selectedLayers = thisFont.selectedLayers
 
 def process( thisLayer ):
-	thisLayer.decomposeCorners()
-	print "-- Decomposed all corners"
+	count = len([h for h in Layer.hints if h.type in (CORNER,CAP) ])
+	if count:
+		thisLayer.decomposeCorners()
+		print "-- Decomposed %i caps and/or corners" % count
 
+Glyphs.clearLog()
 for thisLayer in selectedLayers:
 	thisGlyph = thisLayer.parent
 	print "Processing", thisGlyph.name
