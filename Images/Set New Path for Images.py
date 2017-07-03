2061f6f5f8af2b90dab9a4786f5b6bd93db823fd
Rainer Erich Scheichelbauer <res@glyphsapp.com>
2017-07-03 18:09:22 +0200

    Catch user canceling the dialog

--- Images/Set New Path for Images.py
+++ Images/Set New Path for Images.py
@@ -7,15 +7,6 @@ Resets the path for placed images in selected glyphs. Useful if you have moved y
 import GlyphsApp
 import os
 
-Font = Glyphs.font
-FontMaster = Font.selectedFontMaster
-selectedLayers = Font.selectedLayers
-newFolder = GetFolder( message="Choose location of placed images:", allowsMultipleSelection = False )
-
-Glyphs.clearLog()
-Glyphs.showMacroWindow()
-print "New image path for selected glyphs:\n%s" % newFolder
-
 def process( thisLayer ):
 	try:
 		thisImage = thisLayer.backgroundImage
@@ -30,12 +21,21 @@ def process( thisLayer ):
 	
 	return "new path %s" % thisImageNewFullPath
 
+Font = Glyphs.font
+FontMaster = Font.selectedFontMaster
+selectedLayers = Font.selectedLayers
+newFolder = GetFolder( message="Choose location of placed images:", allowsMultipleSelection = False )
+
+Glyphs.clearLog()
+Glyphs.showMacroWindow()
 Font.disableUpdateInterface()
 
-for thisLayer in selectedLayers:
-	thisGlyph = thisLayer.parent
-	thisGlyph.beginUndo()
-	print "-- %s: %s" % ( thisGlyph.name, process( thisLayer ) )
-	thisGlyph.endUndo()
+if newFolder:
+	print "New image path for selected glyphs:\n%s" % newFolder
+	for thisLayer in selectedLayers:
+		thisGlyph = thisLayer.parent
+		thisGlyph.beginUndo()
+		print "-- %s: %s" % ( thisGlyph.name, process( thisLayer ) )
+		thisGlyph.endUndo()
 
 Font.enableUpdateInterface()
