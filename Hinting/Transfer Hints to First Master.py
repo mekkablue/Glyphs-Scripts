682def860b0ccccc0c8f517b2faf55b6b1d5ed4a
Rainer Erich Scheichelbauer <res@glyphsapp.com>
2019-02-20 21:25:32 +0100

    Update for TT hint types

--- Hinting/Transfer Hints to First Master.py
+++ Hinting/Transfer Hints to First Master.py
@@ -4,13 +4,13 @@ __doc__="""
 Moves PostScript (stem and ghost) hints from the current layer to the first master layer, provided the paths are compatible.
 """
 
-from GlyphsApp import TOPGHOST, BOTTOMGHOST, STEM
+from GlyphsApp import TOPGHOST, BOTTOMGHOST, STEM, TTANCHOR, TTSTEM, TTALIGN, TTINTERPOLATE, TTDIAGONAL, TTDELTA
 
 thisFont = Glyphs.font # frontmost font
 selectedLayers = thisFont.selectedLayers # active layers of selected glyphs
 firstMaster = thisFont.masters[0]
 firstMasterId = firstMaster.id
-supportedHintTypes = (TOPGHOST, BOTTOMGHOST, STEM)
+supportedHintTypes = (TOPGHOST, BOTTOMGHOST, STEM, TTANCHOR, TTSTEM, TTALIGN, TTINTERPOLATE, TTDIAGONAL, TTDELTA, )
 
 def deleteHintsOnLayer(thisLayer):
 	for i in range(len(thisLayer.hints))[::-1]:
@@ -18,22 +18,43 @@ def deleteHintsOnLayer(thisLayer):
 			del thisLayer.hints[i]
 
 def transferHintsFromTo( sourceLayer, targetLayer ):
-	# empty put targetLayer's hints:
+	# clean slate in targetLayer:
 	deleteHintsOnLayer(targetLayer)
+	
+	# go through all hints in source layer:
 	for thisHint in sourceLayer.hints:
+		
+		# if it is a recognized hint type...
 		if thisHint.type in supportedHintTypes and thisHint.originNode:
+			
+			# ... create hint for target layer:
 			pathIndex = sourceLayer.paths.index(thisHint.originNode.parent)
 			originNodeIndex = thisHint.originNode.index
 			newHint = GSHint()
 			newHint.type = thisHint.type
 			newHint.originNode = targetLayer.paths[pathIndex].nodes[originNodeIndex]
+			newHint.horizontal = thisHint.horizontal
+			
+			# ... look for optional nodes:
 			if thisHint.targetNode:
-				# stem hint has target node:
 				targetNodeIndex = thisHint.targetNode.index
 				targetPathIndex = sourceLayer.paths.index(thisHint.targetNode.parent)
 				newHint.targetNode = targetLayer.paths[targetPathIndex].nodes[targetNodeIndex]
-				newHint.horizontal = thisHint.horizontal
+				
+			if thisHint.otherNode1:
+				targetNodeIndex = thisHint.otherNode1.index
+				targetPathIndex = sourceLayer.paths.index(thisHint.otherNode1.parent)
+				newHint.otherNode1 = targetLayer.paths[targetPathIndex].nodes[targetNodeIndex]
+				
+			if thisHint.otherNode2:
+				targetNodeIndex = thisHint.otherNode2.index
+				targetPathIndex = sourceLayer.paths.index(thisHint.otherNode2.parent)
+				newHint.otherNode2 = targetLayer.paths[targetPathIndex].nodes[targetNodeIndex]
+			
+			# ... and add to target layer:
 			targetLayer.hints.append(newHint)
+		
+	# ... delete hints in source layer:
 	deleteHintsOnLayer(sourceLayer)
 
 thisFont.disableUpdateInterface() # suppresses UI updates in Font View
