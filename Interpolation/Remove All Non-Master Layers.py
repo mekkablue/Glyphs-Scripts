28dce417b9a748050da2188a69bb47df82c58fd9
Rainer Scheichelbauer <res@glyphsapp.com>
2019-07-27 23:42:45 +0200

    Update

--- Interpolation/Remove All Non-Master Layers.py
+++ Interpolation/Remove All Non-Master Layers.py
@@ -14,36 +14,26 @@ def process( thisGlyph ):
 	numberOfLayers = len( thisGlyph.layers )
 	for i in range( numberOfLayers )[::-1]:
 		thisLayer = thisGlyph.layers[i]
-		if thisLayer.layerId != thisLayer.associatedMasterId: # not the master layer
-			thisLayerName = thisLayer.name
+		if not thisLayer.isMasterLayer and not thisLayer.isSpecialLayer:
 			thisLayerShouldBeRemoved = True
-			if thisLayerName: # always delete unnamed layers
-				for parentheses in searchTerms:
-					opening = parentheses[0]
-					closing = parentheses[1]
-					
-					# check if ONE of them is at the END of the layer name, like:
-					# Bold [160], Bold [160[, Bold ]160], Regular {120}
-					if thisLayerName.endswith(opening) or thisLayerName.endswith(closing):
-						thisLayerShouldBeRemoved = False
-						
 			if thisLayerShouldBeRemoved:
 				count += 1
 				del thisGlyph.layers[i]
-			
 	return count
 
-Font.disableUpdateInterface()
-
+excludedGlyphNameBeginnings = ("_smart","_part")
 for thisLayer in selectedLayers:
 	thisGlyph = thisLayer.parent
 	thisGlyphName = thisGlyph.name
-
-	if str(thisGlyphName)[:7] != "_smart.":
+	
+	nameIsAnException = False
+	for prefix in excludedGlyphNameBeginnings:
+		if thisGlyphName.startswith(prefix):
+			nameIsAnException = True
+	
+	if not nameIsAnException:
 		thisGlyph.beginUndo()
 		print "%s layers deleted in %s." % ( process( thisGlyph ), thisGlyphName )
 		thisGlyph.endUndo()
 	else:
 		print "Smart layers kept in %s." % ( thisGlyphName )
-
-Font.enableUpdateInterface()
