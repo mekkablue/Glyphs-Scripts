ddc2dc2c96158f354a92439dddba8a60bfe06a37
Rainer Scheichelbauer <res@glyphsapp.com>
2019-07-27 18:46:38 +0200

    Merge Schriftlabor scripts

---
+++ Masters/New Tab with Special Layers.py
@@ -0,0 +1,28 @@
+#MenuTitle: New Tab with Special Layers
+# -*- coding: utf-8 -*-
+__doc__="""
+Opens a new Edit tab containing all special (bracket & brace) layers.
+"""
+
+
+Glyphs.clearLog() # clears log of Macro window
+thisFont = Glyphs.font # frontmost font
+affectedLayers = []
+for thisGlyph in thisFont.glyphs: # loop through all glyphs
+	for thisLayer in thisGlyph.layers: # loop through all layers
+		# collect affected layers:
+		if thisLayer.isSpecialLayer: 
+			affectedLayers.append(thisLayer)
+
+# open a new tab with the affected layers:
+if affectedLayers:
+	newTab = thisFont.newTab()
+	newTab.layers = affectedLayers
+# otherwise send a message:
+else:
+	Message(
+		title = "Nothing Found",
+		message = "Could not find any bracket or brace layers in the font.",
+		OKButton = None
+	)
+	
