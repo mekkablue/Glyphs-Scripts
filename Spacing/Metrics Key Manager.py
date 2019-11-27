08fcd94e1b12550a2ef76400c66dd0c61803afff
Rainer Scheichelbauer <res@glyphsapp.com>
2019-11-27 13:29:04 +0100

    More legible font & scroll bars

--- Spacing/Metrics Key Manager.py
+++ Spacing/Metrics Key Manager.py
@@ -45,14 +45,19 @@ class MetricsKeyManager( object ):
 		linePos, inset, lineHeight, boxHeight = self.getMeasurements()
 		
 		self.w.LeftMetricsKeysText = vanilla.TextBox( (inset, linePos+2, 70, 14), u"Left Keys:", sizeStyle='small', selectable=True )
-		self.w.LeftMetricsKeys = vanilla.EditText( (inset+70, linePos, -inset, boxHeight), "", callback=self.SavePreferences, sizeStyle='small' )
+		self.w.LeftMetricsKeys = vanilla.TextEditor( (inset+70, linePos, -inset, boxHeight), "", callback=self.SavePreferences) #, sizeStyle='small' )
 
 		linePos += boxHeight + 10
 		self.w.RightMetricsKeysText = vanilla.TextBox( (inset, linePos+2, 70, 14), u"Right Keys:", sizeStyle='small', selectable=True )
-		self.w.RightMetricsKeys = vanilla.EditText( (inset+70, linePos, -inset, boxHeight), "", callback=self.SavePreferences, sizeStyle='small' )
+		self.w.RightMetricsKeys = vanilla.TextEditor( (inset+70, linePos, -inset, boxHeight), "", callback=self.SavePreferences) #, sizeStyle='small' )
+		
+		editFont = NSFont.legibileFontOfSize_(NSFont.smallSystemFontSize())
 		
 		for editField in (self.w.LeftMetricsKeys, self.w.RightMetricsKeys):
-			editField.getNSTextField().setToolTip_(u"Enter a metrics key like '=H', followed by a colon (:), followed by glyph names, spearated by space, comma, or any other separator that cannot be part of a glyph name. (Glyph names can contain A-Z, a-z, 0-9, period, underscore and hyphen.)\nExample: ‘=H: B D E F’.")
+			editField.getNSTextView().setToolTip_(u"Enter a metrics key like '=H', followed by a colon (:), followed by glyph names, spearated by space, comma, or any other separator that cannot be part of a glyph name. (Glyph names can contain A-Z, a-z, 0-9, period, underscore and hyphen.)\nExample: ‘=H: B D E F’.")
+			editField.getNSTextView().setFont_(editFont)
+			editField.getNSScrollView().setHasVerticalScroller_(1)
+			editField.getNSScrollView().setRulersVisible_(1)
 		
 		# Buttons:
 		self.w.resetButton = vanilla.Button( (-280-inset, -20-inset, -inset-190, -inset), u"⟲ Reset", sizeStyle='regular', callback=self.SetDefaults )
