01c9e84a0cd6d1308a51ade85ecac9dfe9d4a069
Rainer Scheichelbauer <res@glyphsapp.com>
2019-12-04 13:23:12 +0100

    ssXX names in tooltips

--- Test/Variable Font Test HTML.py
+++ Test/Variable Font Test HTML.py
@@ -113,11 +113,15 @@ def allUnicodeEscapesOfFont( thisFont ):
 
 def featureListForFont( thisFont ):
 	returnString = ""
-	featureList = [f.name for f in thisFont.features if not f.name in ("ccmp", "aalt", "locl", "kern", "calt", "liga", "clig") and not f.disabled()]
-	for f in featureList:
+	featureList = [(f.name, f.notes) for f in thisFont.features if not f.name in ("ccmp", "aalt", "locl", "kern", "calt", "liga", "clig") and not f.disabled()]
+	for (f,n) in featureList:
 		# <input type="checkbox" name="kern" id="kern" value="kern" class="otFeature" onchange="updateFeatures()" checked><label for="kern" class="otFeatureLabel">kern</label>
-
-		returnString += '\t\t\t<input type="checkbox" name="%s" id="%s" value="%s" class="otFeature" onchange="updateFeatures()"><label for="%s" class="otFeatureLabel">%s</label>\n' % (f,f,f,f,f)
+		if f.startswith("ss") and n.startswith("Name:"):
+			# stylistic set name:
+			setName = n.splitlines()[0][5:].strip()
+			returnString += '\t\t\t<input type="checkbox" name="%s" id="%s" value="%s" class="otFeature" onchange="updateFeatures()"><label for="%s" class="otFeatureLabel">%s<span class="tooltip">%s</span></label>\n' % (f,f,f,f,f,setName)
+		else:
+			returnString += '\t\t\t<input type="checkbox" name="%s" id="%s" value="%s" class="otFeature" onchange="updateFeatures()"><label for="%s" class="otFeatureLabel">%s</label>\n' % (f,f,f,f,f)
 	return returnString.rstrip()
 
 def allOTVarSliders(thisFont):
@@ -323,6 +327,20 @@ htmlContent = """
 			a {
 				color: #333;
 			}
+			
+			.otFeatureLabel .tooltip {
+				visibility: hidden;
+				background-color: #333;
+				color: #fff;
+				text-align: center;
+				padding: 0px 5px;
+				position: absolute;
+				z-index: 1;
+			}
+			.otFeatureLabel:hover .tooltip {
+				visibility: visible;
+			}
+			
 			
 			@media (prefers-color-scheme: dark) {
 				body { background: #000; }
