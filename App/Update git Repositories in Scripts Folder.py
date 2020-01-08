cd4ce0f70e9546e88bf67f65a95f4d1f38ef7be0
Rainer Scheichelbauer <res@glyphsapp.com>
2020-01-08 20:30:08 -0300

    proper reporting

--- App/Update git Repositories in Scripts Folder.py
+++ App/Update git Repositories in Scripts Folder.py
@@ -7,15 +7,12 @@ Executes a 'git pull' command on all subfolders in the Glyphs Scripts folder.
 
 from os import system, chdir, path
 
+Glyphs.clearLog()
 scriptsFolderPath = "~/Library/Application Support/Glyphs/Scripts/"
 chdir( path.expanduser(scriptsFolderPath) )
 result = system('find . -mindepth 1 -maxdepth 1 -type d -print -exec git -C {} pull -f \; -exec echo \;')
 
-# Output in Macro Window
-Glyphs.clearLog()
-print(result)
-
-if result.splitlines()[-1].strip()!="0":
+if result != 0:
 	Glyphs.showMacroWindow()
 else:
 	Glyphs.showNotification( 
