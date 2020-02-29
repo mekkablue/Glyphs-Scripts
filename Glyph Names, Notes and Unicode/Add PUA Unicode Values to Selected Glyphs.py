77fcc5426431cf845718fc9f857bed301588719e
Rainer Scheichelbauer <res@glyphsapp.com>
2020-02-29 18:25:50 +0000

    new option to keep existing unicode values

--- Glyph Names, Notes and Unicode/Add PUA Unicode Values to Selected Glyphs.py
+++ Glyph Names, Notes and Unicode/Add PUA Unicode Values to Selected Glyphs.py
@@ -10,13 +10,13 @@ import vanilla
 class CustomUnicode( object ):
 	def __init__( self ):
 		# Window 'self.w':
-		windowWidth  = 250
-		windowHeight = 95
+		windowWidth  = 300
+		windowHeight = 110
 		windowWidthResize  = 200 # user can resize width by this value
 		windowHeightResize = 0   # user can resize height by this value
 		self.w = vanilla.FloatingWindow(
 			( windowWidth, windowHeight ), # default window size
-			"Custom Unicodes for Selected Glyphs", # window title
+			"Add Unicode Values to Selected Glyphs", # window title
 			minSize = ( windowWidth, windowHeight ), # minimum size (for resizing)
 			maxSize = ( windowWidth + windowWidthResize, windowHeight + windowHeightResize ), # maximum size (for resizing)
 			autosaveName = "com.mekkablue.CustomUnicode.mainwindow" # stores last window position and size
@@ -24,16 +24,21 @@ class CustomUnicode( object ):
 		
 		# UI elements:
 		linePos, inset, lineHeight = 12, 15, 22
-		self.w.descriptionText = vanilla.TextBox( (inset, linePos+2, 155, 14), u"Unicode values starting at:", sizeStyle='small', selectable=True )
-		self.w.unicode = vanilla.EditText( (inset+155, linePos, -inset-25, 19), "E700", callback=self.sanitizeEntry, sizeStyle='small' )
+		self.w.descriptionText = vanilla.TextBox( (inset, linePos+3, 190, 14), u"Assign Unicode values starting at:", sizeStyle='small', selectable=True )
+		self.w.unicode = vanilla.EditText( (inset+190, linePos, -inset-25, 19), "E700", callback=self.sanitizeEntry, sizeStyle='small' )
 		self.w.unicode.getNSTextField().setToolTip_(u"The first selected glyph will receive this Unicode value. Subsequent glyphs will get the next respective Unicode value, until all selected glyphs have received one.")
 		self.w.updateButton = vanilla.SquareButton( (-inset-20, linePos, -inset, 18), u"↺", sizeStyle='small', callback=self.update )
 		self.w.updateButton.getNSButton().setToolTip_(u"Resets the starting Unicode to the first BMP PUA available in the font. Useful if you do not wish to overwrite existing PUA codes.")
 		linePos += lineHeight
+		self.w.keepExistingUnicodes = vanilla.CheckBox( (inset, linePos-1, -inset, 20), u"Keep existing Unicode values", value=False, callback=self.SavePreferences, sizeStyle='small' )
+		self.w.keepExistingUnicodes.getNSButton().setToolTip_(u"Two things: it keeps (does not overwrite) the Unicode value of a selected glyph, and it skips Unicode values that are already in use elsewhere in the font.")
+		linePos += lineHeight
 		
+		self.w.status = vanilla.TextBox( (inset, -16-inset, -inset, -inset), u"", sizeStyle='small', selectable=True )
+		linePos += lineHeight
 		
 		# Run Button:
-		self.w.runButton = vanilla.Button((-80-15, -20-15, -15, -15), "Apply", sizeStyle='regular', callback=self.CustomUnicodeMain )
+		self.w.runButton = vanilla.Button((-80-inset, -20-inset, -inset, -inset), "Assign", sizeStyle='regular', callback=self.CustomUnicodeMain )
 		self.w.setDefaultButton( self.w.runButton )
 		
 		# Load Settings:
@@ -55,18 +60,19 @@ class CustomUnicode( object ):
 	def SavePreferences( self, sender ):
 		try:
 			Glyphs.defaults["com.mekkablue.CustomUnicode.unicode"] = self.w.unicode.get()
+			Glyphs.defaults["com.mekkablue.CustomUnicode.keepExistingUnicodes"] = self.w.keepExistingUnicodes.get()
 		except:
 			return False
-			
 		return True
 
 	def LoadPreferences( self ):
 		try:
 			Glyphs.registerDefault("com.mekkablue.CustomUnicode.unicode", "E700")
+			Glyphs.registerDefault("com.mekkablue.CustomUnicode.keepExistingUnicodes", 1)
 			self.w.unicode.set( Glyphs.defaults["com.mekkablue.CustomUnicode.unicode"] )
+			self.w.keepExistingUnicodes.set( Glyphs.defaults["com.mekkablue.CustomUnicode.keepExistingUnicodes"] )
 		except:
 			return False
-			
 		return True
 	
 	def update(self, sender=None):
@@ -92,12 +98,21 @@ class CustomUnicode( object ):
 				allDigitsValid = False
 		
 		return allDigitsValid
+	
+	def statusMsg(self, msg):
+		print("  %s"%msg)
+		self.w.status.set(msg)
 		
 	def CustomUnicodeMain( self, sender ):
 		try:
+			# Try to save prefs:
+			if not self.SavePreferences( self ):
+				print("Note: 'Add PUA Unicode Values to Selected Glyphs' could not write preferences.")
+			
 			# just to be safe:
 			self.sanitizeEntry(self.w.unicode)
 			enteredUnicode = Glyphs.defaults["com.mekkablue.CustomUnicode.unicode"]
+			keepExistingUnicodes = bool(Glyphs.defaults["com.mekkablue.CustomUnicode.keepExistingUnicodes"])
 			
 			if self.checkUnicodeEntry(enteredUnicode):
 				thisFont = Glyphs.font # frontmost font
@@ -111,15 +126,22 @@ class CustomUnicode( object ):
 				# Apply Unicodes to selected glyphs:
 				for thisGlyph in listOfSelectedGlyphs:
 					unicodeValue = "%04X" % PUAcode
+					if keepExistingUnicodes:
+						if thisGlyph.unicode:
+							continue
+						while thisFont.glyphForUnicode_(unicodeValue):
+							self.statusMsg("%s: skipping (already in %s)" % (
+								unicodeValue,
+								thisFont.glyphForUnicode_(unicodeValue).name,
+								))
+							PUAcode += 1
+							unicodeValue = "%04X" % PUAcode
 					thisGlyph.setUnicode_( unicodeValue )
-					print("  %s: %s" % (unicodeValue, thisGlyph.name))
+					self.statusMsg("%s: %s" % (unicodeValue, thisGlyph.name))
 					PUAcode += 1
 				
-				# Try to save prefs:
-				if not self.SavePreferences( self ):
-					print("Note: 'Add PUA Unicode Values to Selected Glyphs' could not write preferences.")
-			
-				self.w.close() # closes window
+				self.statusMsg("Done.")
+				# self.w.close() # closes window
 			else:
 				Message(
 					message="The entered code is not a valid Unicode value. It must be either a four- or five-digit hexadecimal number. Find more details in the Macro Window.",
