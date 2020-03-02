4e8830d5d22bf6ed80c92695c257a8619f90b252
Rainer Scheichelbauer <res@glyphsapp.com>
2020-03-02 18:54:36 +0000

    Glyphs 3 Compatibility

--- Paths/Fill Up with Rectangles.py
+++ Paths/Fill Up with Rectangles.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import division, print_function, unicode_literals
 __doc__="""
-Inserts Rectangles in all empty, selected glyphs.
+Inserts Rectangles in all empty, selected glyphs. Verbose report in Macro Window.
 """
 
 Font = Glyphs.font
@@ -32,17 +32,42 @@ def drawRect( myBottomLeft, myTopRight ):
 
 def process( thisLayer ):
 	layerIsEmpty = ( len(thisLayer.paths) == 0 and len(thisLayer.components) == 0 )
+	thisGlyph = thisLayer.parent
+	try:
+		case = thisGlyph.case
+		if case == GSLowercase:
+			height = 500
+		elif case == GSUppercase:
+			height = 700
+		else:
+			height = 600
+	except:
+		subCategory = thisGlyph.subCategory
+		if subCategory == "Lowercase":
+			height = 500
+		elif subCategory == "Uppercase":
+			height = 700
+		else:
+			height = 600
+			
 	if layerIsEmpty:
 		bottomLeft = ( 50.0, 0.0 )
-		topRight = ( thisLayer.width - 50.0, 600.0 )
+		topRight = ( thisLayer.width - 50.0, height )
 		layerRect = drawRect( bottomLeft, topRight )
 		if layerRect:
-			thisLayer.paths.append( layerRect )
+			try:
+				thisLayer.paths.append( layerRect )
+			except:
+				# Glyphs 3:
+				thisLayer.shapes.append( layerRect )
 			return "OK"
 		else:
 			return "error"
 	else:
 		return "not empty, skipped"
+
+Glyphs.clearLog() # clears macro window log
+print("‘Fill Up with Rectangles’ report for: %s\n" % Font.familyName)
 
 Font.disableUpdateInterface()
 
@@ -53,3 +78,5 @@ for thisLayer in selectedLayers:
 	thisGlyph.endUndo()
 
 Font.enableUpdateInterface()
+
+print("\nDone.")
