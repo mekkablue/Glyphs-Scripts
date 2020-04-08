1ad31c16405d1d052a2a9950d3aeef14de207056
Rainer Scheichelbauer <res@glyphsapp.com>
2020-04-08 20:10:27 +0200

    Fix #189

--- Anchors/New Tab with top and bottom Anchors Not on Metric Lines.py
+++ Anchors/New Tab with top and bottom Anchors Not on Metric Lines.py
@@ -7,10 +7,18 @@ Reports the y positions of top and bottom anchors to the Macro Window if the anc
 
 thisFont = Glyphs.font
 
+print("Looking for misplaced top/bottom anchors")
+print("Font: %s" % thisFont.familyName)
+if thisFont.filepath:
+	print("📄 %s" % thisFont.filepath)
+else:
+	print("⚠️ File not saved yet.")
+print()
+	
 Glyphs.clearLog()
-# Glyphs.showMacroWindow()
+anchorCount = 0
 
-def isAnchorOnMetricLine( thisLayer, thisAnchorName ):
+def isAnchorOffMetrics( thisLayer, thisAnchorName ):
 	try:
 		myMaster = thisLayer.associatedFontMaster()
 		myY = thisLayer.anchors[ thisAnchorName ].y
@@ -21,34 +29,75 @@ def isAnchorOnMetricLine( thisLayer, thisAnchorName ):
 		if myY in masterMetrics:
 			return False
 		else:
-			print("%s: %s @ y=%s" % (thisLayer.parent.name, thisAnchorName, myY))
+			print("%s in %s, layer ‘%s’: %i" % (
+				thisAnchorName,
+				thisLayer.parent.name,
+				thisLayer.name,
+				myY,
+				))
 			return True
 	except Exception as e:
 		return False
 
 anchorsToLookFor = ("top","bottom")
-collectedGlyphNames = {}
-for thisAnchor in anchorsToLookFor:
-	collectedGlyphNames[thisAnchor] = []
 
+# prepare dictionary:
+collectedLayers = {}
+for thisAnchorName in anchorsToLookFor:
+	collectedLayers[thisAnchorName] = []
+	collectedLayers["_%s"%thisAnchorName] = []
+
+# step through all glyphs
 for thisGlyph in thisFont.glyphs:
-	for thisAnchor in anchorsToLookFor:
-		relevantLayers = [l for l in thisGlyph.layers if l and (l.isSpecialLayer or l.isMasterLayer)]
-		for thisVeryLayer in relevantLayers:
-			if isAnchorOnMetricLine( thisVeryLayer, thisAnchor ):
-				collectedGlyphNames[thisAnchor].append(thisGlyph.name)
+	for thisAnchorName in anchorsToLookFor:
+		for thisLayer in thisGlyph.layers:
+			if thisLayer.isSpecialLayer or thisLayer.isMasterLayer:
 
-offAnchorsFound = False
-reporttext = ""
-for thisAnchor in anchorsToLookFor:
-	glyphNames = collectedGlyphNames[thisAnchor]
-	# opens new Edit tab:
-	if glyphNames:
-		offAnchorsFound = True
-		reporttext += "Anchor '%s' off metrics:\n/%s\n\n" % (thisAnchor, "/".join(set(glyphNames)))
-thisFont.newTab( reporttext.strip() )
+				# check for _top instead of top if it exists:
+				connectingAnchorName = "_%s"%thisAnchorName
+				if thisLayer.anchors[connectingAnchorName]:
+					thisAnchorName = connectingAnchorName
+			
+				# check for position:
+				if isAnchorOffMetrics( thisLayer, thisAnchorName ):
+					collectedLayers[thisAnchorName].append(thisLayer)
+					anchorCount += 1
 
-if not offAnchorsFound:
+tab = None
+for thisAnchorName in sorted(collectedLayers.keys()):
+	layers = collectedLayers[thisAnchorName]
+	if layers:
+		if not tab:
+			# Open new Edit tab if it is not open yet:
+			tab = thisFont.newTab()
+			tab.scale=0.04
+			tab.initialZoom()
+		else:
+			# Otherwise add two newlines:
+			tab.layers.append(GSControlLayer.newline())
+			tab.layers.append(GSControlLayer.newline())
+			
+		# Add layers for anchor name:
+		for char in thisAnchorName:
+			charGlyph = thisFont.glyphs[char]
+			if charGlyph:
+				charLayer = charGlyph.layers[thisFont.selectedFontMaster.id]
+				tab.layers.append(charLayer)
+		tab.layers.append(GSControlLayer.newline())
+		
+		# add found anchors:
+		for layerWithMisplacedAnchor in collectedLayers[thisAnchorName]:
+			tab.layers.append(layerWithMisplacedAnchor)
+	
+
+if tab:
+	# Floating notification:
+	Glyphs.showNotification( 
+		u"%s: %i misplaced anchors" % (thisFont.familyName, anchorCount),
+		u"Opened new tab with %i anchors not on metric lines. Details in Macro Window." % anchorCount,
+		)
+	
+else:
 	reporttext = "Could not find %s anchors off metric lines on master layers." % " or ".join(anchorsToLookFor)
 	print(reporttext) # just in case opening a tab fails
 	Message(title="All is good", message=reporttext, OKButton="Cool")
