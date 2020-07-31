6dd7476ab6576e72b96925723ea445a896ae6988
Rainer Scheichelbauer <res@glyphsapp.com>
2020-07-31 15:00:10 +0200

    Glyphs 3 compatible

--- Color Fonts/Add All Color Layers to Selected Glyphs.py
+++ Color Fonts/Add All Color Layers to Selected Glyphs.py
@@ -8,7 +8,13 @@ Adds a duplicate of the fallback layer for each color defined in the Color Palet
 def process( thisGlyph, mID, paletteSize ):
 	for i in range(paletteSize):
 		newLayer = thisGlyph.layers[mID].copy()
-		newLayer.name = "Color %i" % i
+		try:
+			# GLYPHS 3
+			newLayer.setColorPaletteLayer_(1)
+			newLayer.setAttribute_forKey_(i, "colorPalette")
+		except:
+			# GLYPHS 2
+			newLayer.name = "Color %i" % i
 		thisGlyph.layers.append(newLayer)
 
 thisFont = Glyphs.font # frontmost font
@@ -16,11 +22,24 @@ selectedLayers = thisFont.selectedLayers # active layers of selected glyphs
 selectedGlyphs = [l.parent for l in selectedLayers]
 Glyphs.clearLog() # clears log in Macro window
 
-for m in Font.masters:
+CPAL = None
+parameterName = "Color Palettes"
+
+for m in thisFont.masters:
 	mID = m.id
-	CPAL = m.customParameters["Color Palettes"]
-	if CPAL:
-		paletteSize = len(CPAL[0])
-		for thisGlyph in selectedGlyphs:
-			print("Processing %s" % thisGlyph.name)
-			process( thisGlyph, mID, paletteSize )
+	CPAL = m.customParameters[parameterName]
+		
+if not CPAL:
+	CPAL = thisFont.customParameters[parameterName]
+
+if CPAL:
+	paletteSize = len(CPAL[0])
+	for thisGlyph in selectedGlyphs:
+		print("Processing %s" % thisGlyph.name)
+		process( thisGlyph, mID, paletteSize )
+else:
+	Message(
+		title="No Palette Found",
+		message="No ‘Color Palettes’ parameter found in Font Info > Font or Font Info > Masters. Please add the parameter and try again.", 
+		OKButton=None,
+		)
