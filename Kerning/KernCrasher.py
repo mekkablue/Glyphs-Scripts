26ad894913ba44716dfe5c96c7d404c09a0e0bcb
Rainer Scheichelbauer <res@glyphsapp.com>
2021-02-15 11:09:54 +0100

    Added option to limit search to only certain glyph names (Fix #214)

--- Kerning/KernCrasher.py
+++ Kerning/KernCrasher.py
@@ -28,7 +28,7 @@ class KernCrasher( object ):
 	def __init__( self ):
 		# Window 'self.w':
 		windowWidth  = 410
-		windowHeight = 310
+		windowHeight = 350
 		windowWidthResize  = 800 # user can resize width by this value
 		windowHeightResize = 0 # user can resize height by this value
 		self.w = vanilla.FloatingWindow(
@@ -47,8 +47,14 @@ class KernCrasher( object ):
 		
 		self.w.textScript = vanilla.TextBox( (inset, linePos+2, 42, 14), u"Script:", sizeStyle='small', selectable=True )
 		self.w.popupScript = vanilla.ComboBox( (inset+42, linePos-1, 110, 18), ("latin","cyrillic","greek"), callback=self.SavePreferences, sizeStyle='small' )
-		self.w.textDistance = vanilla.TextBox( (inset+160, linePos+2, 100, 14), "Min distance:", sizeStyle='small' )
-		self.w.minDistance = vanilla.EditText( (inset+240, linePos-1, -15, 19), "10", sizeStyle='small')
+		self.w.popupScript.getNSComboBox().setToolTip_("Limits the kerning pairs only to glyphs of this script, and those of no script.")
+		self.w.updateScriptsButton = vanilla.SquareButton( (inset+160, linePos, 20, 18), "↺", sizeStyle='small', callback=self.update )
+		self.w.updateScriptsButton.getNSButton().setToolTip_("Scans the frontost font for the scripts of all its glyphs (Latin, Greek, Cyrillic, Hebrew, Arabic, Thai, ...) and lists only those in the combo box.")
+		self.w.textDistance = vanilla.TextBox( (inset+200, linePos+2, 100, 14), "Min distance:", sizeStyle='small' )
+		self.w.minDistance = vanilla.EditText( (inset+280, linePos-1, -15, 19), "10", sizeStyle='small')
+		tooltipText = "Minimum distance required between glyphs in any given pairing with the current setup. Measured in units."
+		self.w.textDistance.getNSTextField().setToolTip_(tooltipText)
+		self.w.minDistance.getNSTextField().setToolTip_(tooltipText)
 		linePos += lineHeight
 
 		self.w.textSpeed = vanilla.TextBox( (inset, linePos+2, 42, 14), u"Speed:", sizeStyle='small', selectable=True )
@@ -57,36 +63,70 @@ class KernCrasher( object ):
 		if intervalIndex is None:
 			intervalIndex = 0
 		self.w.text_speedExplanation = vanilla.TextBox( (inset+160, linePos+2, -inset, 14), "Measuring every %i units."%intervalList[intervalIndex], sizeStyle='small' )
+		tooltipText = "The distances within every glyph pair will be measured from top to bottom. This setting determines how many measurements are taken. Higher speed means less measurements and thus less accuracy. Many measurements are more accurate, but also slow down the script significantly."
+		self.w.textSpeed.getNSTextField().setToolTip_(tooltipText)
+		self.w.popupSpeed.getNSPopUpButton().setToolTip_(tooltipText)
+		self.w.text_speedExplanation.getNSTextField().setToolTip_(tooltipText)
 		linePos += lineHeight
 		
-		self.w.text_3 = vanilla.TextBox( (inset, linePos+2, 90, 14), "Left Category:", sizeStyle='small' )
+		self.w.text_popupLeftCat = vanilla.TextBox( (inset, linePos+2, 90, 14), "Left Category:", sizeStyle='small' )
 		self.w.popupLeftCat = vanilla.PopUpButton( (inset+90, linePos, -inset, 17), categoryList, callback=self.SavePreferences, sizeStyle='small' )
+		tooltipText = "The category for the glyph on the LEFT side of the kerning pair. In ‘AV’, that would be ‘A’."
+		self.w.text_popupLeftCat.getNSTextField().setToolTip_(tooltipText)
+		self.w.popupLeftCat.getNSPopUpButton().setToolTip_(tooltipText)
 		linePos += lineHeight
 
-		self.w.text_4 = vanilla.TextBox( (inset, linePos+2, 90, 14), "Right Category:", sizeStyle='small' )
+		self.w.text_popupRightCat = vanilla.TextBox( (inset, linePos+2, 90, 14), "Right Category:", sizeStyle='small' )
 		self.w.popupRightCat = vanilla.PopUpButton( (inset+90, linePos, -inset, 17), categoryList, callback=self.SavePreferences, sizeStyle='small' )
+		tooltipText = "The category for the glyph on the RIGHT side of the kerning pair. In ‘AV’, that would be ‘V’."
+		self.w.text_popupRightCat.getNSTextField().setToolTip_(tooltipText)
+		self.w.popupRightCat.getNSPopUpButton().setToolTip_(tooltipText)
 		linePos += lineHeight
 		
-		self.w.text_5 = vanilla.TextBox( (inset, linePos+2, 160, 14), "Exclude glyphs containing:", sizeStyle='small' )
-		self.w.excludeSuffixes = vanilla.EditText( (inset+150, linePos, -inset, 19), ".locl, .alt, .sups, .sinf, .tf, .tosf, Ldot, ldot, Jacute, jacute", callback=self.SavePreferences, sizeStyle='small')
+		self.w.text_limitLeftSuffixes = vanilla.TextBox( (inset, linePos+2, 160, 14), "Left glyph must contain:", sizeStyle='small' )
+		self.w.limitLeftSuffixes = vanilla.EditText( (inset+150, linePos, -inset, 19), "", callback=self.SavePreferences, sizeStyle='small')
+		self.w.limitLeftSuffixes.getNSTextField().setPlaceholderString_("tilde, macron, dieresis, circumflex, caron")
+		tooltipText = "Only glyphs with these parts in their glyph names will be considered for the LEFT side. Comma-separated list. Usually not necessary, but useful for tracking down problems with wide diacritics."
+		self.w.text_limitLeftSuffixes.getNSTextField().setToolTip_(tooltipText)
+		self.w.limitLeftSuffixes.getNSTextField().setToolTip_(tooltipText)
 		linePos += lineHeight
 
-		self.w.text_6 = vanilla.TextBox( (inset, linePos+2, 160, 14), "Ignore height intervals:", sizeStyle='small' )
+		self.w.text_limitRightSuffixes = vanilla.TextBox( (inset, linePos+2, 160, 14), "Right glyph must contain:", sizeStyle='small' )
+		self.w.limitRightSuffixes = vanilla.EditText( (inset+150, linePos, -inset, 19), "", callback=self.SavePreferences, sizeStyle='small')
+		self.w.limitRightSuffixes.getNSTextField().setPlaceholderString_("tilde, macron, dieresis, circumflex, caron")
+		tooltipText = "Only glyphs with these parts in their glyph names will be considered for the RIGHT side. Comma-separated list. Usually not necessary, but useful for tracking down problems with wide diacritics."
+		self.w.text_limitRightSuffixes.getNSTextField().setToolTip_(tooltipText)
+		self.w.limitRightSuffixes.getNSTextField().setToolTip_(tooltipText)
+		linePos += lineHeight
+
+		self.w.text_excludeSuffixes = vanilla.TextBox( (inset, linePos+2, 160, 14), "Exclude glyphs containing:", sizeStyle='small' )
+		self.w.excludeSuffixes = vanilla.EditText( (inset+150, linePos, -inset, 19), ".locl, .alt, .sups, .sinf, .tf, .tosf, Ldot, ldot, Jacute, jacute", callback=self.SavePreferences, sizeStyle='small')
+		tooltipText = "Glyphs with these parts in their glyph names will be ignored. Comma-separated list. Useful for excluding impossible pairings, like ldot (can only appear before l) and jacute (can only appear after iacute), or OpenType variants."
+		self.w.text_excludeSuffixes.getNSTextField().setToolTip_(tooltipText)
+		self.w.excludeSuffixes.getNSTextField().setToolTip_(tooltipText)
+		linePos += lineHeight
+
+		self.w.text_ignoreIntervals = vanilla.TextBox( (inset, linePos+2, 160, 14), "Ignore height intervals:", sizeStyle='small' )
 		self.w.ignoreIntervals = vanilla.EditText( (inset+150, linePos, -inset, 19), "", callback=self.SavePreferences, sizeStyle='small')
 		self.w.ignoreIntervals.getNSTextField().setPlaceholderString_("200:300, 400:370, -200:-150")
+		tooltipText = "Will not measure between the specified heights. Useful for excluding connectors of connecting script typefaces or rekha lines like in Devanagari, where an overlap is intended by design. Specify two numbers separated by a colon, e.g., 200:300, and it will only measure until y=200 and start measuring again at y=300, skipping everything in between. If you specify multiple ranges, separate them with commas."
+		self.w.text_ignoreIntervals.getNSTextField().setToolTip_(tooltipText)
+		self.w.ignoreIntervals.getNSTextField().setToolTip_(tooltipText)
 		linePos += lineHeight
 		
-		self.w.pathGlyphsOnly = vanilla.CheckBox( (inset, linePos-1, -inset, 20), u"Limit to glyphs containing paths (i.e., exclude compounds)", value=False, callback=self.SavePreferences, sizeStyle='small' )
+		self.w.pathGlyphsOnly = vanilla.CheckBox( (inset, linePos-1, -inset, 20), u"Limit to glyphs containing paths (i.e., exclude composites)", value=False, callback=self.SavePreferences, sizeStyle='small' )
+		self.w.pathGlyphsOnly.getNSButton().setToolTip_("If enabled, will ignore glyphs that do not have paths. Useful for focusing on the base shapes (before you deal with the specific problems of composite diacritics).")
 		linePos += lineHeight
 
 		self.w.excludeNonExporting = vanilla.CheckBox( (inset, linePos, -inset, 20), "Exclude non-exporting glyphs", value=True, sizeStyle='small', callback=self.SavePreferences )
+		self.w.excludeNonExporting.getNSButton().setToolTip_("If enabled, will ignore glyphs that are set to not export. Recommended, otherwise you may get a lot of false positives.")
 		linePos += lineHeight
 
-		self.w.reportCrashesInMacroWindow = vanilla.CheckBox( (inset, linePos, -inset, 20), "Also report in Macro Window (slower)", value=False, sizeStyle='small', callback=self.SavePreferences )
+		self.w.reportCrashesInMacroWindow = vanilla.CheckBox( (inset, linePos, -inset, 20), "Verbose report in Macro Window", value=False, sizeStyle='small', callback=self.SavePreferences )
+		self.w.reportCrashesInMacroWindow.getNSButton().setToolTip_("Will output a detailed report of the kern crashing in Window > Macro Panel. Will slow down the script a bit. Usually not necessary, but can be useful for checking if a certain pairing has been taken care of or not.")
 		self.w.reuseCurrentTab = vanilla.CheckBox( (inset+240, linePos, -inset, 20), u"Reuse current tab", value=True, callback=self.SavePreferences, sizeStyle='small' )
-		self.w.reuseCurrentTab.getNSButton().setToolTip_(u"If enabled, will not open a new tab with newly added kern pairs, but reuse the current Edit tab. Will open an Edit tab if none is open.")
+		self.w.reuseCurrentTab.getNSButton().setToolTip_("If enabled, will not open a new tab with newly added kern pairs, but reuse the current Edit tab. Will open an Edit tab if none is open.")
 		linePos += lineHeight
-		
 		
 		# Percentage:
 		self.w.bar = vanilla.ProgressBar((inset, linePos, -inset, 16))
@@ -107,6 +147,14 @@ class KernCrasher( object ):
 		# Open window and focus on it:
 		self.w.open()
 		self.w.makeKey()
+	
+	def update(self, sender=None):
+		if sender == self.w.updateScriptsButton and Glyphs.font:
+			scriptList = [g.script for g in Glyphs.font.glyphs if g.script]
+			if scriptList:
+				updatedScriptList = list(set(scriptList))
+				self.w.popupScript.setItems(updatedScriptList)
+				self.w.popupScript.set(updatedScriptList[0])
 		
 	def SavePreferences( self, sender ):
 		try:
@@ -121,6 +169,8 @@ class KernCrasher( object ):
 			Glyphs.defaults["com.mekkablue.KernCrasher.ignoreIntervals"] = self.w.ignoreIntervals.get()
 			Glyphs.defaults["com.mekkablue.KernCrasher.pathGlyphsOnly"] = self.w.pathGlyphsOnly.get()
 			Glyphs.defaults["com.mekkablue.KernCrasher.reuseCurrentTab"] = self.w.reuseCurrentTab.get()
+			Glyphs.defaults["com.mekkablue.KernCrasher.limitLeftSuffixes"] = self.w.limitLeftSuffixes.get()
+			Glyphs.defaults["com.mekkablue.KernCrasher.limitRightSuffixes"] = self.w.limitRightSuffixes.get()
 		except Exception as e:
 			return False
 		
@@ -146,6 +196,8 @@ class KernCrasher( object ):
 			Glyphs.registerDefault( "com.mekkablue.KernCrasher.ignoreIntervals", "" )
 			Glyphs.registerDefault( "com.mekkablue.KernCrasher.pathGlyphsOnly", 0 )
 			Glyphs.registerDefault( "com.mekkablue.KernCrasher.reuseCurrentTab", 1 )
+			Glyphs.registerDefault( "com.mekkablue.KernCrasher.limitRightSuffixes", "" )
+			Glyphs.registerDefault( "com.mekkablue.KernCrasher.limitLeftSuffixes", "" )
 
 			self.w.minDistance.set( Glyphs.defaults["com.mekkablue.KernCrasher.minDistance"] )
 			self.w.popupScript.set( Glyphs.defaults["com.mekkablue.KernCrasher.popupScript"] )
@@ -158,6 +210,8 @@ class KernCrasher( object ):
 			self.w.ignoreIntervals.set( Glyphs.defaults["com.mekkablue.KernCrasher.ignoreIntervals"] )
 			self.w.pathGlyphsOnly.set( Glyphs.defaults["com.mekkablue.KernCrasher.pathGlyphsOnly"] )
 			self.w.reuseCurrentTab.set( Glyphs.defaults["com.mekkablue.KernCrasher.reuseCurrentTab"] )
+			self.w.limitRightSuffixes.set( Glyphs.defaults["com.mekkablue.KernCrasher.limitRightSuffixes"] )
+			self.w.limitLeftSuffixes.set( Glyphs.defaults["com.mekkablue.KernCrasher.limitLeftSuffixes"] )
 		except:
 			import traceback
 			print(traceback.format_exc())
@@ -187,7 +241,7 @@ class KernCrasher( object ):
 		else:
 			return 0.0
 	
-	def listOfNamesForCategories( self, thisFont, requiredCategory, requiredSubCategory, requiredScript, excludedGlyphNameParts, excludeNonExporting, pathGlyphsOnly ):
+	def listOfNamesForCategories( self, thisFont, requiredCategory, requiredSubCategory, requiredScript, excludedGlyphNameParts, excludeNonExporting, pathGlyphsOnly, mustContain ):
 		caseDict = {
 			"Uppercase": GSUppercase,
 			"Lowercase": GSLowercase,
@@ -202,6 +256,12 @@ class KernCrasher( object ):
 			if excludedGlyphNameParts:
 				for thisNamePart in excludedGlyphNameParts:
 					nameIsOK = nameIsOK and not thisNamePart in glyphName
+			
+			if mustContain:
+				nameIsOK = False
+				for thisNamePart in mustContain:
+					if thisNamePart in glyphName:
+						nameIsOK = True
 			
 			if nameIsOK and (thisGlyph.export or not excludeNonExporting):
 					if thisScript == None or thisScript == requiredScript:
@@ -336,6 +396,8 @@ class KernCrasher( object ):
 			excludedGlyphNameParts = self.splitString( Glyphs.defaults["com.mekkablue.KernCrasher.excludeSuffixes"], delimiter=",", minimum=0 )
 			excludeNonExporting = bool( Glyphs.defaults["com.mekkablue.KernCrasher.excludeNonExporting"] )
 			pathGlyphsOnly = bool( Glyphs.defaults["com.mekkablue.KernCrasher.pathGlyphsOnly"] )
+			limitRightSuffixes = self.splitString( Glyphs.defaults["com.mekkablue.KernCrasher.limitRightSuffixes"], delimiter=",", minimum=0 )
+			limitLeftSuffixes = self.splitString( Glyphs.defaults["com.mekkablue.KernCrasher.limitLeftSuffixes"], delimiter=",", minimum=0 )
 			minDistance = 0.0
 			ignoreIntervals = self.sortedIntervalsFromString( Glyphs.defaults["com.mekkablue.KernCrasher.ignoreIntervals"] )
 			try:
@@ -351,9 +413,19 @@ class KernCrasher( object ):
 				print("Note: KernCrasher could not write preferences.")
 			
 			# get list of glyph names:
-			firstList = self.listOfNamesForCategories( thisFont, firstCategory, firstSubCategory, script, excludedGlyphNameParts, excludeNonExporting, pathGlyphsOnly )
-			secondList = self.listOfNamesForCategories( thisFont, secondCategory, secondSubCategory, script, excludedGlyphNameParts, excludeNonExporting, pathGlyphsOnly )
-
+			firstList = self.listOfNamesForCategories( thisFont, firstCategory, firstSubCategory, script, excludedGlyphNameParts, excludeNonExporting, pathGlyphsOnly, limitLeftSuffixes )
+			secondList = self.listOfNamesForCategories( thisFont, secondCategory, secondSubCategory, script, excludedGlyphNameParts, excludeNonExporting, pathGlyphsOnly, limitRightSuffixes )
+			
+			if not firstList or not secondList:
+				Message(
+					title="Error: could not find any pairs",
+					message="The criteria for glyph selection are too strict. With the current settings, there are %i glyphs for the left side in the current font, and %i glyphs for the right side." % (
+						len(firstList),
+						len(secondList),
+					), 
+					OKButton=None,
+					)
+			
 			if Glyphs.defaults["com.mekkablue.KernCrasher.reportCrashesInMacroWindow"]:
 				print("Minimum Distance: %i\n" % minDistance)
 				print("Left glyphs:\n%s\n" % ", ".join(firstList))
