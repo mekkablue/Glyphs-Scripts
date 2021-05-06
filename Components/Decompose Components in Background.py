f4b93791dc866dd89eb31f1a146994454376d0f5
Rainer Scheichelbauer <res@glyphsapp.com>
2021-05-06 17:07:30 +0200

    More compact UI

--- Components/Decompose Components in Background.py
+++ Components/Decompose Components in Background.py
@@ -1,146 +1,125 @@
 #MenuTitle: Decompose Components in Background
 # -*- coding: utf-8 -*-
 from __future__ import division, print_function, unicode_literals
-from GlyphsApp import GSLayer
+from GlyphsApp import GSLayer, GSBackgroundLayer
 import vanilla as vl
 __doc__="""
-Decomposes components in the Background.
-"""
-
-warning_text_allfonts_allmasters = u"""❗️⚠️⚠️❗️WARNING ❗️⚠️⚠️❗️
-
-Option "All glyphs in all fonts in all masters" is selected
-
-Which means, that script will affect a lot of things with one click.
-Make sure that you want to do it and it is advised to make 
-a backup copy of your all opened fonts.
-
-It is also important to make sure that you've opened 
-only those fonts that you want to edit with this script.
-"""
-
-warning_text_allGlyphs = u"""❗️⚠️❗️WARNING ❗️⚠️❗️
-
-Option "All glyphs" is selected
-
-Which means, that script will affect a lot of things with one click.
-Make sure that you want to do it and it is advised to make 
-a backup copy of your currently edited font.
+Will backup foregrounds into backgrounds, and decompose backgrounds. Useful for keeping original designs of composites for reference.
 """
 
 class Decompose_Components_in_Background(object):
 	key = "com.mekkablue.Decompose_Components_in_Background"
-	baseH = 226
-	padding = (10,10,10)
-	btnH = 20
-	txtH = 14
+	windowHeight = 216
+	padding = (10,10,12)
+	buttonHeight = 20
+	textHeight = 14
 	sizeStyle = 'small'
-	glyphsOptions = ["Selected glyphs in current font", u"⚠️All glyphs in current font",u"⚠️⚠️ All glyphs in all fonts in all masters"]
-	masterOptions = ["Current master only", "All masters"]
+	glyphsOptions = (
+		"Backgrounds of selected glyphs in frontmost font", 
+		"⚠️ All glyph backgrounds in current font",
+		"⚠️ All glyph backgrounds in all masters of ⚠️ all open fonts",
+		)
+	masterOptions = (
+		"On currently selected master only", 
+		"On all masters",
+		)
+	
 	def __init__(self):
 		x,y,p = self.padding
 		
-		self.w = vl.Window((370, self.baseH), "Decompose Components in Background")
+		self.w = vl.FloatingWindow((370, self.windowHeight), "Decompose Backgrounds")
 		
-		### Master Options
-		self.w.glyphsOptionsRadioGroup = vl.RadioGroup(
-			(x, y, -p, self.btnH * len(self.glyphsOptions)),
+		### Description text
+		self.w.descriptionText = vl.TextBox( (x, y, -p, self.textHeight), "Decompose components in glyph backgrounds:", sizeStyle='small', selectable=True )
+		y += self.textHeight + p
+		
+		### Option to backup glyphs in the background before decomposition
+		self.w.copyToBackgroundFirst = vl.CheckBox((x+3, y, -p, self.textHeight), "Create backups in background first", sizeStyle=self.sizeStyle, callback=self.SavePreferences)
+		self.w.copyToBackgroundFirst.getNSButton().setToolTip_("If selected, will copy current foregrounds into backgrounds, overwriting existing background content.")
+		y += self.textHeight
+		
+		### Divider
+		self.w.div_copyToBackgroundFirst = vl.HorizontalLine((x, y, -p, self.textHeight))
+		y += self.textHeight
+		
+		### Which glyphs in which fonts
+		self.w.whichGlyphsInWhichFonts = vl.RadioGroup(
+			(x, y, -p, self.buttonHeight * len(self.glyphsOptions)),
 			self.glyphsOptions, sizeStyle=self.sizeStyle , callback=self.glyphsOptionsCallback)
 		
-
-		y += self.btnH * len(self.glyphsOptions)
-		self.w.div_glyphsOptions = vl.HorizontalLine((x, y, -p, self.txtH))
-		y += self.txtH + p 
-
-		### Master Options
-		self.w.masterOptionsRadioGroup = vl.RadioGroup(
-			(x, y, -p, self.btnH * len(self.masterOptions)),
-			self.masterOptions, sizeStyle=self.sizeStyle)
-
-		y += self.btnH * len(self.masterOptions) 
-		self.w.div_masterOptions = vl.HorizontalLine((x, y, -p, self.txtH))
-		y += self.txtH + p 
+		self.w.whichGlyphsInWhichFonts.getNSMatrix().setToolTip_("Which glyphs are affected in which fonts.\n\n⚠️ Careful with the bigger scopes: they affect many glyph backgrounds in one click. Consider saving your .glyphs file first (so you can revert in the worst case).")
+		y += self.buttonHeight * len(self.glyphsOptions)
 		
-		### backup glyphs in the background before decomposition
-		self.w.storeInBackground = vl.CheckBox((x+3, y, -p, self.txtH), "make background copy of glyphs before decomposition", sizeStyle=self.sizeStyle)
-		y += self.txtH
-		self.w.div_storeInBackground = vl.HorizontalLine((x, y, -p, self.txtH))
-		y += self.txtH + p 
+		### Divider
+		self.w.div_glyphsOptions = vl.HorizontalLine((x, y, -p, self.textHeight))
+		y += self.textHeight
+
+		### Which masters
+		self.w.whichMasters = vl.RadioGroup(
+			(x, y, -p, self.buttonHeight * len(self.masterOptions)),
+			self.masterOptions,
+			sizeStyle=self.sizeStyle,
+			callback=self.SavePreferences,
+			)
+		self.w.whichMasters.getNSMatrix().setToolTip_("Choose which font masters shall be affected.")
+
+		y += self.buttonHeight * len(self.masterOptions) 
 		
-
-		### worning text box
-		self.w.warningText = vl.TextBox((x,y,-p,-self.btnH-p-p),"",sizeStyle=self.sizeStyle)
-
-		### action button
+		### Action button
 		self.w.decomposeButton = vl.Button(
-				(x, -self.btnH-p, -p, self.btnH), 
-				"Decompose Components in Background",sizeStyle=self.sizeStyle,
+				(-p-120, -self.buttonHeight-p, -p, self.buttonHeight), 
+				"Decompose",
+				sizeStyle="regular",
 				callback=self.decomposeButtonCallback
 			)
-		y += self.btnH + p
 		
+		self.w.setDefaultButton( self.w.decomposeButton )
 
-		self.w.bind('close', self.closeWindowEvent)
+		if not self.LoadPreferences():
+			print("Note: 'Decompose Backgrounds' could not load preferences. Will resort to defaults")
+		
 		self.w.open()
-		self.LoadPreferences()
+		self.w.makeKey()
 
-	###
-	## CALLBACKS
-	###
-	
 	def glyphsOptionsCallback(self, sender):
-		enable = True
-		warningText = None
-		if self.glyphsOptions[sender.get()] == u"⚠️⚠️ All glyphs in all fonts in all masters":
-			enable = False
-			warningText = warning_text_allfonts_allmasters
-		if self.glyphsOptions[sender.get()] == u"⚠️All glyphs in current font":
-			warningText = warning_text_allGlyphs
-			
-		self.w.masterOptionsRadioGroup.enable(enable)
-		x,y,w,h = self.w.getPosSize()
-		if warningText is not None:
-			txth = len(warningText.split("\n")) * self.txtH
-			self.w.setPosSize((x,y,w,self.baseH+txth))
-			self.w.warningText.set(warningText)
-		else:
-			self.w.setPosSize((x,y,w,self.baseH))
-			self.w.warningText.set("")
+		allFontsSelected = sender.get()==2
+		self.w.whichMasters.enable(not allFontsSelected)
+		self.SavePreferences()
 		
 	def decomposeButtonCallback(self, sender):
+		# collect user settings:
+		copyToBackgroundFirst = bool(Glyphs.defaults[self.key+"copyToBackgroundFirst"])
+		workOnAllFonts = Glyphs.defaults[self.key+"whichGlyphsInWhichFonts"] == 2
+		workOnAllGlyphs = workOnAllFonts or Glyphs.defaults[self.key+"whichMasters"] != 0
 		
 		# determine affected fonts
-		if self.glyphsOptions[self.w.glyphsOptionsRadioGroup.get()] == u"⚠️⚠️ All glyphs in all fonts in all masters":
+		if workOnAllFonts:
 			selectedFonts = Glyphs.fonts
 		else:
-			selectedFonts = [Glyphs.font]
-
-
+			selectedFonts = (Glyphs.font,)
 		
 		for thisFont in selectedFonts:
 			# determine affected glyphs
-			if self.glyphsOptions[self.w.glyphsOptionsRadioGroup.get()] != "Selected glyphs in current font":
+			if workOnAllGlyphs:
 				selectedGlyphs = thisFont.glyphs
 			else:
 				selectedGlyphs = [thisLayer.parent for thisLayer in thisFont.selectedLayers]
 
 			# determine affected masters
-			if self.masterOptions[self.w.masterOptionsRadioGroup.get()] == "All masters":
+			workOnAllMasters = workOnAllFonts or self.w.whichMasters.get() == 1
+			if workOnAllMasters:
 				selectedMasterIDs = [master.id for master in thisFont.masters]
 			else:
-				selectedMasterIDs = [Glyphs.font.selectedLayers[0].associatedMasterId]
+				selectedMasterIDs = thisFont.selectedFontMaster.id
 
 			thisFont.disableUpdateInterface() # suppresses UI updates in Font View
 			try:
 				for thisGlyph in selectedGlyphs:
-					
 					####print("Processing", thisGlyph.name)
 					thisGlyph.beginUndo() # begin undo grouping
-					for thisMasterID in selectedMasterIDs:
-						thisLayer = thisGlyph.layers[thisMasterID]
-			
-						if thisLayer is not None:
-							self.process( thisLayer )
+					for thisLayer in thisGlyph.layers:
+						if thisLayer is not None and (thisLayer.isMasterLayer or thisLayer.isSpecialLayer) and thisLayer.associatedMasterId in selectedMasterIDs:
+							self.process( thisLayer, copyToBackgroundFirst=copyToBackgroundFirst )
 					thisGlyph.endUndo()   # end undo grouping
 
 			except Exception as e:
@@ -148,70 +127,61 @@ class Decompose_Components_in_Background(object):
 				print("\n⚠️ Script Error:\n")
 				import traceback
 				print(traceback.format_exc())
-				print()
-				raise e
+				print("\n%s"%e)
+				# raise e
 				
 			finally:
 				thisFont.enableUpdateInterface() # re-enables UI updates in Font View
 	
-	def process(self, thisLayer ):
-		
-		if thisLayer.components:
-			if self.w.storeInBackground.get() == 1:
-				thisLayer.setBackground_(thisLayer)	
-				print("  Storing '%s - %s' foreground in the background." % (thisLayer.name, thisLayer.parent.name))
-		background = None
+	def process( self, thisLayer, copyToBackgroundFirst=False ):
+		# Determine foreground and background:
 		if thisLayer.isKindOfClass_(GSBackgroundLayer):
 			background = thisLayer
+			foreground = thisLayer.foreground()
 		else:
 			background = thisLayer.background
+			foreground = thisLayer
 		
-		if not background:
-			print("  Could not access background layer '%s'." % thisLayer.name)
-		elif background.components:			
+		# make backup if user asked for it:
+		if copyToBackgroundFirst:
+			thisLayer.contentToBackgroundCheckSelection_keepOldBackground_(False,False)
+		
+		# decompose background:
+		if background and background.components:			
 			compCount = len(background.components)
-			print("  Decomposed %i component%s in background." % (
+			print("🔠 %s: decomposed %i component%s in background." % (
+				foreground.parent.name,
 				compCount,
 				"" if compCount==1 else "s",
 				))
 			background.decomposeComponents()	
 
-	### preferences handling:
-	def SavePreferences( self ):
+	def SavePreferences( self, sender=None ):
 		try:
-			Glyphs.defaults[self.key+"storeInBackground"] = self.w.storeInBackground.get()
-
-			Glyphs.defaults[self.key+"masterOptionsRadioGroup"] = self.w.masterOptionsRadioGroup.get()
-
-			Glyphs.defaults[self.key+"glyphsOptionsRadioGroup"] = self.w.glyphsOptionsRadioGroup.get()
+			Glyphs.defaults[self.key+"copyToBackgroundFirst"] = self.w.copyToBackgroundFirst.get()
+			Glyphs.defaults[self.key+"whichMasters"] = self.w.whichMasters.get()
+			Glyphs.defaults[self.key+"whichGlyphsInWhichFonts"] = self.w.whichGlyphsInWhichFonts.get()
 		except:
 			return False
 			
 		return True
 
-	def LoadPreferences( self ):
+	def LoadPreferences( self, sender=None ):
 		try:
-			Glyphs.registerDefault(self.key+"storeInBackground", "0")
-			self.w.storeInBackground.set( Glyphs.defaults[self.key+"storeInBackground"] )
+			Glyphs.registerDefault(self.key+"copyToBackgroundFirst", "0")
+			self.w.copyToBackgroundFirst.set( Glyphs.defaults[self.key+"copyToBackgroundFirst"] )
 
-			Glyphs.registerDefault(self.key+"masterOptionsRadioGroup", "0")
-			self.w.masterOptionsRadioGroup.set( Glyphs.defaults[self.key+"masterOptionsRadioGroup"] )
+			Glyphs.registerDefault(self.key+"whichMasters", "0")
+			self.w.whichMasters.set( Glyphs.defaults[self.key+"whichMasters"] )
 
-			Glyphs.registerDefault(self.key+"glyphsOptionsRadioGroup", "0")
-			self.w.glyphsOptionsRadioGroup.set( Glyphs.defaults[self.key+"glyphsOptionsRadioGroup"] )
+			Glyphs.registerDefault(self.key+"whichGlyphsInWhichFonts", "0")
+			self.w.whichGlyphsInWhichFonts.set( Glyphs.defaults[self.key+"whichGlyphsInWhichFonts"] )
 		except:
 			return False
 		
-		self.glyphsOptionsCallback(self.w.glyphsOptionsRadioGroup)
 		return True
-	###
-	## EVENTS
-	###
 
-	def closeWindowEvent(self, window):
-	   self.SavePreferences()
 def run():
 	Decompose_Components_in_Background()
-
 
 run()
