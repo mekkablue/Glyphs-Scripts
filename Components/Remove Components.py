73bfc9c9ec26eb3f6a5bc226091d9965e463f03a
Rainer Scheichelbauer <res@glyphsapp.com>
2021-12-23 16:23:22 -0300

    Better reporting, refactoring

--- Components/Remove Components.py
+++ Components/Remove Components.py
@@ -36,6 +36,7 @@ def match(first, second):
 
 def deleteCornerComponent( componentName, thisLayer ):
 	indToDel = []
+	count = 0
 	for i, h in enumerate(thisLayer.hints):
 		if h.isCorner:
 			#help(h)
@@ -44,36 +45,52 @@ def deleteCornerComponent( componentName, thisLayer ):
 	indToDel = list(reversed(indToDel))
 	for i in indToDel:
 		del thisLayer.hints[i]
+		count += 1
+	
+	if count:
+		print("  ❌ Removed %i corner component%s from layer: %s" % (
+				count, 
+				"" if count==1 else "s",
+				thisLayer.name,
+			))
 
 class RemoveComponentfromSelectedGlyphs( object ):
 	prefID = "com.mekkablue.RemoveComponents"
 	
 	def __init__( self ):
 		# Window 'self.w':
-		windowWidth  = 350
-		windowHeight = 130
-		windowWidthResize  = 200 # user can resize width by this value
+		windowWidth  = 250
+		windowHeight = 135
+		windowWidthResize  = 500 # user can resize width by this value
 		windowHeightResize = 0   # user can resize height by this value
 		self.w = vanilla.FloatingWindow(
 			( windowWidth, windowHeight ), # default window size
-			"Remove Component from Selected Glyphs", # window title
+			"Remove Components", # window title
 			minSize = ( windowWidth, windowHeight ), # minimum size (for resizing)
 			maxSize = ( windowWidth + windowWidthResize, windowHeight + windowHeightResize ), # maximum size (for resizing)
 			autosaveName = self.domain("mainwindow") # stores last window position and size
 		)
 		
 		# UI elements:
-		self.w.text1 = vanilla.TextBox( (15, 12+3, 130, 14), "Remove component", sizeStyle='small' )
+		linePos, inset, lineHeight = 12, 15, 22
+		self.w.text1 = vanilla.TextBox( (inset, linePos, 115, 14), "Remove component", sizeStyle='small' )
 		self.w.componentName  = vanilla.ComboBox(
-				(15+130, 12, -15, 19), 
+				(inset+115, linePos-3, -30-inset, 19), 
 				self.glyphList(), 
 				sizeStyle='small' 
 			)
-		self.w.fromWhere = vanilla.RadioGroup((15, 40, -15, 40), [ "from all selected glyphs", "from all glyphs in the font" ], callback=self.SavePreferences, sizeStyle = 'small' )
-		self.w.fromWhere.set( 0 )
+		self.w.updateButton = vanilla.SquareButton( (-inset-20, linePos-2, -inset, 18), "↺", sizeStyle='small', callback=self.updateUI )
+		
+		linePos += lineHeight
+		self.w.fromWhere = vanilla.RadioGroup(
+				(inset, linePos, -inset, 40),
+				("from all selected glyphs", "⚠️ from all glyphs in the font"),
+				callback=self.SavePreferences,
+				sizeStyle = 'small',
+			)
 		
 		# Run Button:
-		self.w.runButton = vanilla.Button((-100-15, -20-15, -15, -15), "Remove", sizeStyle='regular', callback=self.RemoveComponentfromSelectedGlyphsMain )
+		self.w.runButton = vanilla.Button((-100-inset, -20-inset, -inset, -inset), "Remove", sizeStyle='regular', callback=self.RemoveComponentfromSelectedGlyphsMain )
 		self.w.setDefaultButton( self.w.runButton )
 		
 		# Load Settings:
@@ -127,6 +144,7 @@ class RemoveComponentfromSelectedGlyphs( object ):
 	def removeComponentFromLayer(self, componentName, thisLayer):
 		theseComponents = thisLayer.components
 		numberOfComponents = len( theseComponents )
+		removedComponents = []
 		if numberOfComponents > 0:
 			for i in range(numberOfComponents)[::-1]:
 				thisComponent = theseComponents[i]
@@ -134,16 +152,30 @@ class RemoveComponentfromSelectedGlyphs( object ):
 					if Glyphs.versionNumber >= 3:
 						index = thisLayer.shapes.index(thisComponent)
 						del(thisLayer.shapes[index])
+						removedComponents.append(thisComponent.componentName)
 					else:
 						thisLayer.removeComponent_( thisComponent )
-
+		
+		if removedComponents:
+			print("  ❌ Removed %i component%s (%s) from layer: %s" % (
+					len(removedComponents), 
+					"" if len(removedComponents)==1 else "s",
+					", ".join(set(removedComponents)), 
+					thisLayer.name,
+				))
+			
 		deleteCornerComponent(componentName, thisLayer)
 		
 	def removeComponentFromGlyph(self, componentName, thisGlyph):
+		print("🔠 %s" % thisGlyph.name)
 		for thisLayer in thisGlyph.layers:
 			self.removeComponentFromLayer( componentName, thisLayer )
 
 	def RemoveComponentfromSelectedGlyphsMain( self, sender ):
+		# brings macro window to front and clears its log:
+		Glyphs.clearLog()
+		print( "Removing Components:" )
+		
 		try:
 			thisFont = Glyphs.font # frontmost font
 			listOfGlyphs = thisFont.glyphs 
@@ -157,7 +189,6 @@ class RemoveComponentfromSelectedGlyphs( object ):
 			
 			if not self.SavePreferences( self ):
 				print("Note: 'Remove Component from Selected Glyphs' could not write preferences.")
-			
 			
 		except Exception as e:
 			# brings macro window to front and reports error:
