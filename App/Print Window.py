d8d0b774f249687e1746e560f6e9f8a98a818f98
Rainer Scheichelbauer <res@glyphsapp.com>
2022-03-22 09:00:28 +0000

    Improved printing: less window, more content

--- App/Print Window.py
+++ App/Print Window.py
@@ -1,16 +1,15 @@
 #MenuTitle: Print Window
 """Print the frontmost window."""
 from __future__ import print_function
-
-
+from AppKit import NSPasteboard, NSStringPboardType
 
 doc = Glyphs.currentDocument
-
 if doc:
-	print(doc.windowController().window().print_( None ))
+	print(doc.windowController().window().contentView().print_( None ))
 else:
-	# brings macro window to front and clears its log:
-	Glyphs.clearLog()
-	Glyphs.showMacroWindow()
-	print("Cannot print window: No document open.")
+	Message(
+		title = "Print Window Error",
+		message = "Cannot print window: No document open.",
+		OKButton = None,
+		)
 	
