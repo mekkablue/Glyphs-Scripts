e3af5416a73a6e180137468d66fa2778ebcc0417
Rainer Scheichelbauer <res@glyphsapp.com>
2023-01-27 23:16:19 -0600

    New script

---
+++ App/Toggle Macro Window Separator.py
@@ -0,0 +1,18 @@
+#MenuTitle: Toggle Macro Window Separator
+# -*- coding: utf-8 -*-
+from __future__ import division, print_function, unicode_literals
+__doc__="""
+Toggles the separator position in the Macro Window between 80% and 20%.
+"""
+
+if Glyphs.versionNumber < 4:
+	from Foundation import NSHeight
+	splitview = Glyphs.delegate().macroPanelController().consoleSplitView()
+	frame = splitview.frame()
+	height = NSHeight(frame)
+	currentPos = splitview.positionOfDividerAtIndex_(0)/height
+	if currentPos > 0.5:
+		newPos = 0.2
+	else:
+		newPos = 0.8
+	splitview.setPosition_ofDividerAtIndex_(height * newPos, 0)
