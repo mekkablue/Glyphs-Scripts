b2cae9e24c938a4659ee2c5efbb79b88c922d441
Rainer Scheichelbauer <res@glyphsapp.com>
2023-04-18 15:55:33 +0200

    Tolerance of 2u

--- Compare Frontmost Fonts/Compare Metrics of Two Frontmost Fonts.py
+++ Compare Frontmost Fonts/Compare Metrics of Two Frontmost Fonts.py
@@ -2,27 +2,28 @@
 # -*- coding: utf-8 -*-
 from __future__ import division, print_function, unicode_literals
 __doc__ = """
-Compare widths of two frontmost fonts.
+Compare widths of two frontmost fonts. Tolerates 2 unit
 """
 
-Font1 = Glyphs.font # frontmost font
-Font2 = Glyphs.fonts[1] # other font
+font1 = Glyphs.font # frontmost font
+font2 = Glyphs.fonts[1] # other font
 
 # brings macro window to front and clears its log:
 Glyphs.clearLog()
 Glyphs.showMacroWindow()
+print(f"Comparing:\nFont 1: {font1.filepath}\nFont 2: {font2.filepath}\n"
 
-print("Comparing:\nFont 1: %s\nFont 2: %s\n" % (Font1.filepath, Font2.filepath))
-
-for g1 in [g for g in Font1.glyphs if g.export]:
+masterIndexesInBothFonts = range(min(len(font1.masters), len(font2.masters)))
+for g1 in [g for g in font1.glyphs if g.export]:
 	glyphname = g1.name
-	g2 = Font2.glyphs[glyphname]
+	g2 = font2.glyphs[glyphname]
 	if g2:
-		for mi, m1 in enumerate(Font1.masters):
-			m2 = Font2.masters[mi]
+		for mi in masterIndexesInBothFonts:
+			m1 = font1.masters[mi]
+			m2 = font2.masters[mi]
 			l1 = g1.layers[m1.id]
 			l2 = g2.layers[m2.id]
 			if abs(l1.width - l2.width) > 2.0:
 				print("/%s : widths: %.1f <> %.1f (%s)" % (glyphname, l1.width, l2.width, m1.name))
 	else:
-		print("  %s not in Font 2" % (glyphname))
+		print("  %s not in font 2" % (glyphname))
