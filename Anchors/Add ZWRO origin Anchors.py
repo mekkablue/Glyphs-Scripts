d64f95e7918bcb53ebf0b3c80a6a4e4d17589392
Rainer Scheichelbauer <res@glyphsapp.com>
2023-06-02 20:31:03 +0200

    New scripts (Fix #301)

---
+++ Anchors/Add ZWRO origin Anchors.py
@@ -0,0 +1,190 @@
+#MenuTitle: Add ZWRO origin Anchors
+# -*- coding: utf-8 -*-
+from __future__ import division, print_function, unicode_literals
+__doc__="""
+Insert *origin anchors for ZWRO in all combining marks of specified scripts. 
+"""
+
+from AppKit import NSPoint, NSHeight
+import vanilla, sys
+
+def moveMacroWindowSeparator(pos=20):
+	if Glyphs.versionNumber < 4:
+		splitview = Glyphs.delegate().macroPanelController().consoleSplitView()
+		frame = splitview.frame()
+		height = NSHeight(frame)
+		splitview.setPosition_ofDividerAtIndex_(height*pos/100.0, 0)
+
+class AddZWROOriginAnchors(object):
+	prefID = "com.mekkablue.AddZWROOriginAnchors"
+	prefDict = {
+		# "prefName": defaultValue,
+		"offset": 10,
+		"scripts": "latin, thai",
+		"excludeTransformed": 1,
+		"allFonts": 0,
+	}
+	
+	def __init__( self ):
+		# Window 'self.w':
+		windowWidth  = 300
+		windowHeight = 180
+		windowWidthResize  = 100 # user can resize width by this value
+		windowHeightResize = 0   # user can resize height by this value
+		self.w = vanilla.FloatingWindow(
+			( windowWidth, windowHeight ), # default window size
+			"Add ZWRO *origin Anchors", # window title
+			minSize = (windowWidth, windowHeight), # minimum size (for resizing)
+			maxSize = (windowWidth + windowWidthResize, windowHeight + windowHeightResize), # maximum size (for resizing)
+			autosaveName = self.domain("mainwindow") # stores last window position and size
+		)
+		
+		# UI elements:
+		linePos, inset, lineHeight, tab = 12, 15, 22, 105
+		self.w.descriptionText = vanilla.TextBox((inset, linePos+2, -inset, 14), "Add *origin anchors:", sizeStyle="small", selectable=True)
+		linePos += lineHeight
+
+		self.w.offsetText = vanilla.TextBox((inset, linePos+2, tab, 14), "Offset from bbox:", sizeStyle="small", selectable=True)
+		self.w.offset = vanilla.EditText((inset+tab, linePos, -inset, 19), "10", callback=self.SavePreferences, sizeStyle="small")
+		linePos += lineHeight
+		
+		self.w.scriptsText = vanilla.TextBox((inset, linePos+2, tab, 14), "Scripts:", sizeStyle="small", selectable=True)
+		self.w.scripts = vanilla.EditText((inset+tab, linePos, -inset, 19), "latin, thai", callback=self.SavePreferences, sizeStyle="small")
+		linePos += lineHeight
+		
+		self.w.excludeTransformed = vanilla.CheckBox((inset, linePos-1, -inset, 20), "Exclude marks used in transformed components", value=True, callback=self.SavePreferences, sizeStyle="small")
+		linePos += lineHeight
+		
+		self.w.allFonts = vanilla.CheckBox((inset, linePos-1, -inset, 20), "Apply to all fonts", value=True, callback=self.SavePreferences, sizeStyle="small")
+		linePos += lineHeight
+		
+		# Run Button:
+		self.w.runButton = vanilla.Button((-120-inset, -20-inset, -inset, -inset), "Add Anchors", sizeStyle="regular", callback=self.AddZWROOriginAnchorsMain)
+		self.w.setDefaultButton(self.w.runButton)
+		
+		# Load Settings:
+		if not self.LoadPreferences():
+			print("⚠️ ‘Add ZWRO origin Anchors’ could not load preferences. Will resort to defaults.")
+		
+		# Open window and focus on it:
+		self.w.open()
+		self.w.makeKey()
+	
+	def domain(self, prefName):
+		prefName = prefName.strip().strip(".")
+		return self.prefID + "." + prefName.strip()
+	
+	def pref(self, prefName):
+		prefDomain = self.domain(prefName)
+		return Glyphs.defaults[prefDomain]
+	
+	def SavePreferences(self, sender=None):
+		try:
+			# write current settings into prefs:
+			for prefName in self.prefDict.keys():
+				Glyphs.defaults[self.domain(prefName)] = getattr(self.w, prefName).get()
+			return True
+		except:
+			import traceback
+			print(traceback.format_exc())
+			return False
+
+	def LoadPreferences( self ):
+		try:
+			for prefName in self.prefDict.keys():
+				# register defaults:
+				Glyphs.registerDefault(self.domain(prefName), self.prefDict[prefName])
+				# load previously written prefs:
+				getattr(self.w, prefName).set(self.pref(prefName))
+			return True
+		except:
+			import traceback
+			print(traceback.format_exc())
+			return False
+	
+	def isTransformed(self, componentName, font):
+		for glyph in font.glyphs:
+			for layer in glyph.layers:
+				if layer.isMasterLayer or layer.isSpecialLayer:
+					for component in layer.components:
+						if component.componentName == componentName:
+							if component.transform[:4] != (1.0, 0.0, 0.0, 1.0):
+								return True
+		return False
+			
+	def AddZWROOriginAnchorsMain( self, sender=None ):
+		try:
+			# clear macro window log:
+			Glyphs.clearLog()
+			
+			# update settings to the latest user input:
+			if not self.SavePreferences():
+				print("⚠️ ‘Add ZWRO origin Anchors’ could not write preferences.")
+			
+			# read prefs:
+			for prefName in self.prefDict.keys():
+				try:
+					setattr(sys.modules[__name__], prefName, self.pref(prefName))
+				except:
+					fallbackValue = self.prefDict[prefName]
+					print(f"⚠️ Could not set pref ‘{prefName}’, resorting to default value: ‘{fallbackValue}’.")
+					setattr(sys.modules[__name__], prefName, fallbackValue)
+			
+			thisFont = Glyphs.font # frontmost font
+			if thisFont is None:
+				Message(title="No Font Open", message="The script requires a font. Open a font and run the script again.", OKButton=None)
+			else:
+				if allFonts:
+					theseFonts = Glyphs.fonts
+				else:
+					theseFonts = (thisFont,)
+				
+				for thisFont in theseFonts:
+					filePath = thisFont.filepath
+					if filePath:
+						reportName = f"{filePath.lastPathComponent()}\n📄 {filePath}"
+					else:
+						reportName = f"{thisFont.familyName}\n⚠️ The font file has not been saved yet."
+					print(f"Add ZWRO origin Anchors Report for {reportName}")
+					print()
+				
+					scriptNames = [scriptName.strip() for scriptName in scripts.strip().split(",")]
+					offset = int(self.pref("offset"))
+				
+					for glyph in thisFont.glyphs:
+						if glyph.category=="Mark" and glyph.subCategory=="Nonspacing":
+							if glyph.script in scriptNames or ("latin" in scriptNames and glyph.script==None):
+								if excludeTransformed:
+									if self.isTransformed(glyph.name, thisFont):
+										print(f"⚠️ Skipping {glyph.name} because it is used in a transformed component.")
+										countAnchors = 0
+										for layer in glyph.layers:
+											originAnchor = layer.anchors["*origin"]
+											if originAnchor:
+												countAnchors += 1
+												del layer.anchors["*origin"]
+										if countAnchors:
+											print(f"🚫 Removed {countAnchors} existing anchor{'' if countAnchors==1 else 's'} in all layers of {glyph.name}.")
+										continue
+								for layer in glyph.layers:
+									if layer.isMasterLayer or layer.isSpecialLayer:
+										if layer.bounds:
+											x = layer.bounds.origin.x + layer.bounds.size.width + offset
+											anchorPosition = NSPoint(x, 0)
+											anchor = GSAnchor("*origin", anchorPosition)
+											layer.anchors.append(anchor)
+											print(f"⚓️ {glyph.name}, {layer.name}: added {anchor.name} at {int(x)}x 0y")
+			
+			Glyphs.showMacroWindow()
+			moveMacroWindowSeparator()
+			self.w.close() # delete if you want window to stay open
+			print("\nDone.")
+
+		except Exception as e:
+			# brings macro window to front and reports error:
+			Glyphs.showMacroWindow()
+			print(f"Add ZWRO origin Anchors Error: {e}")
+			import traceback
+			print(traceback.format_exc())
+
+AddZWROOriginAnchors()
---
+++ Color Fonts/Convert Master Colors to CPAL Palette.py
@@ -0,0 +1,60 @@
+#MenuTitle: Convert Master Colors to CPAL Palette
+# -*- coding: utf-8 -*-
+from __future__ import division, print_function, unicode_literals
+__doc__="""
+Will look for ‘Master Color’ parameters in the font masters and then create a ‘Color Palettes’ parameter in Font Info > Font with the same color. Will default to black (for missing Master Color parameters). Will add Dark Mode master colors as second palette.
+"""
+
+from AppKit import NSHeight, NSColor
+
+def colorForMaster(thisMaster, parameterName="Master Color"):
+	color = thisMaster.customParameters[parameterName]
+	if not color:
+		color = NSColor.blackColor()
+	color = color.colorUsingColorSpaceName_("NSCalibratedRGBColorSpace")
+	print(f"\t{parameterName}: {color.redComponent():.3f}r {color.greenComponent():.3f}g {color.blueComponent():.3f}b {color.alphaComponent():.3f}a")
+	return color
+
+Glyphs.clearLog() # clears log in Macro window
+print("Status Report: Convert Master Colors to CPAL Palette")
+if Glyphs.versionNumber < 4:
+	try:
+		splitview = Glyphs.delegate().macroPanelController().consoleSplitView()
+		splitview.setPosition_ofDividerAtIndex_(NSHeight(splitview.frame()) * 0.2, 0)
+	except Exception as e:
+		print("\nFailed resetting the macro panel divider:")
+		import traceback
+		print(traceback.format_exc())
+
+thisFont = Glyphs.font # frontmost font
+thisFont.disableUpdateInterface() # suppresses UI updates in Font View
+try:
+	paletteColors = []
+	paletteColorsDark = []
+	for thisMaster in thisFont.masters:
+		print(f"Analyzing master ‘{thisMaster.name}’...")
+		paletteColors.append(colorForMaster(thisMaster))
+		paletteColorsDark.append(colorForMaster(thisMaster, parameterName="Master Color Dark"))
+	cpalPalettes = NSMutableArray.arrayWithObjects_(paletteColors, paletteColorsDark)
+	if cpalPalettes:
+		print("🌈 Adding Color Palettes parameter in Font Info > Font...")
+		thisFont.customParameters["Color Palettes"] = cpalPalettes
+		print("✅ Done.")
+		# open Font Info:
+		for doc in Glyphs.documents:
+			if doc.font == thisFont:
+				doc.windowController().showFontInfoWindowWithTabSelected_(0)
+				break
+	else:
+		print("🚫 No palette added.")
+		Glyphs.showMacroWindow()
+
+except Exception as e:
+	Glyphs.showMacroWindow()
+	print("\n⚠️ Error in script: Convert Master Colors to CPAL Palette\n")
+	import traceback
+	print(traceback.format_exc())
+	print()
+	raise e
+finally:
+	thisFont.enableUpdateInterface() # re-enables UI updates in Font View
