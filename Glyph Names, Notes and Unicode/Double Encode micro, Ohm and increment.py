a98c6cf9d346e1ca6b09e34592908ad57678f36a
Rainer Scheichelbauer <res@glyphsapp.com>
2023-11-06 17:43:18 +0700

    Apply to All Fonts by holding down ⌘⇧

--- Glyph Names, Notes and Unicode/Double Encode micro, Ohm and increment.py
+++ Glyph Names, Notes and Unicode/Double Encode micro, Ohm and increment.py
@@ -2,8 +2,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import division, print_function, unicode_literals
 __doc__="""
-Add Unicodes of mu, Omega and Delta to micro, Ohm and increment.
+Add Unicodes of mu, Omega and Delta to micro, Ohm and increment. Hold down COMMAND + SHIFT to apply to ALL open fonts.
 """
+
+from AppKit import NSEvent, NSEventModifierFlagShift, NSEventModifierFlagCommand
 
 codeInfos = (
 	{
@@ -20,41 +22,56 @@ codeInfos = (
 	},
 )
 
-thisFont = Glyphs.font # frontmost font
-selectedLayers = thisFont.selectedLayers # active layers of selected glyphs
 Glyphs.clearLog() # clears log in Macro window
 
-thisFont.disableUpdateInterface() # suppresses UI updates in Font View
-try:
-	for codeInfo in codeInfos:
-		targetGlyphName = codeInfo["target"]
-		targetGlyph = thisFont.glyphs[targetGlyphName]
-		targetCode = Glyphs.glyphInfoForName(targetGlyphName).unicode
-		
-		greekName = codeInfo["greek"]
-		greekGlyph = thisFont.glyphs[greekName]
-		greekCode = Glyphs.glyphInfoForName(greekName).unicode
+# CMD+SHIFT pressed?
+keysPressed = NSEvent.modifierFlags()
+shiftKeyPressed = keysPressed & NSEventModifierFlagShift == NSEventModifierFlagShift
+commandKeyPressed = keysPressed & NSEventModifierFlagCommand == NSEventModifierFlagCommand
+shouldApplyToAllFonts = shiftKeyPressed and commandKeyPressed
 
-		if targetGlyph and not greekGlyph:
-			targetGlyph.unicode = targetCode
-			targetGlyph.unicodes = [targetCode, greekCode]
-		elif greekGlyph and not targetGlyph:
-			print(targetGlyphName)
-			greekGlyph.name = targetGlyphName
-			greekGlyph.unicode = targetCode
-			greekGlyph.unicodes = [targetCode, greekCode]
-		elif greekGlyph and targetGlyph:
-			greekGlyph.export = False
-			greekGlyph.name += ".disabled"
-			targetGlyph.unicode = targetCode
-			targetGlyph.unicodes = [targetCode, greekCode]
-	thisFont.newTab("/"+"/".join([i["target"] for i in codeInfos]))
-except Exception as e:
-	Glyphs.showMacroWindow()
-	print("\n⚠️ Error in script: Double Encode micro, Ohm and increment\n")
-	import traceback
-	print(traceback.format_exc())
-	print()
-	raise e
-finally:
-	thisFont.enableUpdateInterface() # re-enables UI updates in Font View
+if shouldApplyToAllFonts:
+	fonts = Glyphs.fonts
+else:
+	fonts = (Glyphs.font,)
+
+
+for thisFont in fonts:
+	print(f"DOUBLE ENCODE REPORT: {thisFont.filepath.lastPathComponent().stringByDeletingDotSuffix()}")
+	thisFont.disableUpdateInterface() # suppresses UI updates in Font View
+	try:
+		for codeInfo in codeInfos:
+			targetGlyphName = codeInfo["target"]
+			targetGlyph = thisFont.glyphs[targetGlyphName]
+			targetCode = Glyphs.glyphInfoForName(targetGlyphName).unicode
+		
+			greekName = codeInfo["greek"]
+			greekGlyph = thisFont.glyphs[greekName]
+			greekCode = Glyphs.glyphInfoForName(greekName).unicode
+
+			if targetGlyph and not greekGlyph:
+				targetGlyph.unicode = targetCode
+				targetGlyph.unicodes = [targetCode, greekCode]
+				print(f"  🔤 {targetGlyphName}: {', '.join(targetGlyph.unicodes)}")
+			elif greekGlyph and not targetGlyph:
+				greekGlyph.name = targetGlyphName
+				greekGlyph.unicode = targetCode
+				greekGlyph.unicodes = [targetCode, greekCode]
+				print(f"  🔤 Renamed {greekName} → {targetGlyphName}: {', '.join(greekGlyph.unicodes)}")
+			elif greekGlyph and targetGlyph:
+				greekGlyph.export = False
+				greekGlyph.name += ".disabled"
+				targetGlyph.unicode = targetCode
+				targetGlyph.unicodes = [targetCode, greekCode]
+				print(f"  🔤 Disabled {greekName}, renamed to {greekGlyph.name}; {targetGlyphName}: {', '.join(targetGlyph.unicodes)}")
+		thisFont.newTab("/"+"/".join([i["target"] for i in codeInfos]))
+	except Exception as e:
+		Glyphs.showMacroWindow()
+		print("\n⚠️ Error in script: Double Encode micro, Ohm and increment\n")
+		import traceback
+		print(traceback.format_exc())
+		print()
+		raise e
+	finally:
+		thisFont.enableUpdateInterface() # re-enables UI updates in Font View
+		print()
