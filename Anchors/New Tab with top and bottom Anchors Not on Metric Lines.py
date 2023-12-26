4ebfd13761f6b041483ab77d1d6390b23a574c1b
Rainer Scheichelbauer <res@glyphsapp.com>
2023-12-26 20:14:12 -0300

    Prevent hang

--- Anchors/New Tab with top and bottom Anchors Not on Metric Lines.py
+++ Anchors/New Tab with top and bottom Anchors Not on Metric Lines.py
@@ -2,17 +2,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import division, print_function, unicode_literals
 __doc__ = """
-For all open fonts, reports the y positions of top and bottom anchors to the Macro Window if the anchors are not on a metric line (baseline, x-height, etc.).
+Reports the y positions of top and bottom anchors to the Macro Window if the anchors are not on a metric line (baseline, x-height, etc.). Hold down OPTION and SHIFT to run for all open fonts.
 """
 
 def isAnchorOffMetrics(thisLayer, thisAnchorName):
 	try:
-		myMaster = thisLayer.associatedFontMaster()
-		myY = thisLayer.anchors[thisAnchorName].y
-		masterMetrics = [0.0, myMaster.xHeight, myMaster.descender, myMaster.ascender, myMaster.capHeight]
-		scHeight = myMaster.customParameters["smallCapHeight"]
-		if scHeight:
-			masterMetrics.append(float(scHeight))
+		myAnchor = thisLayer.anchors[thisAnchorName]
+		if not myAnchor:
+			return False
+		myY = myAnchor.y
+		masterMetrics = [m.position for m in thisLayer.metrics if hasattr(m, "position")]
 		if myY in masterMetrics:
 			return False
 		else:
@@ -26,73 +25,95 @@ def isAnchorOffMetrics(thisLayer, thisAnchorName):
 	except Exception as e:
 		return False
 
+from AppKit import NSEvent, NSEventModifierFlagOption, NSEventModifierFlagShift
+keysPressed = NSEvent.modifierFlags()
+optionKeyPressed = keysPressed & NSEventModifierFlagOption == NSEventModifierFlagOption
+shiftKeyPressed = keysPressed & NSEventModifierFlagShift == NSEventModifierFlagShift
+
 
 # brings macro window to front and clears its log:
 Glyphs.clearLog()
 anchorsToLookFor = ("top", "bottom")
 
-for thisFont in Glyphs.fonts:
-	print("Looking for misplaced top/bottom anchors")
-	print(f"Font: {thisFont.familyName}")
-	if thisFont.filepath:
-		print("📄 {thisFont.filepath}")
-	else:
-		print("⚠️ File not saved yet.")
-	print()
+if optionKeyPressed and shiftKeyPressed:
+	fonts = Glyphs.fonts
+else:
+	fonts = [Glyphs.font]
 
-	anchorCount = 0
+for thisFont in fonts:
+	thisFont.disableUpdateInterface() # suppresses UI updates in Font View
+	try:
+		print("Looking for misplaced top/bottom anchors")
+		print(f"Font: {thisFont.familyName}")
+		if thisFont.filepath:
+			print(f"📄 {thisFont.filepath}")
+		else:
+			print("⚠️ File not saved yet.")
+		print()
+		
+		anchorCount = 0
 
-	# prepare dictionary:
-	collectedLayers = {}
-	for thisAnchorName in anchorsToLookFor:
-		collectedLayers[thisAnchorName] = []
-		collectedLayers["_%s" % thisAnchorName] = []
-
-	# step through all glyphs
-	for thisGlyph in thisFont.glyphs:
+		# prepare dictionary:
+		collectedLayers = {}
 		for thisAnchorName in anchorsToLookFor:
-			for thisLayer in thisGlyph.layers:
-				if thisLayer.isSpecialLayer or thisLayer.isMasterLayer:
+			collectedLayers[thisAnchorName] = []
+			collectedLayers["_%s" % thisAnchorName] = []
 
-					# check for _top instead of top if it exists:
-					connectingAnchorName = f"_{thisAnchorName}"
-					if thisLayer.anchors[connectingAnchorName]:
-						thisAnchorName = connectingAnchorName
+		# step through all glyphs
+		for thisGlyph in thisFont.glyphs:
+			for thisAnchorName in anchorsToLookFor:
+				for thisLayer in thisGlyph.layers:
+					if thisLayer.isSpecialLayer or thisLayer.isMasterLayer:
 
-					# check for position:
-					if isAnchorOffMetrics(thisLayer, thisAnchorName):
-						collectedLayers[thisAnchorName].append(thisLayer)
-						anchorCount += 1
+						# check for _top instead of top if it exists:
+						connectingAnchorName = f"_{thisAnchorName}"
+						if thisLayer.anchors[connectingAnchorName]:
+							thisAnchorName = connectingAnchorName
 
-	tab = None
-	for thisAnchorName in sorted(collectedLayers.keys()):
-		layers = collectedLayers[thisAnchorName]
-		if layers:
-			if not tab:
-				# Open new Edit tab if it is not open yet:
-				tab = thisFont.newTab()
-				tab.scale = 0.04
-				tab.initialZoom()
-			else:
-				# Otherwise add two newlines:
+						# check for position:
+						if isAnchorOffMetrics(thisLayer, thisAnchorName):
+							collectedLayers[thisAnchorName].append(thisLayer)
+							anchorCount += 1
+
+		tab = None
+		for thisAnchorName in sorted(collectedLayers.keys()):
+			layers = collectedLayers[thisAnchorName]
+			if layers:
+				if not tab:
+					# Open new Edit tab if it is not open yet:
+					tab = thisFont.newTab()
+					tab.scale = 0.04
+					tab.initialZoom()
+				else:
+					# Otherwise add two newlines:
+					tab.layers.append(GSControlLayer.newline())
+					tab.layers.append(GSControlLayer.newline())
+
+				# Add layers for anchor name:
+				for char in thisAnchorName:
+					charGlyph = thisFont.glyphs[char]
+					if charGlyph:
+						charLayer = charGlyph.layers[thisFont.selectedFontMaster.id]
+						tab.layers.append(charLayer)
 				tab.layers.append(GSControlLayer.newline())
-				tab.layers.append(GSControlLayer.newline())
 
-			# Add layers for anchor name:
-			for char in thisAnchorName:
-				charGlyph = thisFont.glyphs[char]
-				if charGlyph:
-					charLayer = charGlyph.layers[thisFont.selectedFontMaster.id]
-					tab.layers.append(charLayer)
-			tab.layers.append(GSControlLayer.newline())
+				# add found anchors:
+				for layerWithMisplacedAnchor in collectedLayers[thisAnchorName]:
+					tab.layers.append(layerWithMisplacedAnchor)
 
-			# add found anchors:
-			for layerWithMisplacedAnchor in collectedLayers[thisAnchorName]:
-				tab.layers.append(layerWithMisplacedAnchor)
-
-	if tab:
-		print(f"{anchorCount} anchors not on metric lines.\n")
-	else:
-		print(f"Could not find {' or '.join(anchorsToLookFor)} anchors off metric lines on master layers.\n")
-
+		if tab:
+			print(f"⚠️ {anchorCount} anchors not on metric lines.\n")
+		else:
+			print(f"🎉 Could not find {' or '.join(anchorsToLookFor)} anchors off metric lines on master layers.\n")
+		
+	except Exception as e:
+		Glyphs.showMacroWindow()
+		print("\n⚠️ Error in script: \n")
+		import traceback
+		print(traceback.format_exc())
+		print()
+		raise e
+	finally:
+		thisFont.enableUpdateInterface() # re-enables UI updates in Font View
+	
 print("✅ Done.")
