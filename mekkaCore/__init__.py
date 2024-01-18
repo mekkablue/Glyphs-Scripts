41642f85c9ef82e4f9d07f8e98211887284acf4c
schriftgestalt <georg.seifert@mac.com>
2024-01-18 16:04:54 +0100

    cleanup, missing method and type annotation

--- mekkaCore/__init__.py
+++ mekkaCore/__init__.py
@@ -1,5 +1,6 @@
 
 import math
+from typing import Any
 from AppKit import NSAffineTransform, NSUserDefaults
 from GlyphsApp import Glyphs
 
@@ -57,9 +58,9 @@ def match(first, second):
 	return False
 
 
-def camelCaseSplit(str):
-	words = [[str[0]]]
-	for c in str[1:]:
+def camelCaseSplit(string: str) -> str:
+	words = [[string[0]]]
+	for c in string[1:]:
 		if words[-1][-1].islower() and c.isupper():
 			words.append(list(c))
 		else:
@@ -67,7 +68,7 @@ def camelCaseSplit(str):
 	return [''.join(word) for word in words]
 
 
-def transform(shiftX=0.0, shiftY=0.0, rotate=0.0, skew=0.0, scale=1.0):
+def transform(shiftX=0.0, shiftY=0.0, rotate=0.0, skew=0.0, scale=1.0) -> NSAffineTransform:
 	"""
 	Returns an NSAffineTransform object for transforming layers.
 	Apply an NSAffineTransform t object like this:
@@ -95,27 +96,31 @@ def transform(shiftX=0.0, shiftY=0.0, rotate=0.0, skew=0.0, scale=1.0):
 
 class mekkaObject:
 
-	def domain(self, prefName):
+	def domain(self, prefName: str) -> str:
 		prefName = prefName.strip().strip(".")
 		return self.__class__.__name__ + "." + prefName.strip()
 
-	def pref(self, prefName):
+	def pref(self, prefName: str) -> Any:
 		prefDomain = self.domain(prefName)
 		return Glyphs.defaults[prefDomain]
 
-	def prefInt(self, prefName):
+	def prefBool(self, prefName: str) -> bool:
 		prefDomain = self.domain(prefName)
-		return Glyphs.intDefaults[prefDomain]
+		return NSUserDefaults.standardUserDefaults().boolForKey_(prefDomain)
 
-	def prefFloat(self, prefName):
+	def prefInt(self, prefName: str) -> int:
+		prefDomain = self.domain(prefName)
+		return NSUserDefaults.standardUserDefaults().integerForKey_(prefDomain)
+
+	def prefFloat(self, prefName: str) -> float:
 		prefDomain = self.domain(prefName)
 		return NSUserDefaults.standardUserDefaults().doubleForKey_(prefDomain)
 
-	def setPref(self, prefName, value):
+	def setPref(self, prefName: str, value: Any):
 		prefDomain = self.domain(prefName)
 		Glyphs.defaults[prefDomain] = value
 
-	def uiElement(self, prefName):
+	def uiElement(self, prefName: str) -> Any:
 		particles = prefName.split(".")
 		latestObject = self.w
 		for particle in particles:
@@ -136,7 +141,7 @@ class mekkaObject:
 			print(traceback.format_exc())
 			print(f"⚠️ ‘{self.__class__.__name__}’ could not load preferences. Will resort to defaults.")
 
-	def SavePreferences(self, sender=None):
+	def SavePreferences(self):
 		try:
 			# write current settings into prefs:
 			for prefName in self.prefDict.keys():
