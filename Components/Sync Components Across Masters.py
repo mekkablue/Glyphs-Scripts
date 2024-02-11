0112dab89428631eb4d48854a51318a7a9ce986b
mekkablue <mekka@mekkablue.com>
2024-02-11 21:12:53 +0100

    ignore bracket layers

--- Components/Sync Components Across Masters.py
+++ Components/Sync Components Across Masters.py
@@ -7,62 +7,76 @@ Takes the current layer’s components, and resets all other masters to the same
 from AppKit import NSEvent, NSAlternateKeyMask
 from GlyphsApp import Glyphs, GSComponent
 
+Glyphs.clearLog()
 thisFont = Glyphs.font  # frontmost font
 listOfSelectedLayers = thisFont.selectedLayers  # active layers of selected glyphs
+print(f"Sync Components Across Masters for {thisFont.familyName}:")
 
 # combo key held down:
 keysPressed = NSEvent.modifierFlags()
 optionKeyPressed = keysPressed & NSAlternateKeyMask == NSAlternateKeyMask
+if optionKeyPressed:
+	print(f"Opt key detected: will reset layers, not just add components.")
+print()
 
 
 def process(thisLayer):
 	thisGlyph = thisLayer.parent
+
 	# only act if components are present:
-	if len(thisLayer.components):
-		for thatLayer in thisGlyph.layers:
-			if thatLayer is thisLayer:
-				continue
-			newComponents = []
-			for thisComp in thisLayer.components:
-				thatComp = thisComp.copy()
-				newComponents.append(thatComp)
+	if len(thisLayer.components)==0:
+		print(f"  ⚠️ No components on layer {thisLayer.name}, skipping.")
+		return
+		
+	for thatLayer in thisGlyph.layers:
+		if thatLayer is thisLayer:
+			continue
+			
+		if not thatLayer.isMasterLayer and not thatLayer.isBraceLayer():
+			continue
+		
+		newComponents = []
+		for thisComp in thisLayer.components:
+			newComponents.append(thisComp.copy())
 
-			if newComponents:
-				if Glyphs.versionNumber >= 3:
-					# GLYPHS 3
-					if optionKeyPressed:
-						# clear all:
-						thatLayer.shapes = newComponents
-						thatLayer.anchors = None
-					else:
-						# clear components:
-						for i in range(len(thatLayer.shapes) - 1, -1, -1):
-							shape = thatLayer.shapes[i]
-							if isinstance(shape, GSComponent) or optionKeyPressed:  # NEW: check for paths
-								del thatLayer.shapes[i]
-						# add components:
-						thatLayer.shapes.extend(newComponents)
+		if Glyphs.versionNumber >= 3:
+			# GLYPHS 3
+			if optionKeyPressed:
+				# clear all:
+				thatLayer.shapes = None
+				thatLayer.anchors = None
+			else:
+				# clear components:
+				for i in range(len(thatLayer.shapes) - 1, -1, -1):
+					shape = thatLayer.shapes[i]
+					if isinstance(shape, GSComponent):
+						del thatLayer.shapes[i]
+			# add components:
+			thatLayer.shapes.extend(newComponents)
 
-				else:
-					# GLYPHS 2
-					thatLayer.components = newComponents
-					if optionKeyPressed:
-						if thatLayer.anchors:
-							thatLayer.anchors = None
-							print("    Deleted anchors")
-						if thatLayer.paths:
-							thatLayer.paths = None
-							print("    Deleted paths")
+		else:
+			# GLYPHS 2
+			thatLayer.components = newComponents
+			if optionKeyPressed:
+				if thatLayer.anchors:
+					thatLayer.anchors = None
+					print("    Deleted anchors")
+				if thatLayer.paths:
+					thatLayer.paths = None
+					print("    Deleted paths")
+
+		print(f"  🆗 {thatLayer.name}")
 
 
 thisFont.disableUpdateInterface()  # suppresses UI updates in Font View
 try:
 	for thisLayer in listOfSelectedLayers:
 		thisGlyph = thisLayer.parent
-		print("Processing %s" % thisGlyph.name)
-		# thisGlyph.beginUndo()  # undo grouping causes crashes
+		print(f"🔠 Syncing components in {thisGlyph.name}...")
+		thisGlyph.beginUndo()  # undo grouping
 		process(thisLayer)
-		# thisGlyph.endUndo()  # undo grouping causes crashes
+		thisGlyph.endUndo()  # undo grouping
+	print("\n✅ Done.")
 except Exception as e:
 	Glyphs.showMacroWindow()
 	print("\n⚠️ Script Error:\n")
