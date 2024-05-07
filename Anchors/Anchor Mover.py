dbd527e342c0e98a5dfccf53c1945899d9baf2c8
mekkablue <mekka@mekkablue.com>
2024-05-07 12:28:44 +0200

    Upgarde for G3 (Fix #334)

--- Anchors/Anchor Mover.py
+++ Anchors/Anchor Mover.py
@@ -8,7 +8,7 @@ Batch-process anchor positions in selected glyphs (GUI).
 import vanilla
 import math
 from Foundation import NSPoint
-from GlyphsApp import Glyphs, GSOFFCURVE, Message
+from GlyphsApp import Glyphs, GSOFFCURVE, Message, GSMetricsTypeAscender, GSMetricsTypeCapHeight, GSMetricsTypeDescender, GSMetricsTypexHeight, GSMetricsTypeItalicAngle
 from mekkablue import mekkaObject
 from mekkablue.geometry import transform, italicize
 
@@ -60,7 +60,7 @@ listHorizontal = (
 	("center", "copyLayer.width // 2.0"),
 	("bbox left edge", "copyLayer.fastBounds().origin.x"),
 	("bbox center", "copyLayer.fastBounds().origin.x + copyLayer.fastBounds().size.width // 2.0"),
-	("bbox right edge", "copyLayer.fastBounds().origin.x + copyLayer.bofastBounds()unds.size.width"),
+	("bbox right edge", "copyLayer.fastBounds().origin.x + copyLayer.fastBounds().size.width"),
 	("highest node", "highestNodeInLayer(copyLayer).x"),
 	("lowest node", "lowestNodeInLayer(copyLayer).x"),
 )
@@ -69,13 +69,16 @@ listVertical = (
 	("current position", "copyAnchor.y"),
 	("ascender", "selectedAscender"),
 	("cap height", "selectedCapheight"),
-	# ("smallcap height", "originalMaster.customParameters['smallCapHeight']"),
-	("x-height", "selectedXheight"),
+	("mid height", "selectedMidHeight"),
+	("body height", "selectedBodyHeight"),
+	("x-height/smallcap height", "selectedXheight"),
+	("slant height", "selectedSlantHeight"),
 	("half ascender", "selectedAscender // 2.0"),
 	("half cap height", "selectedCapheight // 2.0"),
-	# ("half smallcap height", "originalMaster.customParameters['smallCapHeight']/2"),
-	("half x-height", "selectedXheight // 2.0"),
-	("baseline", "0.0"),
+	("half mid height", "selectedMidHeight // 2.0"),
+	("half body height", "selectedBodyHeight // 2.0"),
+	("half x-height/smallcap height", "halfXHeight"),
+	("baseline", "selectedBaseline"),
 	("descender", "selectedDescender"),
 	("bbox top", "copyLayer.fastBounds().origin.y + copyLayer.fastBounds().size.height"),
 	("bbox center", "copyLayer.fastBounds().origin.y + (copyLayer.fastBounds().size.height // 2.0)"),
@@ -87,40 +90,51 @@ listVertical = (
 
 def italicSkew(x, y, angle=10.0):
 	"""Skews x/y along the x-axis and returns skewed x value."""
-	new_angle = (angle / 180.0) * math.pi
-	return x + y * math.tan(new_angle)
+	newAngle = (angle / 180.0) * math.pi
+	return x + y * math.tan(newAngle)
 
 
 class AnchorMover2(mekkaObject):
 	prefDict = {
-		"anchor_name": "",
+		"anchorName": "",
 		"hTarget": 0.0,
 		"hChange": 0.0,
 		"vTarget": 0.0,
 		"vChange": 0.0,
 		"italic": True,
 		"allMasters": False,
+		"verbose": False,
 	}
 
 	def __init__(self):
+		windowWidth  = 500
+		windowHeight = 200
+		windowWidthResize  = 700 # user can resize width by this value
+		windowHeightResize = 0   # user can resize height by this value
+		self.w = vanilla.FloatingWindow(
+			(windowWidth, windowHeight), # default window size
+			"Anchor Mover", # window title
+			minSize = (windowWidth -130, windowHeight), # minimum size (for resizing)
+			maxSize = (windowWidth + windowWidthResize, windowHeight + windowHeightResize), # maximum size (for resizing)
+			autosaveName = self.domain("mainwindow") # stores last window position and size
+		)
+		
 		linePos, inset, lineHeight = 12, 15, 22
 
-		self.w = vanilla.FloatingWindow((500, 175), "Anchor Mover", minSize=(370, 175), maxSize=(1000, 175), autosaveName=self.pref("mainwindow"))
-
 		self.w.text_1 = vanilla.TextBox((inset, linePos + 2, inset + 60, 14), "Move anchor", sizeStyle='small')
-		self.w.anchor_name = vanilla.ComboBox((inset + 75, linePos - 1, -110 - inset - 25, 19), self.GetAnchorNames(), sizeStyle='small', callback=self.SavePreferences)
-		self.w.button = vanilla.SquareButton((-110 - inset - 20, linePos, -110 - inset, 18), u"↺", sizeStyle='small', callback=self.SetAnchorNames)
+		self.w.anchorName = vanilla.ComboBox((inset + 75, linePos - 1, -110 - inset - 25, 19), self.GetAnchorNames(), sizeStyle='small', callback=self.SavePreferences)
+		self.w.button = vanilla.SquareButton((-110 - inset - 20, linePos, -110 - inset, 18), "↺", sizeStyle='small', callback=self.SetAnchorNames)
 		self.w.text_2 = vanilla.TextBox((-105 - 15, linePos + 2, -inset, 14), "in selected glyphs:", sizeStyle='small')
 		linePos += lineHeight
 
-		self.w.hText_1 = vanilla.TextBox((inset - 2, linePos, 20, 14), u"↔")
+		self.w.hText_1 = vanilla.TextBox((inset - 2, linePos, 20, 14), "↔")
 		self.w.hText_2 = vanilla.TextBox((inset + 20, linePos + 2, 20, 14), "to", sizeStyle='small')
 		self.w.hTarget = vanilla.PopUpButton((inset + 40, linePos, -50 - 15 - 15 - inset, 17), [x[0] for x in listHorizontal], sizeStyle='small', callback=self.SavePreferences)
 		self.w.hText_3 = vanilla.TextBox((-60 - 15 - 15, linePos + 2, -50 - inset, 14), "+", sizeStyle='small')
 		self.w.hChange = vanilla.EditText((-60 - 15, linePos, -inset, 19), "0.0", sizeStyle='small', callback=self.SavePreferences)
 		linePos += lineHeight
 
-		self.w.vText_1 = vanilla.TextBox((inset, linePos, 20, 14), u"↕")
+		self.w.vText_1 = vanilla.TextBox((inset, linePos, 20, 14), "↕")
 		self.w.vText_2 = vanilla.TextBox((inset + 20, linePos + 2, 20, 14), "to", sizeStyle='small')
 		self.w.vTarget = vanilla.PopUpButton((inset + 40, linePos, -50 - 15 - 15 - inset, 17), [y[0] for y in listVertical], sizeStyle='small', callback=self.SavePreferences)
 		self.w.vText_3 = vanilla.TextBox((-60 - 15 - 15, linePos + 2, -50 - inset, 14), "+", sizeStyle='small')
@@ -130,7 +144,10 @@ class AnchorMover2(mekkaObject):
 		self.w.italic = vanilla.CheckBox((inset, linePos, -inset, 18), "Respect italic angle", value=True, sizeStyle='small', callback=self.SavePreferences)
 		linePos += lineHeight
 
-		self.w.allMasters = vanilla.CheckBox((inset, linePos, -inset, 20), u"All masters and special layers (otherwise only current masters)", value=False, callback=self.SavePreferences, sizeStyle='small')
+		self.w.allMasters = vanilla.CheckBox((inset, linePos, -inset, 20), "All masters and special layers (otherwise only current masters)", value=False, callback=self.SavePreferences, sizeStyle='small')
+		linePos += lineHeight
+		
+		self.w.verbose = vanilla.CheckBox((inset, linePos-1, -inset, 20), "Log in Macro window (slow, use for tracking errors)", value=False, callback=self.SavePreferences, sizeStyle="small")
 		linePos += lineHeight
 
 		self.w.moveButton = vanilla.Button((-80 - 15, -20 - 15, -15, -15), "Move", callback=self.MoveCallback)
@@ -147,97 +164,110 @@ class AnchorMover2(mekkaObject):
 
 		thisFont = Glyphs.font
 		selectedLayers = thisFont.selectedLayers
-		anchor_name = self.pref("anchor_name")
-		horizontal_index = self.prefInt("hTarget")
-		horizontal_change = self.prefFloat("hChange")
-		vertical_index = self.prefInt("vTarget")
-		vertical_change = self.prefFloat("vChange")
+		anchorName = self.pref("anchorName")
+		horizontalIndex = self.prefInt("hTarget")
+		horizontalChange = self.prefFloat("hChange")
+		verticalIndex = self.prefInt("vTarget")
+		verticalChange = self.prefFloat("vChange")
 		allMasters = self.pref("allMasters")
 		respectItalic = self.pref("italic")
+		verbose = self.pref("verbose")
 
-		evalCodeH = listHorizontal[horizontal_index][1]
-		evalCodeV = listVertical[vertical_index][1]
+		evalCodeH = listHorizontal[horizontalIndex][1]
+		evalCodeV = listVertical[verticalIndex][1]
 
 		if not selectedLayers:
-			print("No glyphs selected.")
+			if verbose:
+				print("No glyphs selected.")
 			Message(title="No Glyphs Selected", message="Could not move anchors. No glyphs were selected.", OKButton=None)
 		else:
-			print("Processing %i glyph%s..." % (
-				len(selectedLayers),
-				"" if len(selectedLayers) == 1 else "s",
-			))
+			if verbose:
+				print("Processing %i glyph%s..." % (
+					len(selectedLayers),
+					"" if len(selectedLayers) == 1 else "s",
+				))
 			thisFont.disableUpdateInterface()
 			try:
 				for selectedLayer in selectedLayers:
 					selectedGlyph = selectedLayer.glyph()
-					if selectedGlyph:
-						print("\n🔠 %s" % selectedGlyph.name)
-						if allMasters:
-							originalLayers = tuple([layer for layer in selectedGlyph.layers if layer.isMasterLayer or layer.isSpecialLayer])
-						else:
-							originalLayers = (selectedLayer, )
+					if not selectedGlyph:
+						continue
+					if verbose:
+						print(f"\n🔠 {selectedGlyph.name}")
+					if allMasters:
+						originalLayers = tuple([layer for layer in selectedGlyph.layers if layer.isMasterLayer or layer.isSpecialLayer])
+					else:
+						originalLayers = (selectedLayer, )
 
-						for originalLayer in originalLayers:
-							originalMaster = originalLayer.master
-							selectedAscender = originalMaster.ascender
-							selectedCapheight = originalMaster.capHeight
-							selectedDescender = originalMaster.descender
-							selectedXheight = originalMaster.xHeight
-							halfXHeight = selectedXheight * 0.5
-							italicAngle = originalMaster.italicAngle
+					for originalLayer in originalLayers:
+						selectedAscender = originalLayer.metricForType_(GSMetricsTypeAscender)
+						selectedCapheight = originalLayer.metricForType_(GSMetricsTypeCapHeight)
+						selectedDescender = originalLayer.metricForType_(GSMetricsTypeDescender)
+						selectedXheight = originalLayer.metricForType_(GSMetricsTypexHeight)
+						selectedMidHeight = originalLayer.metricForType_(GSMetricsTypeMidHeight)
+						selectedBodyHeight = originalLayer.metricForType_(GSMetricsTypeBodyHeight)
+						selectedSlantHeight = originalLayer.metricForType_(GSMetricsTypeSlantHeight)
+						selectedBaseline = originalLayer.metricForType_(GSMetricsTypeBaseline)
+						halfXHeight = selectedXheight * 0.5
+						italicAngle = originalLayer.metricForType_(GSMetricsTypeItalicAngle)
 
-							if len(originalLayer.anchors) == 0:
-								print("⚠️ no anchors on layer %s." % originalLayer.name)
-							else:
-								thisGlyph = originalLayer.parent
+						if len(originalLayer.anchors) == 0:
+							if verbose:
+								print(f"⚠️ no anchors on layer ‘{originalLayer.name}’.")
+							continue
 
-								# create a layer copy that can be slanted backwards if necessary
-								copyLayer = originalLayer.copyDecomposedLayer()
-								copyLayer.decomposeCorners()
-								# thisGlyph.beginUndo()  # undo grouping causes crashes
+						thisGlyph = originalLayer.parent
 
-								try:
+						# create a layer copy that can be slanted backwards if necessary
+						copyLayer = originalLayer.copyDecomposedLayer()
+						copyLayer.decomposeCorners()
+						# thisGlyph.beginUndo()  # undo grouping causes crashes
+
+						try:
+							if italicAngle and respectItalic:
+								# slant the layer copy backwards
+								moveDown = transform(shiftY=-halfXHeight).transformStruct()
+								skewBack = transform(skew=-italicAngle).transformStruct()
+								moveUp = transform(shiftY=halfXHeight).transformStruct()
+								copyLayer.applyTransform(moveDown)
+								copyLayer.applyTransform(skewBack)
+								copyLayer.applyTransform(moveUp)
+
+							for copyAnchor in copyLayer.anchors:
+								if copyAnchor.name != anchorName:
+									continue
+									
+								oldAnchorX = copyAnchor.x
+								oldAnchorY = copyAnchor.y
+								xMove = eval(evalCodeH) + horizontalChange
+								yMove = eval(evalCodeV) + verticalChange
+
+								# Ignore moves relative to bbox if there are no paths:
+								if not copyLayer.paths:
+									if "bounds" in evalCodeH:
+										xMove = oldAnchorX
+
+									if "bounds" in evalCodeV:
+										yMove = oldAnchorY
+
+								# Only move if the calculated position differs from the original one:
+								originalAnchor = originalLayer.anchors[anchorName]
+								if int(oldAnchorX) != int(xMove) or int(oldAnchorY) != int(yMove):
 									if italicAngle and respectItalic:
-										# slant the layer copy backwards
-										moveDown = transform(shiftY=-halfXHeight).transformStruct()
-										skewBack = transform(skew=-italicAngle).transformStruct()
-										moveUp = transform(shiftY=halfXHeight).transformStruct()
-										copyLayer.applyTransform(moveDown)
-										copyLayer.applyTransform(skewBack)
-										copyLayer.applyTransform(moveUp)
+										# skew back
+										originalAnchor.position = italicize(NSPoint(xMove, yMove), italicAngle, halfXHeight)
+									else:
+										originalAnchor.position = NSPoint(xMove, yMove)
+									if verbose:
+										print(f"⚓️ {anchorName} → {originalAnchor.x}, {originalAnchor.y} in layer: {originalLayer.name}.")
+								elif verbose:
+									print(f"⚓️ {anchorName} anchor unchanged in {originalLayer.name}.")
 
-									for copyAnchor in copyLayer.anchors:
-										if copyAnchor.name == anchor_name:
-											old_anchor_x = copyAnchor.x
-											old_anchor_y = copyAnchor.y
-											xMove = eval(evalCodeH) + horizontal_change
-											yMove = eval(evalCodeV) + vertical_change
-
-											# Ignore moves relative to bbox if there are no paths:
-											if not copyLayer.paths:
-												if "bounds" in evalCodeH:
-													xMove = old_anchor_x
-
-												if "bounds" in evalCodeV:
-													yMove = old_anchor_y
-
-											# Only move if the calculated position differs from the original one:
-											originalAnchor = originalLayer.anchors[anchor_name]
-											if int(old_anchor_x) != int(xMove) or int(old_anchor_y) != int(yMove):
-												if italicAngle and respectItalic:
-													# skew back
-													originalAnchor.position = italicize(NSPoint(xMove, yMove), italicAngle, halfXHeight)
-												else:
-													originalAnchor.position = NSPoint(xMove, yMove)
-												print("⚓️ %s → %i, %i in layer: %s." % (anchor_name, originalAnchor.x, originalAnchor.y, originalLayer.name))
-											else:
-												print("⚓️ %s anchor unchanged in %s." % (anchor_name, originalLayer.name))
-
-								except Exception as e:
-									print("ERROR: Failed to move anchor in %s." % thisGlyph.name)
-									print(e)
-									import traceback
-									print(traceback.format_exc())
+						except Exception as e:
+							print(f"ERROR: Failed to move anchor in {thisGlyph.name}.")
+							print(e)
+							import traceback
+							print(traceback.format_exc())
 
 			except Exception as e:
 				raise e
@@ -250,8 +280,8 @@ class AnchorMover2(mekkaObject):
 		selectedLayers = Glyphs.currentDocument.selectedLayers()
 		try:
 			for thisLayer in selectedLayers:
-				AnchorNames = list(thisLayer.anchors.keys())  # hack to avoid traceback
-				for thisAnchorName in AnchorNames:
+				anchorNames = list(thisLayer.anchors.keys())  # hack to avoid traceback
+				for thisAnchorName in anchorNames:
 					if thisAnchorName not in myAnchorList:
 						myAnchorList.append(thisAnchorName)
 		except:
@@ -261,7 +291,7 @@ class AnchorMover2(mekkaObject):
 
 	def SetAnchorNames(self, sender):
 		anchorList = self.GetAnchorNames()
-		self.w.anchor_name.setItems(anchorList)
+		self.w.anchorName.setItems(anchorList)
 
 
 AnchorMover2()
