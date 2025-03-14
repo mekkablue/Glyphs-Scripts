21499d24dc673ac0e3c2895b714c7345aa49ffba
mekkablue <mekka@mekkablue.com>
2025-03-14 16:12:58 +0100

    Much faster (thx Lisa)

--- Kerning/New Tab with Uneven Symmetric Kernings.py
+++ Kerning/New Tab with Uneven Symmetric Kernings.py
@@ -7,8 +7,6 @@ Finds kern pairs for symmetric letters like ATA AVA TOT WIW etc. and sees if AT 
 
 from GlyphsApp import Glyphs, Message
 
-Font = Glyphs.font
-
 UC = "AHIMNOTUVWXYАЖИМНОПТФХШЏЫІΑΗΘΙΛΜΝΞΟΠΤΥΦΧΨΩ"
 SC = [f"{x.lower()}.sc" for x in UC]
 LC = "ilovwxжимноптфхшџыθοχω"
@@ -35,37 +33,27 @@ tabString = ""
 for glyphnames in (list(UC) + extraUC + SY, list(LC) + extraLC + SY, SC + extraSC + SY):
 	print(glyphnames)
 	for i, glyphname1 in enumerate(glyphnames):
+		glyph1 = thisFont.glyphs[glyphname1]
+		if not glyph1:
+			continue
 		for glyphname2 in glyphnames[i:]:
 			if glyphname1 != glyphname2:  # AAA makes no sense
-				if not thisFont.glyphs[glyphname1] or not thisFont.glyphs[glyphname2]:
-					for g in (glyphname1, glyphname2):
-						if not thisFont[g]:
-							print(f"⚠️ glyph ‘{g}’ does not exist.")
+				glyph2 = thisFont.glyphs[glyphname2]
+				if not glyph2:
+					continue
 				else:
-					# kerning exceptions:
-					leftKern = Font.kerningForPair(m.id, glyphname1, glyphname2)
-					rightKern = Font.kerningForPair(m.id, glyphname2, glyphname1)
+					layer1 = thisFont.glyphs[glyphname1].layers[m.id]
+					layer2 = thisFont.glyphs[glyphname2].layers[m.id]
+					direction = glyph1.direction
+					if direction == 1:
+						direction = glyph2.direction
+					if direction == 1:
+						direction = 0
+					leftKern = layer1.nextKerningForLayer_direction_(layer2, direction)
+					rightKern = layer2.nextKerningForLayer_direction_(layer1, direction)
 					if leftKern != rightKern:
 						print(f"{glyphname1}-{glyphname2}-{glyphname1}: exception not symmetric: {leftKern} vs. {rightKern}")
 						tabString += f"/{glyphname1}/{glyphname2}/{glyphname1}\n"
-					else:
-						glyph1 = thisFont.glyphs[glyphname1]
-						glyph2 = thisFont.glyphs[glyphname2]
-						if glyph1.rightKerningGroup and glyph2.leftKerningGroup and glyph2.rightKerningGroup and glyph1.leftKerningGroup:
-							# group kerning:
-							leftKern = Font.kerningForPair(m.id, "@MMK_L_" + glyph1.rightKerningGroup, "@MMK_R_" + glyph2.leftKerningGroup)
-							rightKern = Font.kerningForPair(m.id, "@MMK_L_" + glyph2.rightKerningGroup, "@MMK_R_" + glyph1.leftKerningGroup)
-							if leftKern != rightKern:
-								print("@%s-@%s-@%s: group kerning not symmetric: %s vs. %s" % (
-									glyph1.name,
-									glyph2.name,
-									glyph1.name,
-									leftKern,
-									rightKern,
-								))
-								tabString += f"/{glyphname1}/{glyphname2}/{glyphname1}\n"
-						else:
-							print(f"⚠️ missing kerning groups in glyphs: {glyphname1}, {glyphname2}.")
 
 if tabString:
 	# opens new Edit tab:
