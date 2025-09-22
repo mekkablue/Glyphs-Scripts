29bb7ccbc2bf03fe3bf9a4d2b6de2763bbd32301
mekkablue <mekka@mekkablue.com>
2025-09-22 17:15:03 +0200

    reorganize ttfautohint options

--- Hinting/Set Family Alignment Zones.py
+++ Hinting/Set Family Alignment Zones.py
@@ -4,19 +4,25 @@ from __future__ import division, print_function, unicode_literals
 __doc__ = """
 Inserts Family Alignment Zones parameter with values based on an instance. Needs properly set up and compatible alignment zones in Font Info > Masters.
 """
-from copy import deepcopy
+
 import vanilla
 from AppKit import NSFont
-from GlyphsApp import Glyphs, Message
+from copy import deepcopy, copy
+from GlyphsApp import Glyphs, Message, TTF, PLAIN, INSTANCETYPEVARIABLE
 from mekkablue import mekkaObject, UpdateButton
+from ttfautohintoptions import parameterName, valuelessOptions, availableOptionsDict, availableOptions, removeFromAutohintOptions, dictToParameterValue, ttfAutohintDict, glyphInterpolation, idotlessMeasure, writeOptionsToInstance
+# from ttfautohintoptions import *
 
 
 class SetFamilyAlignmentZones(mekkaObject):
+	prefDict = {
+		"addTTReference": False,
+	}
 
 	def __init__(self):
 		# Window 'self.w':
 		windowWidth = 350
-		windowHeight = 124
+		windowHeight = 134
 		windowWidthResize = 100  # user can resize width by this value
 		windowHeightResize = 0  # user can resize height by this value
 		self.w = vanilla.FloatingWindow(
@@ -30,11 +36,10 @@ class SetFamilyAlignmentZones(mekkaObject):
 		# UI elements:
 		linePos, inset, lineHeight = 12, 15, 22
 
-		self.w.descriptionText = vanilla.TextBox((inset, linePos + 2, -inset, lineHeight * 2), "Choose an instance (typically the Regular), and insert its zones as PostScript Family Alignment Zones.", sizeStyle='small', selectable=True)
-		linePos += lineHeight * 2
+		self.w.descriptionText = vanilla.TextBox((inset, linePos, -inset, lineHeight * 2), "Choose an instance (typically the Regular), and insert its zones as PostScript Family Alignment Zones.", sizeStyle='small', selectable=True)
+		linePos += int(lineHeight * 1.7)
 
 		self.w.instanceText = vanilla.TextBox((inset, linePos + 2, inset + 55, 14), "Instance", sizeStyle='small')
-
 		self.w.instancePicker = vanilla.PopUpButton((inset + 51, linePos, -inset - 22, 19), (), sizeStyle='small')
 		self.w.instancePicker.getNSPopUpButton().setToolTip_("Choose the instance that will likely be used most (probably the Regular or Book). Its interpolated zones will be used as Family Alignment Zones. Inactive instances are marked with ‘inactive’.")
 
@@ -45,27 +50,33 @@ class SetFamilyAlignmentZones(mekkaObject):
 		self.w.updateButton = UpdateButton((-inset - 18, linePos - 1, -inset, 18), callback=self.updateInstancePicker)
 		self.w.updateButton.getNSButton().setToolTip_("Click to update the menu with the instances of the currently frontmost font.")
 		linePos += lineHeight
+		
+		self.w.addTTReference = vanilla.CheckBox((inset, linePos-1, -inset, 20), "Also add as reference TTF to ttfautohint options", value=False, callback=self.SavePreferences, sizeStyle="small")
+		self.w.addTTReference.getNSButton().setToolTip_("Will write a TTF of the same instance into /Users/Shared/ttf/ and a --reference option to all existing ‘TTF Autohint Options’ parameters in Font Info > Exports.")
 
-		# Help Button:
-		self.w.helpTutorial = vanilla.HelpButton((inset, -20 - inset, 22, 22), callback=self.openURL)
-		self.w.helpTutorial.getNSButton().setToolTip_("Will open the glyphsapp.com tutorial about PS Hinting, at the section that explains Family Alignment Zones.")
+		# Help Buttons:
+		self.w.helpTutorialPS = vanilla.HelpButton((inset, -20 - inset, 22, 22), callback=self.openURL)
+		self.w.helpTutorialPS.getNSButton().setToolTip_("Will open the glyphsapp.com tutorial about PS Hinting, at the section that explains Family Alignment Zones.")
 
 		# Run Button:
 		self.w.runButton = vanilla.Button((-110 - inset, -20 - inset, -inset, -inset), "Insert FAZ", callback=self.SetFamilyAlignmentZonesMain)
 		self.w.setDefaultButton(self.w.runButton)
-
+		
 		# Open window and focus on it:
+		self.LoadPreferences()
 		self.updateInstancePicker()
 		self.w.open()
 		self.w.makeKey()
 
+
 	def openURL(self, sender):
 		URL = None
-		if sender == self.w.helpTutorial:
+		if sender == self.w.helpTutorialPS:
 			URL = "https://glyphsapp.com/learn/hinting-postscript-autohinting#g-__family-alignment-zones"
 		if URL:
 			import webbrowser
 			webbrowser.open(URL)
+
 
 	def updateInstancePicker(self, sender=None):
 		thisFont = Glyphs.font
@@ -106,20 +117,66 @@ class SetFamilyAlignmentZones(mekkaObject):
 				if regularIndexes:
 					self.w.instancePicker.set(regularIndexes[0])
 
+
+	def removeOption(self, sender, optionName="reference", parameterName="TTFAutohint options"):
+		for thisInstance in Glyphs.font.instances:
+			if thisInstance.customParameters[parameterName]:
+				removeFromAutohintOptions(thisInstance, optionName)
+				print(f"Removing ttfautohint {optionName} from instance '{thisInstance.name}'.")
+
+
+	def addReferenceFontToInstance(self, thisInstance, destinationFolder="/Users/Shared/.ttf", parameterName="TTFAutohint options", reference="reference"):
+		font = thisInstance.font
+		if not font:
+			Message(
+				title="TT Reference Error",
+				message=f"Could not determine the font for instance {thisInstance.name}. No ttfautohint reference established.",
+				OKButton=None,
+				)
+			return
+		
+		# create instance & generate font
+		fileName = font.familyName.strip().replace(" ", "").lower()
+		fullPath = os.path.join(destinationFolder, fileName + ".ttf")
+		referenceInstance = copy(thisInstance)
+		referenceInstance.font = font
+		referenceInstance.customParameters["fileName"] = fileName
+		referenceInstance.generate(TTF, destinationFolder, False, True, False, True, [PLAIN], True)
+		print(f"  📄 --reference={fullPath}")
+
+		for instance in font.instances:
+			if instance.type == INSTANCETYPEVARIABLE:
+				continue
+
+			# remove any previous --reference options
+			removeFromAutohintOptions(instance, reference)
+
+			# add the new reference
+			if instance.customParameters[parameterName] is not None:
+				optionDict = ttfAutohintDict(instance.customParameters[parameterName])
+				optionDict[reference] = fullPath
+				writeOptionsToInstance(optionDict, instance)
+				print(f"  ☑️ Set {reference} in ‘{parameterName}’ parameter in instance '{instance.name}'.")
+			else:
+				print(f"  🙅‍♂️ No ‘{parameterName}’ parameter in instance '{instance.name}'; skipping.")
+
+
 	def SetFamilyAlignmentZonesMain(self, sender):
 		try:
 			Glyphs.clearLog()  # clears macro window log
-
+			self.SavePreferences()
 			thisFont = Glyphs.font  # frontmost font
-			print(f"Set Family Alignment Zones Report for {thisFont.familyName}")
-			print(thisFont.filepath)
-			print()
+			print(f"👾 Family Alignment Zones Report for {thisFont.familyName}")
+			if thisFont.filepath:
+				print(f"📄 {thisFont.filepath}")
 
 			instanceName = self.w.instancePicker.getItem()
 			instanceIndex = int(instanceName[:instanceName.find(":")])
 			thisInstance = [i for i in thisFont.instances if i.type == 0][instanceIndex]
-			print("⚠️", thisInstance.name, instanceName)
+			
+			print(f"\n🤖 Adding PS family alignment zones...")
 			if thisInstance.name in instanceName:
+				# POSTSCRIPT FAMILY ZONES
 				if Glyphs.versionNumber >= 3:
 					# GLYPHS 3 code:
 					instanceZones = deepcopy(thisInstance.interpolatedFont.masters[0].alignmentZones)
@@ -127,14 +184,22 @@ class SetFamilyAlignmentZones(mekkaObject):
 					# GLYPHS 2 code:
 					instanceZones = thisInstance.interpolatedFont.masters[0].alignmentZones.__copy__()
 				thisFont.customParameters["Family Alignment Zones"] = instanceZones
-				print(f"✅ Set family alignment zones to instance {instanceName}")
+				print(f"  ☑️ Set family alignment zones to instance {instanceName}")
 				thisFont.parent.windowController().showFontInfoWindowWithTabSelected_(0) # font tab
+				
+				# TRUETYPE REFERENCE FONT
+				if self.pref("addTTReference"):
+					print("\n🤖 Adding TT reference font...")
+					self.addReferenceFontToInstance(thisInstance)
 			else:
 				Message(
 					title="Family Zones Error",
 					message=f"Seems like the instance you picked ({instanceName}) is not in the frontmost font. Please click the update button and choose again.",
 					OKButton=None,
 				)
+				print(f"\n❌ ERROR: ‘{instanceName}’ not in frontmost font ‘{thisFont.familyName}’.")
+
+			print("\n✅ Done.")
 		except Exception as e:
 			# brings macro window to front and reports error:
 			Glyphs.showMacroWindow()
---
+++ Hinting/ttfautohintoptions.py
@@ -0,0 +1,161 @@
+from AppKit import NSPoint
+from GlyphsApp import Glyphs
+
+parameterName = "TTFAutohint options"
+
+valuelessOptions = (
+	"adjust-subglyphs",
+	"composites",
+	"dehint",
+	"detailed-info",
+	"ignore-restrictions",
+	"no-info",
+	"symbol",
+	"ttfa-table",
+	"windows-compatibility",
+)
+
+availableOptionsDict = {
+	"adjust-subglyphs": "adjust-subglyphs",
+	"composites": "hint-composites",
+	"default-script": "default-script",
+	"dehint": "dehint",
+	"detailed-info": "ttfautohint-info",
+	"fallback-script": "fallback-script",
+	"fallback-stem-width": "fallback-stem-width",
+	"hinting-limit": "hinting-limit",
+	"hinting-range-max": "hint-set-range-minimum-hint-set-range-maximum",
+	"hinting-range-min": "hint-set-range-minimum-hint-set-range-maximum",
+	"ignore-restrictions": "miscellaneous",
+	"increase-x-height": "x-height-increase-limit",
+	"no-info": "ttfautohint-info",
+	"stem-width-mode": "stem-width-and-positioning-mode",
+	"strong-stem-width": "stem-width-and-positioning-mode",
+	"symbol": "symbol-font",
+	"ttfa-table": "add-ttfa-info-table",
+	"windows-compatibility": "windows-compatibility",
+	"x-height-snapping-exceptions": "x-height-snapping-exceptions",
+	"reference": "blue-zone-reference-font",
+}
+
+availableOptions = sorted(availableOptionsDict.keys())
+
+
+def removeFromAutohintOptions(thisInstance, removeOption):
+	parameter = thisInstance.customParameters[parameterName]
+	if parameter:
+		ttfAutohintOptions = parameter.split(" ")
+		popList = []
+		optionToBeRemoved = f"--{removeOption.strip()}"
+		for i, currentOption in enumerate(ttfAutohintOptions):
+			if currentOption.split("=")[0] == optionToBeRemoved:
+				popList.append(i)
+		if popList:
+			for j in sorted(popList)[::-1]:
+				ttfAutohintOptions.pop(j)
+			thisInstance.customParameters[parameterName] = " ".join(ttfAutohintOptions)
+
+
+def dictToParameterValue(ttfAutohintDict):
+	parameterValue = ""
+	for key in ttfAutohintDict:
+		parameterValue += " "
+		if not ttfAutohintDict[key]:
+			parameterValue += f"--{key.strip(' -')}"
+		else:
+			value = str(ttfAutohintDict[key]).strip()
+			parameterValue += f"--{key.strip(' -')}={value}"
+	return parameterValue.strip()
+
+
+def ttfAutohintDict(parameterValue):
+	"""Returns a dict for a TTFAutohint parameter value."""
+	ttfAutohintDict = {}
+	for ttfAutohintOption in parameterValue.split("--"):
+		if "=" in ttfAutohintOption:
+			[key, value] = ttfAutohintOption.split("=")
+			value = value.strip()
+		else:
+			key = ttfAutohintOption
+			value = None
+		if key:
+			ttfAutohintDict[key.strip(" -")] = value
+	return ttfAutohintDict
+
+
+def glyphInterpolation(thisGlyph, thisInstance):
+	try:
+		interpolatedFont = thisInstance.pyobjc_instanceMethods.interpolatedFont()
+		interGlyph = interpolatedFont.glyphForName_(thisGlyph.name)
+		if Glyphs.versionNumber >= 3:
+			# GLYPHS 3
+			interpolatedLayer = interGlyph.layer0()
+		else:
+			# GLYPHS 2
+			interpolatedLayer = interGlyph.layerForKey_(interpolatedFont.fontMasterID())
+
+		thisFont = thisGlyph.parent
+		if not thisInstance.customParameters["Grid Spacing"] and not (thisFont.gridMain() / thisFont.gridSubDivision):
+			interpolatedLayer.roundCoordinates()
+		if len(interpolatedLayer.paths) != 0:
+			return interpolatedLayer
+		else:
+			return None
+	except Exception as e:  # noqa: F841
+		import traceback
+		print(traceback.format_exc())
+		return None
+
+
+def idotlessMeasure(instance):
+	thisFont = instance.font
+	idotless = thisFont.glyphs["idotless"]
+	idotlessLayer = glyphInterpolation(idotless, instance)
+	if idotlessLayer:
+		measureHeight = idotlessLayer.bounds.size.height * 0.5
+		measureStartX = idotlessLayer.bounds.origin.x - 10
+		measureEndX = measureStartX + idotlessLayer.bounds.size.width + 20
+		measureStart = NSPoint(measureStartX, measureHeight)
+		measureEnd = NSPoint(measureEndX, measureHeight)
+		intersections = idotlessLayer.intersectionsBetweenPoints(measureStart, measureEnd, components=True)
+		if not len(intersections) > 2:
+			return None
+		else:
+			firstIntersection = intersections[1]
+			lastIntersection = intersections[-2]
+			idotlessWidth = lastIntersection.x - firstIntersection.x
+			return idotlessWidth
+	else:
+		return None
+
+
+def writeOptionsToInstance(optionDict, instance):
+	value = dictToParameterValue(optionDict)
+	try:
+		if Glyphs.versionNumber >= 3:
+			instanceWeightValue = instance.axes[0]  # fallback if there is no weight axis
+			font = instance.font
+			for axisIndex, axis in enumerate(font.axes):
+				if axis.axisTag == "wght":
+					instanceWeightValue = instance.axes[axisIndex]
+					break
+		else:
+			# GLYPHS 2
+			instanceWeightValue = instance.weightValue
+	except Exception as e:
+		instanceWeightValue = None
+		print(f"⚠️ Error determining the instance weight value:\n{e}")
+		import traceback
+		print(traceback.format_exc())
+
+	if instanceWeightValue is not None:
+		value = value.replace("--fallback-stem-width=*", f"--fallback-stem-width={instanceWeightValue}")
+
+	if "fallback-stem-width=idotless" in value:
+		actualStemWidth = idotlessMeasure(instance)
+		if actualStemWidth:
+			value = value.replace("--fallback-stem-width=idotless", f"--fallback-stem-width={int(actualStemWidth)}")
+		else:
+			print(f"Warning: Could not measure stem width of idotless in instance ‘{instance.name}’.")
+			return  # do nothing
+	instance.customParameters[parameterName] = value
