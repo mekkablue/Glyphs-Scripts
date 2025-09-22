5aa41d8dd9013427e8e94096c7b1c5b53a4a7b2c
mekkablue <mekka@mekkablue.com>
2025-09-22 11:32:05 +0200

    Update for G3 and alternate .fea conventions (thx Amin)

--- Kerning/Import Kerning from .fea File.py
+++ Kerning/Import Kerning from .fea File.py
@@ -4,90 +4,113 @@ from __future__ import division, print_function, unicode_literals
 __doc__ = """
 Choose an .fea file containing a kern feature in AFDKO code, and this script will attempt to import the kerning values into the frontmost font master (see Window > Kerning).
 """
-"""
-importFea.py
 
+"""
+originally: importFea.py
 Created by Georg Seifert on 2010-04-03.
 Copyright (c) 2010 schriftgestaltung.de. All rights reserved.
 """
 
 import os
-from GlyphsApp import Glyphs, GSGlyphsInfo, Message, GetFile
+from GlyphsApp import Glyphs, Message, GetOpenFile
 
+leftSideMarkers = ("MMK_L_", "_1ST", "_first")
+rightSideMarkers = ("MMK_R_", "_2ND", "_second")
 
-def importfea_file(Doc, filePath):
-	if os.path.isfile(filePath):
-		f = open(filePath)
+def importFeaFileToCurrentMaster(font, filePath):
+	master = font.selectedFontMaster
+	if not master:
+		Message(
+			title="No Font Master Selected",
+			message=f"Could not import .fea file because there is no font master selected in font ‘{font.familyName}’.",
+			OKButton=None,
+			)
+		return
 
-		Font = Doc.font
-		FontMasterIndex = Doc.windowControllers()[0].masterIndex()
-		print(FontMasterIndex)
-		GlyphsInfo = GSGlyphsInfo.sharedManager()
-		FontMaster = Font.masters[FontMasterIndex]
-		KerningLines = []
-		Line = f.readline()
-		while (len(Line) > 0):
+	if not os.path.isfile(filePath):
+		Message(
+			title="No .fea File",
+			message=f"Could not import .fea file because there appears to be no such file at {filePath}.",
+			OKButton=None,
+			)
+		return
+		
+	kerningLines = []
+	feaFile = open(filePath)
+	currentLine = feaFile.readline()
+	groupNameDict = {}
+	while (currentLine != ""): # end of file
+		if (currentLine[0] == "@"):
+			while "  " in currentLine:
+				currentLine = currentLine.replace("  ", " ")
+			groupName = currentLine[currentLine.find("@"):currentLine.find("=")].strip()
+			glyphNames = currentLine[currentLine.find("[")+1:currentLine.find("]")].strip().split(" ")
+			left = True
+			right = True
+			if any(subString in groupName for subString in leftSideMarkers):
+				right = False
+			elif any(subString in groupName for subString in rightSideMarkers):
+				left = False
+			oldGroupName = groupName
+			for nameParticle in leftSideMarkers + rightSideMarkers + ("@", "."):
+				groupName = groupName.replace(nameParticle, "")
+			# groupName = Glyphs.niceGlyphName(groupName)
+			for glyphName in glyphNames:
+				glyphName = Glyphs.niceGlyphName(glyphName)
+				glyph = font.glyphs[glyphName]
+				if glyph:
+					if left:
+						glyph.rightKerningGroup = groupName
+					if right:
+						glyph.leftKerningGroup = groupName
+			newGroupName = f"@MMK_{'L' if left else 'R'}_{groupName}"
+			groupNameDict[oldGroupName] = newGroupName
+		elif currentLine.strip().find("pos") == 0:
+			kerningLines.append(currentLine.strip())
+		currentLine = feaFile.readline() # read the next line
 
-			if (Line[0] == "@"):
-				key = Line[0:Line.find(" ")]
-				GlyphNames = Line[Line.find("[") + 1:-4].split(" ")
-				Left = True  # 1ST
-				Right = True
-				if key.find("_1ST") > 0:
-					Right = False
-				elif key.find("_2ND") > 0:
-					Left = False
+	if len(kerningLines) > 0:
+		for i in range(len(kerningLines)-1, -1, -1):
+			line = kerningLines[i]
+			keys = line.split(" ")
+			leftKey = keys[1]
+			rightKey = keys[2]
+			kernValue = keys[3]
 
-				key = key.split("_")[1]
-				key = GlyphsInfo.niceGlyphNameForName_(key)
+			if leftKey[0] == "@":
+				if leftKey in groupNameDict.keys():
+					leftKey = groupNameDict[leftKey]
+			else:
+				leftKey = leftKey.strip("[]")
+				leftKey = font.glyphs[leftKey].id
 
-				for GlyphName in GlyphNames:
-					GlyphName = GlyphsInfo.niceGlyphNameForName_(GlyphName)
-					Glyph = Font.glyphForName_(GlyphName)
-					if Glyph:
-						if Left:
-							Glyph.setRightKerningGroup_(key)
-						if Right:
-							Glyph.setLeftKerningGroup_(key)
-			elif Line.strip().find("pos") == 0:
-				KerningLines.append(Line.strip())
+			if rightKey[0] == "@":
+				if rightKey in groupNameDict.keys():
+					rightKey = groupNameDict[rightKey]
+			else:
+				rightKey = rightKey.strip("[]")
+				rightKey = font.glyphs[rightKey].id
 
-			Line = f.readline()
-
-		if len(KerningLines) > 0:
-			for i in range(len(KerningLines) - 1, -1, -1):
-				Line = KerningLines[i]
-				Keys = Line.split(" ")
-				LeftKey = Keys[1]
-				RightKey = Keys[2]
-				Value = Keys[3]
-				if LeftKey[0] == "@":
-					pass
-				else:
-					LeftKey = LeftKey.strip("[]")
-					LeftKey = Font.glyphs[LeftKey].id
-
-				if RightKey[0] == "@":
-					pass
-				else:
-					RightKey = RightKey.strip("[]")
-					RightKey = Font.glyphs[RightKey].id
-
-				Value = float(Value.replace(";", ""))
-				Font.setKerningForFontMasterID_LeftKey_RightKey_Value_(FontMaster.id, LeftKey, RightKey, Value)
+			kernValue = float(kernValue.replace(";", ""))
+			font.setKerningForPair(
+				master.id, # fontMasterId (str) – The id of the FontMaster
+				leftKey, # leftKey (str) – either a glyph name or a class name
+				rightKey, # rightKey (str) – either a glyph name or a class name
+				kernValue, # value (float) – kerning value
+				)
 
 
 def main():
-	Doc = Glyphs.currentDocument
-	if (Doc):
-		result = GetOpenFile(
-			message="Choose fea file containing kerning.",
+	font = Glyphs.font
+	if font:
+		feaFile = GetOpenFile(
+			message="Choose .fea file containing kerning.",
 			allowsMultipleSelection=False,
 			filetypes=["fea"],
-			path=None,
+			path=os.path.dirname(font.filepath) if font.filepath else None,
 			)
-		if result is not None:
-			importfea_file(Doc, result)
+		if feaFile is not None:
+			importFeaFileToCurrentMaster(font, feaFile)
 	else:
 		Message(
 			title="No Font Open",
