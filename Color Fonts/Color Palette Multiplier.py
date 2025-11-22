553b64435eb2bd2ce097030d50153ea28ac5223b
mekkablue <mekka@mekkablue.com>
2025-11-22 11:43:52 +0000

    First commit (not working yet)

---
+++ Color Fonts/Color Palette Multiplier.py
@@ -0,0 +1,192 @@
+#MenuTitle: Color Palette Multiplier
+# -*- coding: utf-8 -*-
+from __future__ import division, print_function, unicode_literals
+__doc__="""
+Apply a list of filter parameters to copies of CPAL/COLR layers. This way you can derive a shadow layer from a front layer, for example.
+"""
+
+import vanilla, sys
+from mekkablue import mekkaObject, UpdateButton
+from GlyphsApp import GSLayer, GSCustomParameter
+from AppKit import NSFont, NSString
+from copy import copy, deepcopy
+
+
+def extractNumber(s):
+	while len(s) > 0 and not s.isdigit():
+		if not s[0].isdigit():
+			s = s[1:]
+		if not s[-1].isdigit():
+			s = s[:-1]
+	return int(s)
+
+
+class ColorPaletteMultiplier(mekkaObject):
+	prefID = "com.mekkablue.ColorPaletteMultiplier"
+	prefDict = {
+		# "prefName": defaultValue,
+		"build": "# Color 1\nOffsetCurve;10;10;0;0.5;keep;\nExtrude;50;-46",
+		"derive": 0,
+		"allGlyphs": 0,
+		"overwrite": False,
+	}
+	
+	def __init__(self):
+		# Window 'self.w':
+		windowWidth  = 280
+		windowHeight = 360
+		windowWidthResize  = 400 # user can resize width by this value
+		windowHeightResize = 1000   # user can resize height by this value
+		self.w = vanilla.FloatingWindow(
+			(windowWidth, windowHeight), # default window size
+			"Color Palette Multiplier", # window title
+			minSize = (windowWidth, windowHeight), # minimum size (for resizing)
+			maxSize = (windowWidth + windowWidthResize, windowHeight + windowHeightResize), # maximum size (for resizing)
+			autosaveName = self.domain("mainwindow") # stores last window position and size
+		)
+		
+		# UI elements:
+		linePos, inset, lineHeight = 12, 15, 22
+		self.w.deriveText = vanilla.TextBox((inset, linePos+2, 70, 14), "Derive from", sizeStyle="small", selectable=True)
+		self.w.derive = vanilla.PopUpButton((inset+70, linePos, -inset-22, 17), self.currentColors(), sizeStyle="small", callback=self.SavePreferences)
+		self.w.deriveUpdate = UpdateButton((-inset-16, linePos-3, -inset, 18), callback=self.updateDerive)
+		linePos += lineHeight
+		
+		self.w.allGlyphs = vanilla.CheckBox((inset, linePos-1, -inset, 20), "Apply to all glyphs (otherwise selected only)", value=False, callback=self.SavePreferences, sizeStyle="small")
+		linePos += lineHeight
+		
+		self.w.overwrite = vanilla.CheckBox((inset, linePos-1, -inset, 20), "Overwrite preexisting target colors", value=False, callback=self.SavePreferences, sizeStyle="small")
+		linePos += lineHeight
+		
+		
+		self.w.descriptionText = vanilla.TextBox((inset, linePos+2, -inset, 14), "Build target colors with filter parameters:", sizeStyle="small", selectable=True)
+		linePos += lineHeight
+		
+		self.w.build = vanilla.TextEditor((1, linePos, -1, -40), "", callback=self.SavePreferences)
+		self.w.build.getNSTextView().setFont_(NSFont.userFixedPitchFontOfSize_(14.0))
+		self.w.build.getNSTextView().turnOffLigatures_(1)
+		self.w.build.getNSTextView().useStandardLigatures_(0)
+		self.w.build.selectAll()
+		
+		# Run Button:
+		self.w.runButton = vanilla.Button((-80-inset, -20-inset, -inset, -inset), "Build", sizeStyle="regular", callback=self.ColorPaletteMultiplierMain)
+		self.w.setDefaultButton(self.w.runButton)
+		
+		# Load Settings:
+		self.LoadPreferences()
+		
+		# Open window and focus on it:
+		self.w.open()
+		self.w.makeKey()
+
+	def currentColors(self, sender=None):
+		font = Glyphs.font
+		if not font:
+			return []
+		for parameter in font.customParameters:
+			if parameter.name != "Color Palettes":
+				continue
+			return [f"Color {i}" for i in range(len(parameter.value[0]))]
+		return []
+
+	def updateDerive(self, sender=None):
+		self.w.derive.setItems(self.currentColors())
+
+	def ColorPaletteMultiplierMain(self, sender=None):
+		try:
+			# clear macro window log:
+			Glyphs.clearLog()
+			
+			# update settings to the latest user input:
+			if not self.SavePreferences():
+				print("⚠️ ‘Color Palette Multiplier’ could not write preferences.")
+			
+			# read prefs:
+			for prefName in self.prefDict.keys():
+				try:
+					setattr(sys.modules[__name__], prefName, self.pref(prefName))
+				except:
+					fallbackValue = self.prefDict[prefName]
+					print(f"⚠️ Could not set pref ‘{prefName}’, resorting to default value: ‘{fallbackValue}’.")
+					setattr(sys.modules[__name__], prefName, fallbackValue)
+			
+			buildOrder = []
+			for buildLine in build.strip().splitlines():
+				buildLine = buildLine.strip()
+				if not buildLine:
+					continue
+				if buildLine[0] == "#":
+					buildOrder.append(extractNumber(buildLine))
+				else:
+					filterString = buildLine.strip().strip(";")
+					customParameter = GSCustomParameter(name="Filter", value=filterString)
+					buildOrder.append(customParameter)
+			
+			if not buildOrder:
+				Message(
+					title="Input Error",
+					message="Could not parse the input. Try ‘# Color 1:’ for the target layers and filter parameter strings split into lines below.",
+					OKButton=None,
+					)
+				return
+			
+			targetColors = set([i for i in buildOrder if isinstance(i, int)])
+			print("targetColors", targetColors)
+				
+			thisFont = Glyphs.font # frontmost font
+			if thisFont is None:
+				Message(title="No Font Open", message="The script requires a font. Open a font and run the script again.", OKButton=None)
+			else:
+				filePath = thisFont.filepath
+				if filePath:
+					reportName = f"{filePath.lastPathComponent()}\n📄 {filePath}"
+				else:
+					reportName = f"{thisFont.familyName}\n⚠️ The font file has not been saved yet."
+				print(f"Color Palette Multiplier Report for {reportName}")
+				print()
+
+				if self.pref("allGlyphs"):
+					theseGlyphs = thisFont.glyphs
+				else:
+					theseGlyphs = [l.parent for l in thisFont.selectedLayers if isinstance(l, GSLayer)]
+
+				derive = int(self.pref("derive"))
+				for thisGlyph in theseGlyphs:
+					for layerIndex in range(len(thisGlyph.layers)-1, -1, -1):
+						thisLayer = thisGlyph.layers[layerIndex]
+						print("layerIndex", layerIndex, thisLayer)
+
+						# remove layers to be regenerated
+						if self.pref("overwrite") and thisLayer.attributes["colorPalette"] in buildOrder:
+							print("deleting", layerIndex)
+							del thisGlyph.layers[layerIndex]
+							continue
+
+						# process layer duplicates and insert them
+						if thisLayer.attributes and thisLayer.attributes["colorPalette"] == derive:
+							print(thisLayer)
+							colorPalette = 0
+							for step in buildOrder:
+								print("STEP", step, type(step))
+								if isinstance(step, int):
+									print(f"  INSERT COLOR {step} AT {layerIndex}")
+									newLayer = deepcopy(thisLayer)
+									newLayer.attributes["colorPalette"] = step
+									newLayer.layerId = NSString.UUID()
+									# thisGlyph.layers.insert(layerIndex-1, newLayer)
+									thisGlyph.insertObject_inLayersArrayAtIndex_(newLayer, layerIndex-1)
+								else:
+									print("  APPLY PARAMETER")
+									newLayer.applyCustomParameters_callbacks_font_error_([step], None, thisFont, None)
+
+			print("\nDone.")
+
+		except Exception as e:
+			# brings macro window to front and reports error:
+			Glyphs.showMacroWindow()
+			print(f"Color Palette Multiplier Error: {e}")
+			import traceback
+			print(traceback.format_exc())
+
+
+ColorPaletteMultiplier()
