42bd123363372d0649190a19a17a4fa58a35ce05
Rainer Erich Scheichelbauer <mekka@mekkablue.com>
2026-03-30 11:54:11 +0200

    safety check for open font

--- Hinting/Set blueFuzz to zero for master instances.py
+++ Hinting/Set blueFuzz to zero for master instances.py
@@ -7,22 +7,25 @@ Adds blueFuzz custom parameter with value 0 for instances that are the same as a
 
 from GlyphsApp import Glyphs, INSTANCETYPESINGLE
 
-thisFont = Glyphs.font  # frontmost font
-axesValuesOfAllMasters = [m.axes for m in thisFont.masters]
 Glyphs.clearLog()  # clears log in Macro window
 
-thisFont.disableUpdateInterface()  # suppresses UI updates in Font View
-try:
-	for thisInstance in [i for i in thisFont.instances if i.type == INSTANCETYPESINGLE]:
-		if thisInstance.axes in axesValuesOfAllMasters:
-			thisInstance.customParameters["blueFuzz"] = 0
-			print(f"ℹ️ {thisInstance.name}: blueFuzz 0")
-except Exception as e:
-	Glyphs.showMacroWindow()
-	print("\n⚠️ Error in script: Set blueFuzz to zero for master instances\n")
-	import traceback
-	print(traceback.format_exc())
-	print()
-	raise e
-finally:
-	thisFont.enableUpdateInterface()  # re-enables UI updates in Font View
+thisFont = Glyphs.font  # frontmost font
+if thisFont is None:
+	print("⚠️ No font open.")
+else:
+	axesValuesOfAllMasters = [m.axes for m in thisFont.masters]
+	thisFont.disableUpdateInterface()  # suppresses UI updates in Font View
+	try:
+		for thisInstance in [i for i in thisFont.instances if i.type == INSTANCETYPESINGLE]:
+			if thisInstance.axes in axesValuesOfAllMasters:
+				thisInstance.customParameters["blueFuzz"] = 0
+				print(f"ℹ️ {thisInstance.name}: blueFuzz 0")
+	except Exception as e:
+		Glyphs.showMacroWindow()
+		print("\n⚠️ Error in script: Set blueFuzz to zero for master instances\n")
+		import traceback
+		print(traceback.format_exc())
+		print()
+		raise e
+	finally:
+		thisFont.enableUpdateInterface()  # re-enables UI updates in Font View
